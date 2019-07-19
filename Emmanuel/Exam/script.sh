3840c108b2536fc6c57756f5b1d2c6679b525a40
Emmanuel Rubio Navarro <emanuel.lv.97@gmail.com>
2019-07-19 13:55:46 -0500

    que sad

---
+++ Emmanuel/Exam/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#COMANDOS="ls pwd cd clear history pushd popd locate find which whatis mkdir touch man rm mv cp cat grep ps sudo chmod chown chgrp kill"
+#COMANDOSMAN="ls pwd clear locate find which whatis mkdir touch man rm mv cp cat grep ps sudo chmod chown chgrp kill"
+#mkdir bash
+#cd bash
+#mkdir $COMANDOS 
+#for COM in $COMANDOSMAN
+#  do
+#    whatis $COM > $COM/$COM'-nombre'
+#done
+#cd cd
+#echo "change directory">cd-nombre
+#cd ..
+#cd history
+#echo "historial de commandos">hisotry-nombre
+#cd ..
+#cd pushd
+#echo "push directory">pushd-nombre
+#cd ..
+#cd popd
+#echo "pop direcotry">popd-nombre
+#cd ..
