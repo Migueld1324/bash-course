ae0f287c5d211c37a97c1b96bc3387e5fc0b10d5
unknown <cristianliljon@hotmail.com>
2019-07-19 18:51:15 -0500

    Comit de tarea 3

---
+++ Cristian/cristian-bash-exercise3/script2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo adduser owner owner
+sudo adduser owner crisss
+sudo owner humberto
+sudo owner kimberly
+sudo chown -R owner Programs/
+sudo chgrp -R admin2 Programs/
+sudo chown owner Users/
+sudo chgrp -R guest Users/
+cd Users
+sudo chown -R crisss User1/
+sudo chown -R humberto User2/
+sudo chown -R kimberly User3/
+
