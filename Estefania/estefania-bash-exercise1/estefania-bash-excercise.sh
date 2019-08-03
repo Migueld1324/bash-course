c8090428f26dacb9acdecaebda0d3011511eb9ee
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 17:04:15 -0500

    Added Estefania's exercise

---
+++ Estefania/estefania-bash-exercise1/estefania-bash-excercise.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+clear
+mkdir -p C/{Programs,Windows,Users}
+cd C
+cd Programs/
+mkdir Spotify
+cd Spotify/
+touch license.txt version.txt
+cd ..
+cp -r Spotify/. Slack/
+cp -r Spotify/. Skype/
+cp -r Spotify/. WindowsDefender
+cp -r Spotify/. Intel/
+cp -r Spotify/. Lenovo/
+cp -r Spotify/. Mail/
+cp -r Spotify/. nodejs/
+cp -r Spotify/. mcafee/
+cp -r Spotify/. Media/
+ls -R
+cd ..
+cd Windows/
+mkdir PrimerFolder
+cd PrimerFolder
+touch file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt file10.txt
+cd ..
+cp -r PrimerFolder/. SegundoFolder/
+cp -r PrimerFolder/. TercerFolder/
+cp -r PrimerFolder/. CuartoFolder/
+cp -r PrimerFolder/. QuintoFolder/
+mkdir System32
+cd System32
+touch file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt file14.txt file15.txt
+cd ..
+ls -R
+cd ..
+cd Users/
+mkdir -p estefania/{Desktop,Documents,Downloads,Music,Pictures,Videos,Mail,Projects,Homework}
+cd Pictures
+cd estefania/
+cd Pictures
+touch gatito.png perro.png cutecat.jpg polonio.jpg vacaciones.jpg tarea1.jpg londres.png pingüino.png pingüinoazul.png gatos.jpg arboles.gif meme.gif planta.png flores.jpg polonio2.png
+ls
+cd ..
+cd Music
+mkdir Muse
+cd Muse
+touch track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cp -r Muse/. ArcticMonkeys/
+cp -r Muse/. Adele/
+cp -r Muse/. Babasonicos/
+cp -r Muse/. Björk/
+cp -r Muse/. Blink182/
+cp -r Muse/. Blur/
+cp -r Muse/. BrandonFlowers/
+cp -r Muse/. BrunoMars
+cp -r Muse/. Caifanes
+cp -r Muse/. CapitalCities
+cp -r Muse/. ChemicalBrothers
+cp -r Muse/. Coldplay
+cp -r Muse/. Disturbed
+cp -r Muse/. Dorian
+cp -r Muse/. FooFighters
+cp -r Muse/. FranzFerdinand
+cp -r Muse/. Jaguares
+cp -r Muse/. JoyDivision
+cp -r Muse/. Maroon5
+ls -R
+cd ..
+cd Videos
+touch reyLeon.mp4 spiderMan.mp4 toyStory.mp4 mascotas.mp4 mejorQueNunca.mp4 dolorYGloria.mp4 aladdin.mp4 leto.mp4 apolo.mp4 rocketman.mp4 iceCalling.mp4 johnWick.mp4 paranormal.mp4 hombresNegro.mp4 intimidad.mp4 mentiras.mp4 reinoBajoLaLuna.mp4 bajosInstintos.mp4 thor.mp4 ironMan.mp4
+ls
+cd ..
+cd Desktop/
+touch file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt file14.txt file15.txt
+ls
+cd ..
+cd Documents/
+touch file1.pdf file2.docx file3.pdf file4.txt file5.docx file6.pdf file7.txt file8.docx file9.pdf file10.pdf file11.docx file12.docx file13.pdf file14.docx file15.pdf
+ls
+cd ..
+cd Downloads/
+touch file1.pdf file2.docx file3.pdf file4.txt file5.docx file6.pdf file7.txt file8.docx file9.pdf file10.pdf file11.docx file12.docx file13.pdf file14.docx file15.pdf
+ls
+cd ..
+ls -R
+cd Projects/
+touch file1.pdf file2.docx file3.pdf file4.txt file5.docx file6.pdf file7.txt file8.docx file9.pdf file10.pdf file11.docx file12.docx file13.pdf file14.docx file15.pdf
+cd ..
+cd Homework/
+touch file1.pdf file2.docx file3.pdf file4.txt file5.docx file6.pdf file7.txt file8.docx file9.pdf file10.pdf file11.docx file12.docx file13.pdf file14.docx file15.pdf
+cd Homework
+ls
+ls -R
+cd ..
+ls -R
+cd Mail/
+touch file1.pdf file2.docx file3.pdf file4.txt file5.docx file6.pdf file7.txt file8.docx file9.pdf file10.pdf file11.docx file12.docx file13.pdf file14.docx file15.pdf
+cd ..
+ls -R
+cd ..
+cp -r estefania/. usuario2/
+cp -r estefania/. usuario3/
+ls -R
+ls
+cd ..
+history -w estefania-bash-excercise1
+cat estefania-bash-excercise1 
+history -w estefania-bash-excercise1.txt
