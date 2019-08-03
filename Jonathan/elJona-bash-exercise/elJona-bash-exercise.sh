9fb8f8d6e9e2038b55b7e8655eeb50d584640b42
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 18:41:12 -0500

    Added Jonathan's exercise

---
+++ Jonathan/elJona-bash-exercise/elJona-bash-exercise.sh
@@ -0,0 +1,371 @@
+#!/bin/bash
+
+mkdir C:
+cd C:
+mkdir Programs
+mkdir Windows
+mkdir Users
+# mkdir file1
+# cp file1
+# cp -r file1
+# cp -r file1
+# cp file1 -r
+# mkdir file2
+# mkdir file3
+# mkdir file4
+# mkdir file5
+# mkdir file6
+# mkdir file7
+# mkdir file8
+# mkdir file9
+# mkdir file10
+# cd file1
+# rm -r file10
+# rm -r file2
+# rm -r file3
+# rm -r file4
+# rm -r file5
+# rm -r file6
+# rm -r file7
+# rm -r file8
+# rm -r file9
+cd Programs
+mkdir folder1
+mkdir folder2
+mkdir folder3
+mkdir folder4
+mkdir folder5
+mkdir folder6
+mkdir folder7
+mkdir folder8
+mkdir folder9
+mkdir folder10
+touch folder1/file1.txt
+touch folder1/file2.txt
+# cp folder1 folder2
+# cd folder1
+# copy file1 folder2
+# cp file1 folder2
+cp -r folder1/. folder2/
+cp -r folder1/. folder3/
+cp -r folder1/. folder4/
+cp -r folder1/. folder5/
+cp -r folder1/. folder6/
+cp -r folder1/. folder7/
+cp -r folder1/. folder8/
+cp -r folder1/. folder9/
+cp -r folder1/. folder10/
+# cp folder1/file1 folder2
+# cp folder1/file1 folder2/
+# cp folder1/file1.txt folder2/
+# cp folder1/file2.txt folder2/
+# ls folder2/
+# cp folder2/file1.txt folder3 
+# cp folder2/file2.txt folder3 
+# ls folder3
+# cp folder3/file1.txt folder4
+# cp folder3/file2.txt folder4
+# ls folder 4
+# ls folder4
+# cp folder4/file2.txt folder5
+# cp folder4/file1.txt folder5
+# cp folder5/file1.txt folder6
+# cp folder5/file2.txt folder6
+# cp folder7/file2.txt folder7
+# cp folder6/file2.txt folder7
+# cp folder6/file1.txt folder7
+# cp folder6/file1.txt folder8
+# cp folder6/file2.txt folder8
+# cp folder6/file2.txt folder9
+# cp folder6/file1.txt folder9
+# cp folder6/file1.txt folder10
+# cp folder6/file2.txt folder10
+# ls folder 9
+# ls folder9
+cd ../Windows
+mkdir etc
+mkdir log
+mkdir green
+cd etc
+touch file1 file2
+cd ../
+cp -r etc/. log/
+cp -r etc/. green/
+mkdir blue cp
+cp -r etc/. blue
+cp -r etc/. cp
+# cd green
+# rm -r file1
+# touch file1
+# LS
+# cp blue/file1,file2 blue
+# cp blue/file1/file2 blue
+# cp blue/(file1,file2) blue
+# cp blue/file1 blue
+# cp log/file1 blue
+# cp log/file2 blue
+# cp log/file2 cp
+# cp log/file1 cp
+# clear
+mkdir System32
+System32/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10,file11,file12,file13,file14,file15}
+touch System32/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10,file11,file12,file13,file14,file15}
+mkdir System32/{dir1,dir2,dir3,dir4,dir5}
+cd System32
+touch dir1/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10}
+ls dir1
+cp dir1/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10} dir2
+ls dir2
+cp dir2/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10} dir3
+cp dir3/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10} dir4
+cp dir4/{file1,file2,file3,file4,file5,file6,file7,file8,file9,file10} dir5
+ls dir5
+ls dir4
+# cd ../..
+# ls -la
+# mkdir {Desktop,Documents,Downloads,Music,Pictures,Videos,Taxes,Porn,Puppies}
+# cd Downloads
+# cd Porn
+# touch Desktop/{document,document2,tesis,tesisOK,tesisOK1,tesisOKfinal,excel,spreadsheet,powerpoint,slides,publisher,image,jsonfile,note,notepad}
+# touch Documents/{file.word,file2.word,file1.ppt,file2.ppt,file3.pptx,file3.pbs,file.xls,file.vcs,file.txt,file.json,file2.mp4,file.rar,file.zip,file.ntp,files.*}
+# touch Downloads/{pelicula.mov,pelicula.mp4,pelicula.vcd,pelicula.avi,pelicula.mkv,song.mp3,song.avi,song.3gp,canciones.rar,programa.rar,aplicacion.iso,imagen.jpg,imagen.png,libro.pdf,revista.rev}
+# Windows
+# cd ls
+# cd cp
+# ls folder 2
+# ls folder2
+cd ../
+cp -r System32/dir1/. cp
+cp -r System32/dir1/. green
+cp -r System32/dir1/. etc
+cp -r System32/dir1/. blue
+cp -r System32/dir1/. log
+# cp System32 cp
+# cp System32 /cp
+pwd
+cd Programs
+cd Windows
+ls System32/dir1
+cd blue
+touch etc/{file3,file4,file5}
+cp etc/{file3,file4,file5} blue
+cp etc/{file3,file4,file5} cp
+ls cp
+cp etc/{file3,file4,file5} green
+cp etc/{file3,file4,file5} log
+touch log/{file6,file7,file8,file9,file10}
+ls log
+cp log/{file6,file7,file8,file9,file10}e3,file4,file5} green
+cp log/{file6,file7,file8,file9,file10} green
+ls green
+cp log/{file6,file7,file8,file9,file10} etc
+cp log/{file6,file7,file8,file9,file10} cp
+cp log/{file6,file7,file8,file9,file10} blue
+ls etc
+ls blue
+ls System32
+cd ../../Users
+mkdir {admi,elJona,guest}
+mkdir admi/{Desktop,Documents,Downloads,Music,Pictures,Videos,Taxes,Porn,Puppies}
+cd admi/
+touch Desktop/{document,document2,tesis,tesisOK,tesisOK1,tesisOKfinal,excel,spreadsheet,powerpoint,slides,publisher,image,jsonfile,note,notepad}
+touch Documents/{file.word,file2.word,file1.ppt,file2.ppt,file3.pptx,file3.pbs,file.xls,file.vcs,file.txt,file.json,file2.mp4,file.rar,file.zip,file.ntp,files.*}
+touch Downloads/{pelicula.mov,pelicula.mp4,pelicula.vcd,pelicula.avi,pelicula.mkv,song.mp3,song.avi,song.3gp,canciones.rar,programa.rar,aplicacion.iso,imagen.jpg,imagen.png,libro.pdf,revista.rev}
+la Downloads
+touch Pictures/{imagen1.jpg,image2.png,img3.psd,imagen4.pic,img5.dib,img6.atcd,playa.img,torre.jpeg,vacaciones.png,verano10.png,casa.rgb,familia.png,test.jpg,invierno.jpeg,tacos.png}
+# cd projects
+# history
+# cd cursoBash
+# cd C:
+# cd Users
+# ls Documents
+# ls Downloads
+# ls Pictures
+touch Music/{vuelve1.mp3,toxicity2.mp3,chop_suey3.mp3,bed_of_roses4.mp3,torn5.mp3,getout6.mp3,rastamandita7.mp3,controlMachete8.mp3,la_mordidita9.mp3,veneno10.mp3,malherido11.mp3,crazy12.mp3,carencia13.mp3,frijolero14.mp3,despacito15.mp3}
+ls music
+ls Desktop
+# lsman history
+# man history
+# history -W
+ls 
+touch Videos/{lord_of_the_rings.mp4,Vanilla_sky.avi,big_hero_6.vcd,Avengers.mp4,robocop.avi,terminator.mp4,terminator2.avi,terminator3.mkv,the_revenant.mp4,spiderman.mp4,thor.avi,shazam.vcd,the_lion_king.mp4,aladdin.avi}
+touch Videos/{tron.mp4,queen.avi}
+ls Videos
+touch Porn/{asa_akira,alexis_texas,brandi_love,lisa_ann,abella_danger,capri,holly_michaels,ava_addams,arianna_knight,lanny_barbie,esperanza_gomez,angela_white,valerie_key,franceska_james,jayden_james}
+ls Porn
+touch Puppies/{rothwiller,cody,mastin,labrador,220,mestizo,petra,catrina,pastor_aleman,husky,lobo,bulldog,salchicha,lassie,beethoven,k9}
+ls Puppies
+touch Taxes/{taxesYear2000,taxesYear2001,taxesYear2002,taxesYear2004,taxesYear2005,taxesYear2006,taxesYear2008,taxesYear2010,taxesYear2011,taxesYear2012,taxesYear2014,taxesYear2015,taxesYear2016,taxesYear2017,taxesYear2018}
+ls Taxes
+mkdir Music/{molotov,juanga,chepe_chepe,la_arrolladora_banda_limon,creed}
+ls -a
+ls -l Music
+ls -a Music
+cd Music/
+touch chepe_chepe/{payaso.mp3,farolero.mp3,la_nave_del_olvido.mp3,anda_y_ve.mp3,el_triste.mp3}
+# touch {kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura paussini,cors,michael jackson,calo,velvet revolver,naruto OST,caifanes}
+cd ..
+# touch Music/{kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura paussini,cors,michael jackson,calo,velvet revolver,naruto OST,caifanes}
+touch Music/{kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura_paussini,cors,michael_jackson,calo,velvet_revolver,naruto_OST,caifanes}
+cd Music
+mkdir Music/{kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura_paussini,cors,michael_jackson,calo,velvet_revolver,naruto_OST,caifanes}
+ls Music
+mv Music/{kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura paussini,cors,michael jackson,calo,velvet revolver,naruto OST,caifanes} ../Music
+rm -r Music
+rm -r Music/{kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura_paussini,cors,michael_jackson,calo,velvet_revolver,naruto_OST,caifanes}
+rm -r {kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura_paussini,cors,michael_jackson,calo,velvet_revolver,naruto_OST,caifanes}
+y
+mkdir {kabah,fey,rock_en_tu_idioma,joan_sebastian,valentin_elizalde,juanes,cranberries,ricky,laura_paussini,cors,michael_jackson,calo,velvet_revolver,naruto_OST,caifanes}
+ls
+ls -l
+touch caifanes/{la_celula_que_explota.mp3,mi_ojo_de_venado.mp3,la_negra_tomasa.mp3,viento.mp3,matenme_porque_me_muero.mp3}
+touch calo/{formas_de_amor.mp3,el_cubo.mp3,ponte_atento.mp3,no_puedo_mas.mp3,colegiala.mp3}
+ls chepe_chepe
+touch cors/{breathless.mp3,what_can_i_do.mp3,all_the_love_in_the_world.mp3,runaway.mp3,radio.mp3}
+ls cors
+touch cranberries/{zombie.mp3,dreams.mp3,linger.mp3,promises.mp3,why?.mp3}
+ls cranberries
+touch creed/{my_sacrifice.mp3,lullaby.mp3,higher.mp3,one.mp3,bullets.mp3}
+touch fey/{azucar_amargo.mp3,popocatepetl.mp3,muevelo.mp3,frio.mp3,subidon.mp3}
+touch joan_sebastian/{25rosas.mp3,idiota.mp3,me_gustas.mp3,rumores.mp3,juliantla.mp3}
+touch juanes/{la_camisa_negra.mp3,no_creo_en_el_jamas.mp3,yerbatero.mp3,la_paga.mp3,mala_gente.mp3}
+touch juanga/{abrazame_muy_fuerte.mp3,querida.mp3,caray.mp3,asi_fue.mp3,amor_eterno.mp3}
+touch kabah/{la_calle_de_las_sirenas.mp3,vive.mp3,antro.mp3,estare.mp3,al_pasar.mp3}
+touch la_arrolladora_banda_limon/{sobre_mis_pies.mp3,huele_a_peligro.mp3,cabecita_dura.mp3,y_que_quede_claro.mp3,mas_adelante.mp3}
+touch laura_paussini/{viveme.mp3,en_cambio_no.mp3,laura.mp3,bienvenido.mp3,vive_ya.mp3}
+touch michael_jackson/{thriller.mp3,billie_jean.mp3,beat_it.mp3,bad.mp3,dangerous.mp3}
+touch molotov/{puto.mp3,cerdo.mp3,frijolero.mp3,rastamandita.mp3,gimme_the_power.mp3}
+# cd projects
+# cd cursoBash
+# cd C:
+# cd Users
+# cd Music
+ls
+ls -l
+touch naruto_OST/{flow.mp3,haruka_kanata.mp3,go.mp3,guren.mp3,heros_come_back.mp3}
+touch ricky/{la_copa_de_la_vida.mp3,vuelve.mp3,volveras.mp3,pegadito.mp3,maria.mp3}
+touch rock_en_tu_idioma/{musica_ligera.mp3,plastico_fino.mp3,mi_aguita_amarilla.mp3,sin_aliento.mp3,la_ciudad_de_la_furia.mp3}
+touch valentin_elizalde/{lobo_domesticado.mp3,satisfaccion.mp3,volvere_a_amar.mp3,vencedor.mp3,soy_asi.mp3}
+touch velvet_revolver/{the_last_fight.mp3,big_machine.mp3,let_it_rol.mp3,she_mine.mp3,spectacle.mp3}
+history
+rm -r {bed_of_roses4.mp3,carencia13.mp3,chop_suey3.mp3,controlMachete8.mp3,crazy12.mp3,despacito15.mp3,frijolero14.mp3,getout6.mp3,la_mordidita9.mp3,malherido11.mp3,rastamandita7.mp3,torn5.mp3,toxicity2.mp3,veneno10.mp3,vuelve1.mp3}
+ls
+cd ../..
+ls
+cd users
+ls
+cd ..
+cd Users
+mkdir {admi,elJona,guest}
+ls
+mv Desktop admi
+ls
+mv {Documents/,Downloads,Music/,Pictures/,Porn/,Puppies/,Taxes/,Videos/} admi
+ls
+ls admi
+cp admi/{Documents/,Downloads,Music/,Pictures/,Porn/,Puppies/,Taxes/,Videos/} elJona
+cp -r admi/. elJona
+ls elJona
+ls admi
+# cp -r admi/{Documents/,Downloads,Music/,Pictures/,Porn/,Puppies/,Taxes/,Videos/} elJona
+ls admi
+ls elJona
+# ls
+# rm -r elJona
+# ls elJona
+# ls
+# rm -r elJona
+# rm -r elJona
+# rm -r elJona
+ls
+# ls admi
+# mkdir elJona
+# cp admi/Documents elJona
+# cp admi/Desktop elJona
+# cp -r admi/Desktop elJona
+# ls elJona
+# cp -r admi/Downloads elJona
+cp -r admi guest
+ls guest
+ls
+cd guest
+cd ..
+rm -r guest/admi
+y
+rm -r guest/admi
+y
+rm -r guest/admi
+ls
+ls guest
+ls admi
+cp -r admi/Desktop guest
+ls guest
+cp -r admi/Documents guest
+cp -r admi/Downloads guest
+cp -r admi/Music guest
+cp -r admi/Pictures guest
+cp -r admi/Porn guest
+cp -r admi/Puppies guest
+cp -r admi/Taxes guest
+cp -r admi/Videos guest
+ls elJona
+ls guest
+cp -r admi/Documents elJona
+cp -r admi/Music elJona
+cp -r admi/Pictures elJona
+cp -r admi/Porn elJona
+cp -r admi/Puppies elJona
+cp -r admi/Taxes elJona
+cp -r admi/Videos elJona
+ls
+ls admi
+ls guest
+ls elJona
+# history > historial.txt
+# cat historial.txt
+# export HISTTIMEFORMAT='%F %T : '
+# history
+# echo "export HISTTIMEFORMAT='%F %T : '" >> $HOME/.bashrc
+# history
+# history -a
+# history | tail 200
+# history | tail -200
+# history | tail -2000
+# cat historial.txt
+# history
+# cat ~/.zsh_history
+# locate bash_history
+# cat ~/.bash_history
+# locate _history
+# history
+# vim ~/.bash_history
+# vim ~/.zsh_history
+# ls -al ~/
+# vim ~/.zhistory
+# cat ~/.zhistory | tail -200
+# vim ~/.zhistory
+# cat ~/.zhistory > historial.txt
+# cat historial.txt
+# rm -r historial.txt
+# cat ~/.zhistory > historial.txt
+# cat historial.txt
+# nano historial.txt
+# vim historial.txt
+# cd ..
+# git clone git@github.com:Migueld1324/bash-course.git
+# ls
+# cd bash-course
+# ls
+# git branch -b elJona-bash-exercise1
+# git checkout -b elJona-bash-exercise1
+# ls
+# git branch
+# ls Jonathan
+# git branch
+# cd ..
+# ls
+# cd bash-course
+# ls
+# git branch
+# cd Jonathan
+# cat ~/.zhistory > historial.txt
