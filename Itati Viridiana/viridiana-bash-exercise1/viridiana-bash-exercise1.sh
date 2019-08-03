2739e35d2b988b8b279932083274eaf27bc2e243
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 17:35:17 -0500

    Added Viridiana's exercise

---
+++ Itati Viridiana/viridiana-bash-exercise1/viridiana-bash-exercise1.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+# cd/
+# clear
+# cd /
+# ls
+# cd mnt
+# ls
+# cd c
+# ls
+# cd Users
+# ls
+# cd 'Lenovo 1'
+# ls
+# cd Documents/
+mkdir C
+LS
+ls
+cd C
+mkdir -p Programs/{Carpeta1,Carpeta2,Carpeta3,Carpeta4,Carpeta5,Carpeta6,Carpeta7,Carpeta8,Carpeta9,Carpeta10}
+cd Programs/
+ls
+cd Carpeta1
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta2
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta3
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta4
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta5
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta6
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta7
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta8
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta9
+touch Archivo1.txt Archivo2.txt
+cd ..
+cd Carpeta10
+touch Archivo1.txt Archivo2.txt
+cd ../../
+mkdir Windows
+cd Windows/
+mkdir Carpeta1
+cd Carpeta1/
+touch Archivo1.txt Archivo2.txt Archivo3.txt Archivo4.txt Archivo5.txt Archivo6.txt Archivo7.txt Archivo8.txt Archivo9.txt Archivo10.txt
+ls
+cd ..
+cp -p Carpeta1/. Carpeta2/
+cp -r Carpeta1/. Carpeta2/
+cp -r Carpeta1/. Carpeta3/
+cp -r Carpeta1/. Carpeta4/
+cp -r Carpeta1/. Carpeta5/
+mkdir System32
+cd System32/
+touch Archivo1.txt Archivo2.txt Archivo3.txt Archivo4.txt Archivo5.txt Archivo6.txt Archivo7.txt Archivo8.txt Archivo9.txt Archivo10.txt Archivo11.txt Archivo12.txt Archivo13.txt Archivo14.txt Archivo15.txt
+cd ..
+cd System32/
+mkdir Carpeta1
+cd Carpeta1/
+touch Archivo1.txt Archivo2.txt Archivo3.txt Archivo4.txt Archivo5.txt Archivo6.txt Archivo7.txt Archivo8.txt Archivo9.txt Archivo10.txt Archivo11.txt Archivo12.txt Archivo13.txt Archivo14.txt Archivo15.txt
+cd ..
+cp -r Carpeta1/. Carpeta2/
+cp -r Carpeta1/. Carpeta3/
+cp -r Carpeta1/. Carpeta4/
+cp -r Carpeta1/. Carpeta5/
+cd ../../
+mkdir Users
+cd Users/
+mkdir -p UserOne/{Desktop,Documents,Downloads,Music,Pictures,Video,Carpeta1,Carpeta2,Carpeta3}
+cp -r UserOne/. UserTwo/
+cp -r UserOne/. UserThree/
+cd UserOne/
+ls
+cd Desktop/
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Documents/
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Downloads/
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Carpeta1
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Carpeta2
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Carpeta3
+touch Gato.txt Perro.docx Gallina.doc Leon.xls Pajaro.ppt Pato.pdf Araña.txt Mariposa.pdf Leopardo.ppt Gato.txt Vaca.docx Burro.pdf Gato.txt Aguila.ppt Oso.txt
+cd ..
+cd Music/
+mkdir Pablo_Montero
+cd Pablo_Montero/
+track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Pablo_Montero/
+touch track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cp -r Pablo_Montero/. Thalia/
+cp -r Pablo_Montero/. Pablo_Alvoran/
+cp -r Pablo_Montero/. Wisin_Yandel/
+cp -r Pablo_Montero/. Shakira/
+cp -r Pablo_Montero/. Enrique_iglesias/
+cp -r Pablo_Montero/. Jennifer_Lopez/
+cp -r Pablo_Montero/. Ricky_Martin/
+cp -r Pablo_Montero/. Marc_Anthony/
+cp -r Pablo_Montero/. Michael_jackson/
+cp -r Pablo_Montero/. Chayanne/
+cp -r Pablo_Montero/. David_Bisbal/
+cp -r Pablo_Montero/. Laura_Pausini/
+cp -r Pablo_Montero/. Ricardo_Arjona/
+cp -r Pablo_Montero/. Paulina_Rubio/
+cp -r Pablo_Montero/. Ricardo_Arjona/
+cp -r Pablo_Montero/. Lady_Gaga/
+cp -r Pablo_Montero/. Juanes/
+cp -r Pablo_Montero/. Gloria Trevi/
+cp -r Pablo_Montero/. Gloria_Trevi/
+cp -r Pablo_Montero/. Alejandro_Sanz/
+cp -r Pablo_Montero/. Belinda/
+cd ..
+cd Pictures/
+touch Perro.png Gato.jpg Leon.png Mariposa.jpg Araña.png Cocodrilo.jpg Leon.png Pajaro.jpg Leopardo.png Mariposa.jpg vibora.png Nube.png Sol.jpg silla.png mesa.jpg computadora.png Pulsera.pnj mause.pgn teclado.png dedo.jpg
+cd ..
+cd Video/
+video1.mp4 video2.mp4 video3.mp4 video4.mp4 video5.mp4 video6.mp4 video7.mp4 video8.mp4 video9.mp4 video10.mp4 video11.mp4 video12.mp4 video13.mp4 video14.mp4 video15.mp4
+touch video1.mp4 video2.mp4 video3.mp4 video4.mp4 video5.mp4 video6.mp4 video7.mp4 video8.mp4 video9.mp4 video10.mp4 video11.mp4 video12.mp4 video13.mp4 video14.mp4 video15.mp4
+cd ..
+rm -r UserTwo
+rm -r UserThree
+cp -r UserOne/. UserTwo/
+cp -r UserOne/. UseThree/
+cd ..
+history -w Viridiana-bash-excercise1
+history -w Viridiana-bash-excercise1.txt
