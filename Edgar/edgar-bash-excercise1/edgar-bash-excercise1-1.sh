dcf58fa7d827651434a4d51af2546a9da2e0857d
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 16:18:12 -0500

    Added Edgar's exercises

---
+++ Edgar/edgar-bash-excercise1/edgar-bash-excercise1-1.sh
@@ -0,0 +1,221 @@
+#!/bin/bash
+
+mkdir C C/Programs C/Windows C/Users
+cd C
+ls
+mkdir Programs/Dolby Programs/GIT Programs/Intel Programs/Iexplorer Programs/Lenovo Programs/Mcafee Programs/nodejs Programs/Realtek Programs/Synaptics Programs/rempl
+touch C/Programs/Dolby/Demo.txt C/Programs/Dolby logo.png
+cd Programs
+ls
+touch Dolby/demo.txt Dolby/logo.png
+touch GIT/license.txt GIT/notes.txt
+touch Ixplorer/notes.txt Iexplorer/export.js
+touch Iexplorer/notes.txt
+touch Intel/license.txt Intel/ver.dll
+touch Lenovo/modern.txt Lenovo/winmd.dll
+touch Mcafee/viewer.png Mcafee/license.txt
+touch Realtek/license.txt Realtek/adctl.png
+touch Synaptics/tap.txt Synaptics/cortana.png
+touch nodejs/license.txt nodejs/notes.dll
+touch rempl/logo.png rempl/sedsvc.dll
+cd ..
+cd Windows
+mkdir Boot Fonts Branding Help IME
+ls
+touch Boot/boot1.txt Boot/boot2.txt Boot/boot3.txt Boot/boot4.txt Boot/boot5.txt Boot/boot6.dll Boot/boot7.dll Boot/boot8.dll Boot/boot9.dll Boot/boot10.dll
+touch Branding/Br1.txt Branding/BR2.txt Branding/Br3.txt Branding/Br4.txt Branding/br5.txt Branding/Br6.dll Branding/Br7.dll Branding/Br8.dll Branding/Br9.dll Branding/Br10.dll
+touch Fonts/1.dll Fonts/2.dll Fonts/3.dll Fonts/4.dll Fonts/5.dll Fonts/6.txt Fonts/7.txt Fonts/8.txt Fonts/9.txt Fonts/10.txt
+touch Help/H1.dll Help/H2.dll Help/H3.dll Help/H4.dll Help/H5.dll Help/H6.txt Help/H7.txt Help/H8.txt Help/H9.txt Help/H10.txt
+touch IME/1.txt IME/2.txt IME/3.txt IME/4.txt IME/5.txt IME/6.dll IME/7.dll IME/8.dllIME/9.dll IME/10.dll
+touch IME/8.dll IME/9.dll
+mkdir System32
+cd System32
+touch 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt 10.txt 11.txt 13.txt 13.txt 14.txt 15.txt
+mkdir 1 2 3 4 5 
+touch 1/1.txt 1/2.txt 1/3.txt 1/4.txt 1/5.txt 1/6.txt 1/7.txt 1/8.txt 1/9.txt 1/10.txt
+touch 2/1.dll 2/2.dll 2/3.dll 2/4.dll 2/5.dll 2/6.dll 2/7.dll 2/8.dll 2/9.dll 2/19.dll
+touch 3/1.txt 3/2.txt 3/3.txt 3/4.txt 3/5.txt 3/6.txt 3/7.txt 3/8.txt 3/9.txt 3/10.txt
+touch 4/1.dll 4/2.dll 4/3.dll 4/5.dll 4/6.dll 4/7.dll 4/8.dll 4/9.dll 4/10.dll
+touch 5/1.dll 5/2.txt 5/3.dll 5/4.txt 5/5.dll 5/6.txt 5/7.dll 5/8.txt 5/9.dll 5/10.txt
+ls
+cd 3
+ls
+cd ..
+cd Users
+cd User
+cd ..
+cd Users
+mkdir user1 user2 user3
+cd user1
+mkdir Desktop Documents Downloads Music Pictures Video tarea git bash
+touch Desktop/1.ico Desktop/2.ico Desktop/3.ico Desktop/4.ico Desktop/5.ico Desktop/6.ico Desktop/7.ico Desktop/8.ico Desktop/9.ico Desktop/10.ico Desktop/11.ico Desktop/12.ico Desktop/13.ico Desktop/14.ico Dekstop/15.ico
+touck Desktop/15.ico
+touch Desktop/15.ico
+touch Documents/tarea1.pdf Documents/tarea2.pdf Documents/tarea3.pdf Documents/tarea4.pdf Documents/tarea5.pdf Documents/expo1.ppt Documents/expo2.ppt Documents/expo3.ppt Documents/expo4.ppt Documents/expo5.ppt Documents/inv1.doc Documents/inv2.doc Documents/inv3.doc Documents/inv4.doc Documents/inv5.doc
+touch Downloads/1.png Downloads/2.png Downloads/3.png Downloads/4.png Downloads/5.png Downloads/p1.exe Downloads/p2.exe Downloads/p3.exe Downloads/94.exe Downloads/p5.exe Downloads/1.doc Downloads/2.xls Downloads/3.ppt Downloads/4.pdf Downloads/5.txt
+touch Pictures/1.png Pictures/2.png Pictures/2.png Pictures/3.png Pictures/4.png Pictures/5.png Pictures/6.jpg Pictures/7.jpg Pictures/8.jpg Pictures/9.jpg Pictures/10.jpg Pictures/11.gif Pictures/12.gif Pictures/13.gif Pictures/14.gif Pictures/15.gif
+ls 
+cd bash
+touch 1.SH 2.SH 3.SH 4.SH 5.SH 6.SH 7.SH 8.SH 9.SH 10.SH 11.SH 12.SH 13.SH 14.SH 15.SH
+cd ..
+cd git
+touch 1.git 2.git 3.git 4.git 5.git 6.git 7.git 8.git 9.git 10.git 11.git 12.git 13.git 14.git 15.git
+cd ..
+cd tarea
+touch tarea1.doc tarea2.doc tarea3.doc tarea4.doc tarea5.doc tarea6.pdf tarea7.pdf tarea8.pdf tarea9.pdf tarea10.pdf tarea11.xls tarea12.xls tarea13.xls tarea14.xls tarea15.xls
+cd ..
+cd Video
+touch antman.mp4 avengers.mp4 thor.mp4 ironman.mp4 hulk.mp4 captainamerica.mp4 ageofultron.mp4 infinitywar.mp4 endgame.mp4 blackpanther.mp4 spiderman.mp4 civilwar.mp4 ragnarok.mp4 wintersoldier.mp4 drstrange.mp4
+cd ..
+cd Music
+mkdir caifanes jaguares fobia sabaton disturbed atreyu greenday linkinpark paparoach limpbizkit korn metallica mudvayne heroesdelsilencio panda ozzy malditavecindad cuca cafetacvba sodastereo
+ls
+cd atreyu
+tocuch lipglossandblack.mp3 rightsideofthebed.mp3 oursickstory.mp3 whentwoareone.mp3 blow.mp3
+touch lipglossandone.mp3 rightsideofthebed.mp3 oursickstory.mp3 whentwoareone.mp3 blow.mp3
+cd ..
+cd cuca 
+touch elsondeldolor.mp3 elmamondelapistola.mp3 labalada.mp3 señoritacaradepizza.mp3 implacable.mp3
+cd ..
+cd greenday
+touch waiting.mp3 warning.mp3 minority.mp3 holiday.mp3 americanidiot.mp3
+cd .. 
+cd korn
+touch adidas.mp3 twist.mp3 makemebad.mp3 gotthelife.mp3 thougtless.mp3
+cd ..
+cd malditavecindad
+touch morenaza.mp3 pachuco.mp3 solin.mp3 mojado.mp3 kumbala.mp3 
+cd ..
+cd ozzy
+touch crazytrain.mp3 mrcrowley.mp3 ironman.mp3 paranoid.mp3 dreamer.mp3 
+cd ..
+cd sabaton
+touch panzerkampf.mp3 nightwitches.mp3 whitedeath.mp3 carolusrex.mp3 lionfromthenorth.mp3
+cd ..
+cd cafetacvba
+touch eres.mp3 ingrata.mp3 esanoche.mp3 maria.mp3 lasflores.mp3
+cd ..
+cd disturbed
+touch stupify.mp3 imalive.mp3 thenight.mp3 serpentine.mp3 stricken.mp3
+cd ..
+cd heroesdelsilencio
+touch avalancha.mp3 bendecida.mp3 tesoro.mp3 agosto.mp3 nuestrosnombres.mp3
+cd ..
+cd limpbizkit
+touch rearrenged.mp3 nookie.mp3 9teen99.mp3 myway.mp3 faith.mp3
+cd ..
+cd metallica
+touch unforgiven.mp3 unforgiven2.mp3 unforgiven3.mp3 untilitsleeps.mp3 batery.mp3 
+cd ..
+cd panda
+touch pathetica.mp3 nuestraafliccion.mp3 usted.mp3 ilasha.mp3 muñeca.mp3
+cd ..
+cd sodastereo
+touch nadapersonal.mp3 demusicaligera.mp3 laciudaddelafuria.mp3 persianaamericana.mp3 signos.mp3
+cd ..
+cd caifanes
+touch amanece.mp3 miedo.mp3 avientame.mp3 tortuga.mp3 mariquita.mp3
+cd ..
+cd fobia
+touch microbito.mp3 venenovil.mp3 camila.mp3 hipnotizame.mp3 elcrucifijo.mp3
+cd ..
+cd jaguares
+touch huracan.mp3 imaginame.mp3 parpadea.mp3 adios.mp3 derritete.mp3
+cd ..
+cd linkinpark
+touch numb.mp3 papercut.mp3 whativedone.mp3 crawling.mp3 newdivide.mp3
+cd ..
+cd mudvayne
+touch dig.mp3 deathblooms.mp3 happy.mp3 notfalling.mp3 forgettoremember.mp3
+cd ..
+cd paparoach
+touch infest.mp3 lastresort.mp3 binge.mp3 revenge.mp3 snakes.mp3
+cd ..
+cd user1
+copy /* /C/Users/user1
+cp /* /C/Users/user1
+cp /* C/Users/user1
+cp /* /C/Users/user2
+cp /* C/Users/user2
+cd ..
+cp /user1/* /C/Users/user2
+ls
+cd user
+cd user1
+cp /* /C/Users/user2
+cp -r user1/ C/Users/user2
+cp -r /* /C/Users/user2
+cd ..
+cp -r user1/ C/Users/user2
+cp -r user1/ C/Users/user2/
+cp -r C/Users/user1/ C/Users/user2/
+cp -r -a user1/ /C/Users/user2
+cd user1
+cp -r /* /C/Users/user2
+cp -r /* /C/Users/user2/
+cd ..
+ls
+cd user1
+ls
+cp -r user1/ C/Users/user2
+cd ..
+cp -r user1/ C/Users/user2
+cp -r user1/ Users/user2
+cp -r user1/ user2
+cp -r user1/ user3
+cd user3
+ls
+rm -f user1
+rm -f -r user1
+rm -r user1
+cd ..
+cd user2
+ls
+rm -r user2/user1
+rmdir -r user1
+rm -R user1
+ls
+cd ..
+cd user3 
+rm -R user1
+ls
+cd ..
+cd user1
+ls
+cp Desktop/ Documents/ Downloads/ Music/ Pictures/ Video/ bash/ git/ tarea/ user2
+cp Desktop/ Documents/ Downloads/ Music/ Pictures/ Video/ bash/ git/ tarea/ C/Users/user2
+cp Desktop/ ../user2
+cp -r Desktop ../user2
+cd ..
+cd user2
+ls
+cd ..
+cd user1
+cd ..
+cd user2
+ls
+cd Desktop
+ls
+cd ..
+cd user1
+cp -r Documents/ Downloads/ Music/ Pictures/ Video/ bash/ git/ tarea/ ../user2
+cp -r Desktop/ Documents/ Downloads/ Pictures/ Video/ bash/ git/ tarea/ ..user3
+cd -r Desktop/ Documents/ Downloads/ Pictures/ Video/ bash/ git/ tarea/ ../user3
+cp -r Desktop/ Documents/ Downloads/ Pictures/ Video/ bash/ git/ tarea/ ../user3
+cd ..
+cd user3
+ls
+cd ..
+cd user1
+cp -r Music/ ../user3
+cd ..
+cd user2
+ls
+cd ..
+cd user3
+ls
+cd music
+cd Music
+ls
+cd ..
+history > exercise.txt
