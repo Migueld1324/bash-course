e59d0f71688ef00ce1694e7cd502b9428dba4406
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 17:19:33 -0500

    Added Hector's exercise

---
+++ Hector/hector-bash-exercise1/hector-bash-exercise1.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+
+clear
+mkdir C
+cd C
+mkdir Programs
+mkdir Windows
+mkdir Users
+cd Programs/
+mkdir chrome
+mkdir ares
+mkdir spotify
+mkdir word
+mkdir excel
+mkdir powerpoint
+mkdir access
+mkdir edge
+mkdir calculator
+mkdir calendar
+cd chrome
+touch file1
+touch file2
+ls
+cd ..
+cp chrome/file1 ares/
+cp chrome/file2 ares/
+cp chrome/file1 spotify/
+cp chrome/file2 spotify/
+cp chrome/file1 word/
+cp chrome/file2 word/
+cp chrome/file1 excel/
+cp chrome/file2 excel/
+cp chrome/file1 powerpoint/
+cp chrome/file2 powerpoint/
+cp chrome/file1 access/
+cp chrome/file2 access/
+cp chrome/file1 edge/
+cp chrome/file2 edge/
+cp chrome/file1 calculator/
+cp chrome/file2 calculator/
+cp chrome/file1 calendar/
+cp chrome/file2 calendar/
+cd edge/
+ls
+cd ..
+mkdir debug
+rm -r debug/
+cd ..
+cd Windows/
+mkdir debug
+mkdir app
+mkdir boot
+mkdir diagnostics
+mkdir help
+cd debug/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10
+cd ..
+cd app/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10
+cd ..
+cd boot/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10
+cd ..
+cd diagnostics/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10
+cd ..
+cd help/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10
+ls
+cd ..
+mkdir System32
+cd System32/
+touch file1 file2 file3 file4 file5 file6 file7 file8 file9 file10 file11 file12 file13 file14 file15
+ls
+cp -r ../debug .
+ls
+cd debug/
+ls
+cd ..
+cp -r ../app .
+cp -r ../boot .
+cp -r ../diagnostics .
+cp -r ../help .
+ls
+cd help/
+ls
+cd ../../../
+cd Users/
+mdkir User1
+mkdir User1
+mkdir User2
+mkdir User3
+cd User1
+mkdir Desktop
+mkdir Documents
+mkdir Downloads
+mkdir Music
+mkdir Pictures
+mkdir Videos
+mkdir Games
+mkdir BTS
+mkdir Extra
+cd Desktop/
+touch Chrome RecycleBin Steam Blizzard Postman Firefox Office Drive Ubuntu Paint AdobeReader Photoshop AdobeXD Sketch Spotify 
+cd ..
+cd Documents/
+touch tesis.docx tesis-final.docx tesis-este-si.docx tesis-ahora-si-ya.docx tesis-final-final.docx tesis-ultimate-edition.docx tesis-entrega-final.docx tesis-no-se-controlar-versiones.docx recetas-abuela.docx presupuesto.xlsx horarios.xlsx registro-ocio.xlsx registro-aprendizaje.xlsx pro-git.pdf pragmatic-programmer.pdf
+ls
+cd ..
+cd Downloads/
+touch chrome.exe cmder.exe spotify.zip tesis.pdf foto.png slack.exe filezilla.zip foto2.jpg foto3.jpg foto4.jpg feedback-asesor.docx index.html firacode.ttf xampp.zip node-v-10.zip
+ls
+cd ..
+cd Music
+mkdir Five_Finger_Death_Punch
+mkdir Avenged_Sevenfold
+mkdir Linkin_Park
+mkdir Luis_Miguel
+mkdir Mijares
+mkdir Daft_Punk
+mkdir TWRP
+mkdir Ninja_Sex_Party
+mkdir Pepe_Aguilar
+mkdir Steve_Aoki
+mkdir Queen
+mkdir AC_DC
+mkdir Franz_Ferdinand
+mkdir Toto
+mkdir Eminem
+mkdir Rio_Roma
+mkdir Dragonforce
+mkdir Romeo_Santos
+mkdir Skillet
+mkdir Fall_Out_Boy
+cd Five_Finger_Death_Punch/
+touch wrong_side_of_heaven.mp3 bad_company.mp3 wash_it_all_away.mp3 under_and_over_it.mp3 lift_me_up.mp3
+ls
+cd ..
+cd Avenged_Sevenfold/
+touch dear_god.mp3 afterlife.mp3 bat_country.mp3 hail_to_the_king.mp3 buried_alive.mp3
+cd ..
+cd Linkin_Park/
+touch numb.mp3 in_the_end.mp3 breaking_the_habit.mp3 new_divide.mp3 castle_of_glass.mp3
+ls
+cd ..
+cd Luis_Miguel/
+touch la_incondicional.mp3 entregate.mp3 ahora_te_puedes_marchar.mp3 la_bikina.mp3 culpable_o_no.mp3
+ls
+cd ..
+cd Mijares/
+touch para_amarnos_mas.mp3 soldado_del_amor.mp3 bella.mp3 si_me_tenias.mp3 no_hace_falta.mp3
+cd ..
+cd Daft_Punk/
+touch get_lucky.mp3 one_more_time.mp3 around_the_world.mp3 instant_crush.mp3 lose_yourself_to_dance.mp3
+ls
+cd ..
+cd TWRP/
+touch atomic_karate.mp3 the_hit.mp3 starlight_brigade.mp3 phantom_racer.mp3 groove_crusaders.mp3
+ls
+cd ..
+ls
+cd Ninja_Sex_Party/
+touch if_we_were_gay.mp3 cool_patrol.mp3 danny_dont_you_know.mp3 unicorn_wizard.mp3 no_reason_boner.mp3
+ls
+cd ..
+cd Pepe_Aguilar/
+touch prometiste.mp3 por_mujeres_como_tu.mp3 directo_al_corazon.mp3 con_otro_sabor.mp3 cielo_rojo.mp3
+ls
+cd ..
+cd Steve_Aoki/
+touch are_you_lonely.mp3 waste_it_on_me.mp3 azukita.mp3 delirious.mp3 no_beef.mp3
+ls
+cd ../Queen/
+touch bohemian_rhapsody.mp3 radio_ga_ga.mp3 i_want_it_all.mp3 somebody_to_love.mp3 killer_queen.mp3
+ls
+cd ../AC_DC/
+touch highway_to_hell.mp3 thunderstruck.mp3 hells_bells.mp3 tnt.mp3 moneytalks.mp3
+ls
+cd ../Franz_Ferdinand/
+touch take_me_out.mp3 do_you_want_to.mp3 walk_away.mp3 this_fire.mp3 no_you_girls.mp3
+ls
+cd ../Toto/
+touch africa.mp3 hold_the_line.mp3 99.mp3 rosanna.mp3 pamela.mp3
+ls
+cd ../Eminem/
+touch love_the_way_you_lie.mp3 rap_god.mp3 lose_yourself.mp3 not_afraid.mp3 venom.mp3
+ls
+cd ../Rio_Roma/
+touch me_cambiaste_la_vida.mp3 mi_persona_favorita.mp3 por_eso_te_amo.mp3 contigo.mp3 princesa.mp3
+ls
+cd ../Dragonforce/
+touch cry_thunder.mp3 the_game.mp3 fury_of_the_storm.mp3 heroes_of_our_time.mp3 through_the_fire_and_the_flames.mp3
+ls
+cd ../Romeo_Santos/
+touch aullando.mp3 you.mp3 eres_mia.mp3 ella_y_yo.mp3 imitadora.mp3
+ls
+cd ../Skillet/
+touch monster.mp3 hero.mp3 feel_invincible.mp3 awake_and_alive.mp3 not_gonna_die.mp3
+ls
+cd ../Fall_Out_Boy/
+touch centuries.mp3 the_phoenix.mp3 dance_dance.mp3 uma_thurman.mp3 immortals.mp3
+ls
+cd ../../
+ls
+cd Pictures/
+touch foto1.jpg foto2.jpg foto3.jpg foto4.jpg foto5.jpg foto6.jpg foto7.jpg foto8.jpg foto9.jpg foto10.jpg foto11.jpg foto12.jpg foto13.jpg foto14.jpg foto15.jpg
+ls
+cd ../Videos/
+touch pulp_fiction.mp4 the_thing.mp4 the_avengers.mp4 reservoir_dogs.mp4 django.mp4 kill_bill.mp4 inglorious_bastards.mp4 the_hateful_eight.mp4 the_godfather.mp4 the_birds.mp4 vertigo.mp4 v_for_vendetta.mp4 sin_city.mp4 citizen_kane.mp4 godzilla_2.mp4
+ls
+cd ../Games/
+touch age_of_empires.sav skyrim.sav civilization_v.sav hollow_knight.sav grand_theft_auto.sav transistor.sav witcher.sav halo.sav gears_of_war.sav smash.sav pokemon.sav legend_of_zelda.sav donkey_kong.sav super_mario_64.sav call_of_duty.sav
+ls
+cd ../Extra/
+mv ../Extra/ ../Books
+cd ..
+ls
+cd Books/
+touch ulysses.pdf night_circus.pdf american_gods.pdf 1984.pdf pride_and_prejudice.pdf the_great_gatsby.pdf moby_dick.pdf war_of_the_worlds.pdf the_hobbit.pdf crime_and_punishment.pdf dracula.pdf fahrenheit_451.pdf tale_of_two_cities.pdf emma.pdf little_woman.pdf
+ls
+touch project1.js project2.js project3.js project4.vue project5.html project6.css project7.php project8.php project9.html project10.js project11.vue project12.js project13.scss project14.php project15.js
+ls
+mv project1.js ../BTS/
+ls
+mv project2.js ../BTS/
+mv project3.js ../BTS/
+mv project4.vue ../BTS/
+mv project5.html ../BTS/
+mv project6.css ../BTS/
+mv project7.php ../BTS/
+mv project8.php ../BTS/
+mv project9.html ../BTS/
+mv project10.js ../BTS/
+mv project11.vue ../BTS/
+mv project12.js ../BTS/
+mv project13.scss ../BTS/
+mv project14.php ../BTS/
+mv project15.js ../BTS/
+ls
+cd ..
+cd BTS/
+ls
+cd ..
+cp -r Documents ../User2/
+cp -r Desktop ../User2/
+cp -r Downloads ../User2/
+cp -r Pictures ../User2/
+cp -r Videos ../User2/
+cp -r Music ../User2/
+cp -r Games ../User2/
+cp -r Books ../User2/
+cp -r BTS ../User2/
+cp -r Documents ../User3/
+cp -r Desktop ../User3/
+cp -r Downloads ../User3/
+cp -r Pictures ../User3/
+cp -r Videos ../User3/
+cp -r Music ../User3/
+cp -r Games ../User3/
+cp -r Books ../User3/
+cp -r BTS ../User3/
+cd ..
+cd .
+cd ..
+cat history > exercise_1_history.txt
+history > exercise_1_history.txt
