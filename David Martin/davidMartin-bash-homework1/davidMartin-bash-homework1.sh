f9497c7ab00a09a4907dbc82e322d50e1f977126
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 13:05:45 -0500

    Added David's homework1

---
+++ David Martin/davidMartin-bash-homework1/davidMartin-bash-homework1.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+
+# clear
+# cd /
+# ls
+# cd mnt/c/Users/silva/Desktop/cmder/C/Programs
+# ls
+# rm -r folder6 folder7 folder8 folder9 folder10
+ls
+cd C
+cd Windows
+rm -r folder5
+cd folder1
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder2
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder3
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder4
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd Systems32
+rm -r file6 file7 file8 file9 file10 file11 file12 file13 file14 file15
+cd folder1
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder2
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder3
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder4
+rm -r file6 file7 file8 file9 file10
+cd ..
+cd folder5
+rm -r file6 file7 file8 file9 file10
+cd ../../../
+cd Programs
+cd folder1
+# cat > file1abc
+# rm -r fileabc
+# rm -r file1abc
+# cat > file1
+# cat file1 >> file2
+cd ..
+cp -r folder1/. folder2/
+cp -r folder1/. folder3/
+cp -r folder1/. folder4/
+cp -r folder1/. folder5/
+cd ../
+cd Windows
+cd folder1
+# cat > file1
+# cat file1 >> file2
+# cat file1 >> file3
+# cat file1 >> file4
+# cat file1 >> file5
+cd ..
+cp -r folder1/. folder2/
+cp -r folder1/. folder3/
+cp -r folder1/. folder4/
+cp -r folder1/. Systems32/folder1/
+cp -r folder1/. Systems32/folder2/
+cp -r folder1/. Systems32/folder3/
+cp -r folder1/. Systems32/folder4/
+cp -r folder1/. Systems32/folder5/
+cd Systems32
+# cat > file1
+# cat file1 >> file2
+# cat file1 >> file3
+# cat file1 >> file4
+# cat file1 >> file5
+cd ../../
+cd Users
+cd User1
+rm -r Books Comics Videogames Videos
+ls
+cd ..
+cp -r User1/. User2/
+cp -r User1/. User3/
+cd User2
+rm -r Books Comics Videogames Videos
+cd ..
+cd User3
+rm -r Books Comics Videogames Videos
+cd ..
+cd User1
+cd Desktop
+rm -r desktop6 desktop7 desktop8 desktop9 desktop10
+cd ..
+cd Documents
+rm -r document6 document7 document8 document10 document11 document12 document13 document14 document15
+cd ..
+cd Desktop/
+rm -r desktop11 desktop12 desktop13 desktop14 desktop15
+cd ..
+cd Downloads
+rm -r download6 download7 download8 download9 download10 download11 download12 download13 download14 download15
+cd ..
+cd Pictures
+rm -r picture6 picture7 picture8 picture9 picture10 picture11 picture12 picture13 picture14 picture15
+cd ..
+cd Desktop
+# cat > desktop1
+# cat desktop1 >> desktop2
+# cat desktop1 >> desktop3
+# cat desktop1 >> desktop4
+# cat desktop1 >> desktop5
+cd ..
+cd Documents
+# cat > document1
+# cat document1 > document2
+# cat document1 > document3
+# cat document1 > document4
+# cat document1 > document5
+cd ..
+cd Downloads
+# cat > download1
+# cat download1 >> download2
+# cat download1 >> download3
+# cat download1 >> download4
+# cat download1 >> download5
+cd ..
+cd Pictures
+# cat picture1 > picture2
+# cat > picture1
+# cat picture1 >> picture2
+# cat picture1 >> picture3
+# cat picture1 >> picture4
+# cat picture1 >> picture5
+cd ..
+cd Music
+cd Artist1
+touch song6 song7 song8 song9 song10
+cd ..
+cp -r Artist1/. Artist2/
+cp -r Artist1/. Artist3/
+cp -r Artist1/. Artist4/
+cp -r Artist1/. Artist5/
+cp -r Artist1/. Artist6/
+cp -r Artist1/. Artist7/
+cp -r Artist1/. Artist8/
+cp -r Artist1/. Artist9/
+cp -r Artist1/. Artist10/
+cd ../../
+rm -r User2 User3
+cp -r User1/. User2/
+cp -r User1/. User3/
+cd ..
+cd Users
+cd User1
+cd Music
+cd Artist1
+rm -r song*
+cd ..
+cd Artist2
+rm -r song*
+cd ..
+cd Artist3
+rm -r song*
+cd ..
+cd Artist4
+rm -r song*
+cd ..
+cd Artist5
+rm -r song*
+cd ..
+cd Artist6
+rm -r song*
+cd ..
+cd Artist7
+rm -r song*
+cd ..
+cd Artist8
+rm -r song*
+cd ..
+cd Artist9
+rm -r song*
+cd ..
+cd Artist10
+rm -r song*
+cd ..
+ls
+cd Artist1
+touch TheBeatles-
+touch TheBeatles-DigIt
+touch TheBeatles-DontLetMeDown
+touch TheBeatles-GetBack
+touch TheBeatles-IveGotAFeeling
+touch TheBeatles-LetItBe
+touch TheBeatles-MaxwellsSilverHammer
+touch TheBeatles-SheCameInThroughTheBathroomWindow
+touch TheBeatles-ShortFatFannie
+touch TheBeatles-StandByMe
+mv TheBeatles- TheBeatles-AllThingsMustPass
+cd ..
+cd Artist2
+touch TheBeatles-
+touch TheBeatles-TwoOfUs
+touch TheBeatles-Woman
+touch TheBeatles-PaperbackWriter
+touch TheBeatles-Rain
+touch TheBeatles-AnnaGoToHim
+touch TheBeatles-AskMeWhy
+touch TheBeatles-BabyItsYou
+touch TheBeatles-Boys
+touch TheBeatles-Chains
+# mv TheBeatles- TheBeatles-ThreeCoolCats
+cd ..
+cd Artist3
+touch TheBeatles-
+touch TheBeatles-ISawHerStandingThere
+touch TheBeatles-LoveMeDo
+touch TheBeatles-Misery
+touch TheBeatles-PSILoveYou
+touch TheBeatles-PleasePleaseMe
+touch TheBeatles-ATasteOfHoney
+touch TheBeatles-TheresAPlace
+touch TheBeatles-CantBuyMeLove
+touch TheBeatles-IShouldHaveKnownBetter
+mv TheBeatles- TheBeatles-DoYouWantToKnowASecret
+cd ..
+cd Artist4
+touch TheBeatles-
+touch TheBeatles-WhenIGetHome
+touch TheBeatles-YouCantDoThat
+touch TheBeatles-AndILoveHer
+touch TheBeatles-IfIFell
+touch TheBeatles-NobodysChild
+touch TheBeatles-Why
+touch TheBeatles-EveryTryingToBeMyBaby
+touch TheBeatles-HoneyDont
+touch TheBeatles-IFeelFine
+mv TheBeatles- TheBeatles-TellMeWhy
+cd ..
+cd Artist5
+touch TheBeatles-
+touch TheBeatles-AllMyLove
+touch TheBeatles-DontBotherMe
+touch TheBeatles-HoldMeTight
+touch TheBeatles-IWannaBeYourMan
+touch TheBeatles-LittleChild
+touch TheBeatles-AndILoverHer
+touch TheBeatles-AnyTimeAtAll
+touch TheBeatles-ImHappyJustToDanceWithYou
+touch TheBeatles-KommGibMirDeineHand
+mv TheBeatles- TheBeatles-NoReply
+cd ..
+cd Artist6
+touch TheBeatles-
+touch TheBeatles-IllCryInstead
+touch TheBeatles-SlowDown
+touch TheBeatles-TellMeWhy
+touch TheBeatles-WhenIGetHome
+touch TheBeatles-PleaseMisterPostman
+touch TheBeatles-SheLovesYou
+touch TheBeatles-YouveReallyGotAHoldOnMe
+touch TheBeatles-DizzyMissLizzy
+touch TheBeatles-EihgtDaysAWeek
+mv TheBeatles- TheBeatles-ThingsWeSaidToday
+cd .. 
+cd Artist7
+touch TheBeatles-IDontWantToSpoilTheParty
+touch TheBeatles-TellMeWhatYouSee
+touch TheBeatles-WhatYoureDoing
+touch TheBeatles-AnotherGirl
+touch TheBeatles-INeedYou
+touch TheBeatles-TheNightBefore
+touch TheBeatles-YouveGotToHideYourLoveAway
+touch TheBeatles-DriveMyCar
+touch TheBeatles-Girl
+touch TheBeatles-ImLookingThroughYou
+cd ..
+cd Artist8
+touch TheBeatles-IfINeededSomeone
+touch TheBeatles-InMyLife
+touch TheBeatles-Michelle
+touch TheBeatles-NowhereMan
+touch TheBeatles-ThinkForYourself
+touch TheBeatles-Wait
+touch TheBeatles-WhatGoesOn
+touch TheBeatles-TheWord
+touch TheBeatles-YouWontSeeMe
+touch TheBeatles-FromeMeToYou
+cd ..
+cd Artist9
+touch TheBeatles-AndYourBirdCanSing
+touch TheBeatles-DoctorRobert
+touch TheBeatles-EleanorRigby
+touch TheBeatles-GoodDaySunshine
+touch TheBeatles-GotToGetYouIntoMyLife
+touch TheBeatles-IWantToTellYou
+touch 
+touch TheBeatles-SheSaidSheSaid
+touch TheBeatles-Taxman
+touch TheBeatles-YellowSubmarine
+cd ..
+cd Artist10
+touch TheBeatles-Because
+touch TheBeatles-CarryThatWeight
+touch TheBeatles-ComeTogether
+touch TheBeatles-TheEnd
+touch TheBeatles-GoldenSlumbers
+touch TheBeatles-HerMajesty
+touch TheBeatles-HereComesTheSun
+touch TheBeatles-MaxwellsSilverHammer
+touch TheBeatles-OctopusGarden
+touch TheBeatles-OhDarling
+cd ..
+rm -r User2 User3
+cp -r User1/. User2/
+cp -r User1/. User3/
+history -w davidMartin-bash-homework1
