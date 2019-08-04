72887f6378a1fb172c582c43927c65efc01d973c
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 23:03:51 -0500

    Added Luis Carlos' exercise

---
+++ Luis Carlos/luiscarlos-bash-exercise/luiscarlos-bash-exercise1.sh
@@ -0,0 +1,391 @@
+#!/bin/bash
+
+mkdir C
+cd C
+mkdir Programs
+mkdir Windows
+mkdir Users
+cd Programs
+mkdir Eclipse
+cd Eclipse
+touch README.txt
+touch Unistall.exe
+cd ../
+cp -r Eclipse AndroidStudio
+cp -r Eclipse Skype
+cp -r Eclipse NetBeans
+cp -r Eclipse Chrome
+cp -r Eclipse Firefox
+cp -r Eclipse Safari
+cp -r Eclipse Opera
+cp -r Eclipse Office
+cp -r Eclipse VisualStudio
+cd ../
+cd Windows/
+mkdir Help
+cd Help
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch file4.txt
+touch file5.txt
+touch file6.txt
+touch file7.txt
+touch file8.txt
+touch file9.txt
+touch file10.txt
+cd ..
+cp -r Help/ Fonts
+cp -r Help/ IME
+cp -r Help/ INF
+cp -r Help/ logs
+cd ../
+cd Windows/
+mkdir System32
+cd System32/
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch file4.txt
+touch file5.txt
+touch file6.txt
+touch file7.txt
+touch file8.txt
+touch file9.txt
+touch file10.txt
+touch file11.txt
+touch file12.txt
+touch file13.txt
+touch file14.txt
+touch file15.txt
+mkdir carpeta1
+cd carpeta1/
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch file4.txt
+touch file5.txt
+touch file6.txt
+touch file7.txt
+touch file8.txt
+touch file9.txt
+touch file10.txt
+touch file11.txt
+touch file12.txt
+touch file13.txt
+cd ../
+cp -r carpeta1/ carpeta2
+cp -r carpeta1/ carpeta3
+cp -r carpeta1/ carpeta4
+cp -r carpeta1/ carpeta5
+cd ../../
+cd Users/
+mkdir Luis
+mkdir Miguel
+mkdir Invitados
+cd Luis/
+mkdir Desktop
+mkdir Documents
+mkdir Downloads
+mkdir Music
+mkdir Pictures
+mkdir Videos
+mkdir Juegos
+mkdir Vinculos
+mkdir Favoritos
+cd Desktop
+touch Chrome.Ink
+touch Firefox.Ink
+touch Music.Ink
+touch Pictures.Ink
+touch Documents.Ink
+touch Visual.Ink
+touch Skype.Ink
+touch Tareas.Ink
+touch Safari.Ink
+touch Word.Ink
+touch Paint.Ink
+touch Explorer.Ink
+touch Juegos.Ink
+touch Papelera.Ink
+touch Shop.Ink
+cd ..
+cd Documents/
+touch tarea.doc
+touch tabla.xsl
+touch precios.xsl
+touch proyecto.doc
+touch notas.txt
+touch ensayo.doc
+touch mates.xsl
+touch receta.txt
+touch archivos.rar
+touch proyectofinal.doc
+touch tareaciencias.doc
+touch exposicion.ppt
+touch exposicion-final.ppt
+touch porcentaje.xsl
+touch catalogo.xsl
+cd ../
+cd Downloads/
+touch pelicula.rar
+touch meme.png
+touch setup.exe
+touch chrome.exe
+touch firefox.exe
+touch eclipse.exe
+touch cmnder.rar
+touch office.rar
+touch photoshop.rar
+touch comprimidos.rar
+touch plantilla.png
+touch anime.rar
+touch manga.rar
+touch serie.rar
+touch utorrent.exe
+cd ../
+cd Pictures/
+touch IMG-001.jpg
+touch IMG-002.jpg
+touch IMG-003.jpg
+touch IMG-004.jpg
+touch IMG-005.jpg
+touch IMG-006.jpg
+touch IMG-007.jpg
+touch IMG-008.jpg
+touch IMG-009.jpg
+touch IMG-0010.jpg
+touch IMG-0011.jpg
+touch IMG-0012.jpg
+touch IMG-0013.jpg
+touch IMG-0014.jpg
+touch IMG-0015.jpg
+cd -
+cd ../
+ls
+cd Luis/
+ls
+cd Videos
+touch caidaedgar.avi
+touch one-piece.avi
+touch naruto.avi
+touch dragon-ball.avi
+touch cumple.avi
+touch despedida.avi
+touch vacaciones.avi
+touch pokemon.avi
+touch digimon.avi
+touch ironman.avi
+touch spiderman.avi
+touch hulk.avi
+touch avengers.avi
+touch thor.avi
+cd ../
+cd Juegos/
+touch buscaminas.exe
+touch solitario.exe
+touch spider.exe
+touch ageofempires.exe
+touch pinball.exe
+touch arcanoi.exe
+touch mario.exe
+touch gba.exe
+touch n64.exe
+touch emuds.exe
+touch candycrush.exe
+touch guitarhero.exe
+touch zelda.exe
+touch mariokart.exe
+touch marioparty.exe
+cd ../
+ls
+cp -r Desktop/ Vinculos
+rmdir Vinculos
+rmdir -r Vinculos
+rm -r Vinculos
+rm -r Favoritos
+cp -r Desktop/ Vinculos
+cp -r Desktop/ Favoritos
+cd Music/
+mkdir LaBrissa
+cd LaBrissa/
+touch Colales.mp3
+touch Palillos-chinos.mp3
+touch las-chiquitas.mp3
+touch magia-blanca.mp3
+touch ojos-cafe.mp3
+cd ../
+mkdir Queen
+cd Queen/
+touch somebody-to-love.mp3
+touch hammer-to-fall.mp3
+touch radio-gaga.mp3
+touch bohemian-raphsody.mp3
+touch another-one-bites-the-dust.mp3
+cd ../
+mkdir ABBA
+cd ABBA
+touch fernando.mp3
+touch chiquitita.mp3
+touch super-truper.mp3
+touch dancing-queen.mp3
+touch mamma-mia.mp3
+cd ../
+cd LaOrejaVanGogh
+mkdir LaOrejaVanGogh
+
+cd LaOrejaVanGogh/
+touch rosas.mp3
+touch viernes.mp3
+touch mu�eca-trapo.mp3
+touch playa.mp3
+touch ultimo-vals.mp3
+cd ../
+cd LaArrolladora
+mkdir LaArrolladora
+cd LaArrolladora/
+touch calabaza.mp3
+touch quede-claro.mp3
+touch otra-cara-moneda.mp3
+touch sinaloense.mp3
+touch popurri.mp3
+cd ../
+mkdir ValentinElizalde
+cd ValentinElizalde/
+touch lobo-domesticado.mp3
+touch papa.mp3
+touch para-que-continuar.mp3
+touch soy-asi.mp3
+touch te-quiero-asi.mp3
+cd ../
+mkdir Beatles
+cd Beatles/
+touch let-it-be.mp3
+touch dont-let-me-down.mp3
+touch hey-jude.mp3
+touch help.mp3
+touch come-together.mp3
+cd ../
+mkdir JuanGabriel
+cd JuanGabriel/
+touch noanoa.mp3
+touch frontera.mp3
+touch querida.mp3
+touch me-haces-llorar.mp3
+touch si-quieres.mp3
+cd ../
+mkdir LuisMiguel
+cd LuisMiguel/
+touch sue�a.mp3
+touch bikina.mp3
+touch incondicional.mp3
+touch te-puedes-marchar.mp3
+touch culpable-o-no.mp3
+cd ../
+mkdir Buki
+cd Buki
+touch amigos.mp3
+touch donde-vamos-a-parar.mp3
+touch tu-carcel.mp3
+touch quiereme.mp3
+touch inventame.mp3
+cd ../
+cd SergioVega
+mkdir SergioVega
+cd SergioVega/
+touch necesito-due�a.mp3
+touch cuando-sol-salga-alreves.mp3
+touch cuando-lavas-tu-cara.mp3
+touch millonario-amor.mp3
+touch numero-20.mp3
+cd ../
+mkdir MartinGarrix
+cd MartinGarrix/
+touch animals.mp3
+touch seven.mp3
+touch ocean.mp3
+touch there-for-you.mp3
+touch scared-to-be-lonely.mp3
+cd ../
+mkdir Recodo
+cd Recodo/
+touch popurri-baladas.mp3
+touch popurri-banda.mp3
+touch popurri-corridos.mp3
+touch popurri-variado.mp3
+touch el-sinaloense.mp3
+cd ../
+mkdir BillyJoel
+cd BillyJoel/
+touch piano-man.mp3
+touch longest-time.mp3
+touch uptown-girl.mp3
+touch inocent-man.mp3
+touch we-didnt-start-the-fire.mp3
+cd ../
+mkdir TheWho
+cd TheWho/
+touch happy-jack.mp3
+touch bara-oriley.mp3
+touch wont-get-fooled-again.mp3
+touch who-are-you.mp3
+touch my.mp3
+cd ../
+mkdir Oasis
+cd Oasis/
+touch wonderwall.mp3
+touch dont-loock-back-together.mp3
+touch stop-crying.mp3
+touch supersonic.mp3
+touch the-hindu.mp3
+cd ../
+mkdir Coldplay
+cd Coldplay/
+touch Viva-la-via.mp3
+touch 'The scientist.mp3'
+touch 'Yellow.mp3'
+touch 'Paradise.mp3'
+touch 'Fix you.mp3'
+cd ../
+mkdir Canserbero
+cd Canserbero/
+touch 'Es epico'
+rm 'Es epico'
+ls
+touch 'Es epico.mp3'
+touch 'Jeremias 17:5.mp3'
+touch 'Jeremias 17 5.mp3'
+touch 'Perdiendo la fe.mp3'
+touch 'Querer querernos.mp3'
+touch 'Stupid love history.mp3'
+ls
+cd ../
+mkdir Jesed
+cd Jesed/
+touch "pidiendo limosna.mp3"
+touch "Noche oscura.mp3"
+touch "Miserere.mp3"
+touch "Desolada.mp3"
+touch "Mi raz�n de ser.mp3"
+cd ../
+mkdir The Credence
+cd The Credence/
+ls
+rm -r {The, Credence}
+rm -r The, Credence
+rm -r The
+ls
+mkdir 'The Credence'
+cd 'The Credence'/
+touch 'Have you ever seen the rain.mp3'
+touch 'Run trough the jungle.mp3'
+touch 'Cottonfields.mp3'
+touch 'Proud Mary.mp3'
+touch 'Jambalaya.mp3'
+cd ../../../../
+ls
+rm -r Invitados Miguel
+ls
+cp -r Luis/. Invitados
+cp -r Luis/. Miguel
+history > luiscarlos-bash-exercise
