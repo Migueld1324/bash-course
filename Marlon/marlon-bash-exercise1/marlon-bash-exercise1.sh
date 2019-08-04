b1b6efef0e3e7dab140bf97e2abdda3cd7fb59b8
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-03 23:18:28 -0500

    Added Marlon's exercise

---
+++ Marlon/marlon-bash-exercise1/marlon-bash-exercise1.sh
@@ -0,0 +1,424 @@
+#!/bin/bash
+
+history
+mkdir C:
+cd C\:/
+mkdir Programs Windows Users
+cd Programs
+mkdir Program0
+mkdir Program1
+mkdir Program2
+mkdir Program3
+mkdir Program4
+mkdir Program5
+mkdir Program6
+mkdir Program7
+mkdir Program8
+mkdir Program9
+touch Program0/Program0.exe Program0/uninstall_Program0.exe
+touch Program1/Program1.exe Program1/uninstall_Program1.exe
+touch Program2/Program2.exe Program2/uninstall_Program2.exe
+touch Program3/Program3.exe Program3/uninstall_Program3.exe
+touch Program4/Program4.exe Program4/uninstall_Program4.exe
+touch Program5/Program5.exe Program5/uninstall_Program5.exe
+touch Program6/Program6.exe Program6/uninstall_Program6.exe
+touch Program7/Program7.exe Program7/uninstall_Program7.exe
+touch Program8/Program8.exe Program8/uninstall_Program8.exe
+touch Program9/Program9.exe Program9/uninstall_Program9.exe
+ls -R
+cd ..
+ls
+cd Windows/
+mkdir Folder0
+mkdir Folder1
+mkdir Folder2
+mkdir Folder3
+mkdir Folder4
+mkdir Folder5
+mkdir Folder6
+mkdir Folder7
+mkdir Folder8
+mkdir Folder9
+rm Folder5
+rmdir Folder5
+rmdir Folder6
+rmdir Folder7
+rmdir Folder8
+rmdir Folder9
+ls
+cd Folder0
+touch archive0.txt
+touch archive1.txt
+touch archive2.txt
+touch archive3.txt
+touch archive4.txt
+touch archive5.txt
+touch archive6.txt
+touch archive7.txt
+touch archive8.txt
+touch archive9.txt
+cp archive0.txt archive1.txt archive2.txt archive3.txt archive4.txt archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt ../Folder1
+cp archive0.txt archive1.txt archive2.txt archive3.txt archive4.txt archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt ../Folder2
+cp archive0.txt archive1.txt archive2.txt archive3.txt archive4.txt archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt ../Folder3
+cp archive0.txt archive1.txt archive2.txt archive3.txt archive4.txt archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt ../Folder4
+cd ..
+ls -R
+mkdir System32
+ls
+cd System32/
+touch archive00.dll
+touch archive01.dll
+touch archive02.dll
+touch archive03.dll
+touch archive04.dll
+touch archive05.dll
+touch archive06.dll
+touch archive07.dll
+touch archive08.dll
+touch archive09.dll
+touch archive10.dll
+touch archive11.dll
+touch archive12.dll
+touch archive13.dll
+touch archive14.dll
+mkdir Folder1
+mkdir Folder2
+mkdir Folder3
+mkdir Folder4
+mkdir Folder5
+cd Folder1
+touch archive0.dll
+touch archive2.dll
+touch archive1.dll
+touch archive3.dll
+touch archive4.dll
+touch archive5.dll
+touch archive6.dll
+touch archive7.dll
+touch archive8.dll
+touch archive9.dll
+cp archive0.dll archive1.dll archive2.dll archive3.dll archive4.dll archive5.dll archive6.dll archive7.dll archive8.dll archive9.dll ../Folder2
+cp archive0.dll archive1.dll archive2.dll archive3.dll archive4.dll archive5.dll archive6.dll archive7.dll archive8.dll archive9.dll ../Folder3
+cp archive0.dll archive1.dll archive2.dll archive3.dll archive4.dll archive5.dll archive6.dll archive7.dll archive8.dll archive9.dll ../Folder4
+cp archive0.dll archive1.dll archive2.dll archive3.dll archive4.dll archive5.dll archive6.dll archive7.dll archive8.dll archive9.dll ../Folder5
+cd ..
+ls -R
+cd ../../
+cd Users/
+mkdir Marlon Miriam Jorge
+ls
+cd Marlon
+mkdir Desktop Documents Downloads Music Pictures Videos Folder1 Folder2 Folder3
+cd Desktop
+touch Program0.lnk
+touch Program1.lnk
+touch Program2.lnk
+touch Program3.lnk
+touch Program4.lnk
+touch Program5.lnk
+touch Program6.lnk
+touch Program7.lnk
+touch Program8.lnk
+touch Program9.lnk
+touch codeExample1.txt
+touch codeExample2.txt
+touch codeExample3.txt
+touch codeExample4.txt
+touch codeExample5.txt
+cd ..
+ls
+cd Documents
+touch report0.docx
+touch report1.docx
+touch report2.docx
+touch report3.docx
+touch report4.docx
+touch report5.docx
+touch report6.docx
+touch report7.docx
+touch report8.docx
+touch report9.docx
+touch report0.pdf
+touch report1.pdf
+touch report2.pdf
+touch report3.pdf
+touch report4.pdf
+cd ..
+ls
+cd Downloads/
+touch Program0.zip
+touch Program1.zip
+touch Program2.zip
+touch Program3.zip
+touch Program4.zip
+touch Program5.zip
+touch Program6.zip
+touch Program7.zip
+touch Program8.zip
+touch Program9.zip
+touch Instalador1.exe
+touch Instalador2.exe
+touch Instalador3.exe
+touch Instalador4.exe
+touch Instalador5.exe
+cd ..
+cd Pictures/
+touch Wallpaper0.jpg
+touch Wallpaper1.jpg
+touch Wallpaper2.jpg
+touch Wallpaper3.jpg
+touch Wallpaper4.jpg
+touch Wallpaper5.jpg
+touch Wallpaper6.jpg
+touch Wallpaper7.jpg
+touch Wallpaper8.jpg
+touch Wallpaper9.jpg
+touch Animation1.gif
+touch Animation2.gif
+touch Animation3.gif
+touch Animation4.gif
+touch Animation5.gif
+cd ..
+cd Pictures
+touch Movie0.mp4
+touch Movie1.mp4
+touch Movie2.mp4
+touch Movie3.mp4
+touch Movie4.mp4
+touch Movie5.mp4
+touch Movie6.mp4
+touch Movie7.mp4
+touch Movie8.mp4
+touch Movie9.mp4
+touch Movie01.avi
+touch Movie02.avi
+touch Movie03.avi
+touch Movie04.avi
+touch Movie05.avi
+cd ..
+ls
+cd Folder1
+touch archive01.txt
+touch archive02.txt
+touch archive03.txt
+touch archive04.txt
+touch archive05.txt
+touch archive06.txt
+touch archive07.txt
+touch archive08.txt
+touch archive09.txt
+touch archive10.txt
+touch archive11.txt
+touch archive12.txt
+touch archive13.txt
+touch archive14.txt
+touch archive15.txt
+cp archive01.txt archive02.txt archive03.txt archive04.txt archive05.txt archive06.txt archive07.txt archivo08.txt archivo09.txt archivo10.txt archivo11.txt archivo12.txt archivo13.txt archivo14.txt archivo15.txt ../Folder2 
+cp archive08.txt archive09.txt archive10.txt archive11.txt archive12.txt archive13.txt archive14.txt archive15.txt ../Folder2 
+cp archive01.txt archive02.txt archive03.txt archive04.txt archive05.txt archive06.txt archive07.txt archive08.txt archive09.txt archive10.txt archive11.txt archive12.txt archive13.txt archive14.txt archive15.txt ../Folder3 
+cd ..
+ls -R
+cd Pictures/
+mv Movie0.avi ../Videos/
+mv Movie0.mp4 ../Videos/
+mv Movie1.mp4 ../Videos/
+mv Movie2.mp4 ../Videos/
+mv Movie3.mp4 ../Videos/
+mv Movie4.mp4 ../Videos/
+mv Movie5.mp4 ../Videos/
+mv Movie6.mp4 ../Videos/
+mv Movie7.mp4 ../Videos/
+mv Movie8.mp4 ../Videos/
+mv Movie9.mp4 ../Videos/
+mv Movie01.avi ../Videos/
+mv Movie02.avi ../Videos/
+mv Movie03.avi ../Videos/
+mv Movie04.avi ../Videos/
+mv Movie05.avi ../Videos/
+ls -l
+cd ..
+ls -R
+cd Music
+mkdir LirikaInversa Canserbero Proof Eminem Nach Chojin Residente Rapsusklei BobMarley MikeDiaz LionReggae Gondwana Gorillaz MichaelJackson Queen TheBeatles LosAldeanos RafaelLechowski HijosDeLasHojas ArsenalDeRimas
+ls -l
+cd ArsenalDeRimas/
+touch ArsenalDeRimas-ElBuenoElFeoYElMalo.mp3
+touch ArsenalDeRimas-HueleAMoy.mp3
+touch ArsenalDeRimas-Ella.mp3
+touch ArsenalDeRimas-43.mp3
+touch ArsenalDeRimas-QuisieraSerUnaLagrima.mp3
+cd ..
+cd BobMarley
+touch BobMarley-ThreeLittleBirds.mp3
+touch BobMarley-IsThisLove.mp3
+touch BobMarley-OneLove.mp3
+touch BobMarley-BuffaloSoldier.mp3
+touch BobMarley-BadBoys.mp3
+cd..
+cd ..
+cd Canserbero/
+touch Canserbero-EsEpico.mp3
+touch Canserbero-MundoDePiedra.mp3
+touch Canserbero-Llovia.mp3
+touch Canserbero-YEnUnEspejoVi.mp3
+touch Canserbero-HastaCuando.mp3
+cd ..
+cd Chojin/
+touch Chojin-RapVsRacismo.mp3
+touch Chojin-RieCuandoPuedas.mp3
+touch Chojin-MiOdisea.mp3
+touch Chojin-ElMundoSigueGirando.mp3
+touch Chojin-ElAtaqueDeLosQueObservaban.mp3
+cd ..
+cd Eminem/
+touch Eminem-WithOutMe.mp3
+touch Eminem-TillICollapse.mp3
+touch Eminem-ImNotAfraid.mp3
+touch Eminem-Venom.mp3
+touch Eminem-WhenImGone.mp3
+cd ..
+ls -l
+cd Gondwana
+touch Gondwana-SentimientoOriginal.mp3
+touch Gondwana-Felicidad.mp3
+touch Gondwana-VerdeAmarilloYRojo.mp3
+touch Gondwana-SinCadenas.mp3
+touch Gondwana-Muchoverso.mp3
+cd ..
+cd Gorillaz/
+touch Gorillaz-ClintEastwood.mp3
+touch Gorillaz-FeelGoodInc.mp3
+touch Gorillaz-OnMelancolicHill.mp3
+touch Gorillaz-KidsWithGuns.mp3
+touch Gorillaz-RockTheHouse.mp3
+cd ..
+cd HijosDeLasHojas/
+touch HijosDeLasHojas-AntuFushaKushe.mp3
+touch HijosDeLasHojas-FrutosViejos.mp3
+touch HijosDeLasHojas-Caeran.mp3
+touch HijosDeLasHojas-LoQueCallanLasAbejas.mp3
+touch HijosDeLasHojas-BajoElMismoCielo.mp3
+cd ..
+cd LionReggae/
+touch LionReggae-CuandoPiensoEnTi.mp3
+touch LionReggae-TeFuiste.mp3
+touch LionReggae-MuchaFuerza.mp3
+touch LionReggae-Luna.mp3
+touch LionReggae-TuBelleza.mp3
+cd ..
+cd LirikaInversa/
+touch LirikaInversa-Niebla.mp3
+touch LirikaInversa-Statera.mp3
+touch LirikaInversa-Mandala.mp3
+touch LirikaInversa-Humano.mp3
+touch LirikaInversa-TempestadPrimigenia.mp3
+cd ..
+cd LosAldeanos/
+touch LosAldeanos-Poesia.mp3
+touch LosAldeanos-FueUnPlacer.mp3
+touch LosAldeanos-NoLeTengoMiedo.mp3
+touch LosAldeanos-LaClase.mp3
+touch LosAldeanos-Loco.mp3
+cd ..
+ls -l
+cd MichaelJackson/
+touch MichaelJackson-BillieJean.mp3
+touch MichaelJackson-SmoothCriminal.mp3
+touch MichaelJackson-BeatIt.mp3
+touch MichaelJackson-TheWayYouMakeMeFeel.mp3
+touch MichaelJackson-Thriller.mp3
+cd ..
+cd MikeDiaz/
+touch MikeDiaz-Universos.mp3
+touch MikeDiaz-Corona.mp3
+touch MikeDiaz-Renace.mp3
+touch MikeDiaz-Mision.mp3
+touch MikeDiaz-Dragon.mp3
+cd ..
+cd Nach
+touch Nach-ElIdiomaDeLosDioses.mp3
+touch Nach-EfectosVocales.mp3
+touch Nach-LosZurdosMuerenAntes.mp3
+touch Nach-MiGranAmor.mp3
+touch Nach-MilVidas.mp3
+cd ..
+cd Proof/
+touch Proof-RecetaSecreta.mp3
+touch Proof-HeavyMetal.mp3
+touch Proof-ElBanquete.mp3
+touch Proof-Helicoptero.mp3
+touch Proof-TodosALaCarcel.mp3
+cd ..
+cd Queen
+touch Queen-BohemianRhapsody.mp3
+touch Queen-SomebodyToLove.mp3
+touch Queen-IWantToBreakFree.mp3
+touch Queen-WeAreTheChampions.mp3
+touch Queen-WeWillRockYou.mp3
+cd ..
+cd RafaelLechowski/
+touch RafaelLechowski-PorAmorAlOdio.mp3
+touch RafaelLechowski-DondeDueleInspira.mp3
+touch RafaelLechowski-ArtesanoDelArteInsano.mp3
+touch RafaelLechowski-LaNadaEterna.mp3
+touch RafaelLechowski-Trece.mp3
+cd ..
+ls -l
+cd Rapsusklei/
+touch Rapsusklei-Enero.mp3
+touch Rapsusklei-AFuego.mp3
+touch Rapsusklei-60VecesPorMinuto.mp3
+touch Rapsusklei-Cleopatra.mp3
+touch Rapsusklei-Nacimiento.mp3
+cd ..
+cd Residente
+touch Residente-SomosAnormales.mp3
+touch Residente-UnaLeyendaChina.mp3
+ls
+touch Residente-DagombasEnTamale.mp3
+touch Residente-Desencuentro.mp3
+touch Residente-Guerra.mp3
+cd ..
+cd TheBeatles/
+touch TheBeatles-LetItBe.mp3
+touch TheBeatles-Yesterday.mp3
+touch TheBeatles-HeyJude.mp3
+touch TheBeatles-LucyInTheSkyWithDiamonds.mp3
+touch TheBeatles-DontLetMeDown.mp3
+cd ..
+touch song01.mp3
+touch song02.mp3
+touch song03.mp3
+touch song04.mp3
+touch song05.mp3
+touch song06.mp3
+touch song07.mp3
+touch song08.mp3
+touch song09.mp3
+touch song10.mp3
+touch song11.mp3
+touch song12.mp3
+touch song13.mp3
+touch song14.mp3
+touch song15.mp3
+cd ..
+# ls
+# bash-course
+# cd bash-course
+# ls
+# cd Marlon
+# ls
+# history
+# history > history_Marlon
+# ls
+# history > history_Marlon.txt
+# cd ~
+# cd C\:/
+# cd Users
+# cd Marlon
+cp -r Desktop Documents Downloads Folder1 Folder2 Folder3 Music Pictures Videos ../Miriam/
+cp -r Desktop Documents Downloads Folder1 Folder2 Folder3 Music Pictures Videos ../Jorge/
+# cd ~
+# cd bash-course
+# cd Marlon
+# history > history_Marlon.txt
