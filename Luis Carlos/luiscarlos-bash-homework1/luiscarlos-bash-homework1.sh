d38e27b1c21ea5e13538ee233163506b6ad53183
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 16:06:02 -0500

    Added Luis Carlos homework1

---
+++ Luis Carlos/luiscarlos-bash-homework1/luiscarlos-bash-homework1.sh
@@ -0,0 +1,231 @@
+cd C
+mv Unistall.exe Unistall.txt
+ls
+cd Programs/
+cd AndroidStudio/
+mv Unistall.exe Unistall.txt
+ls
+nano Unistall.txt 
+nano README.txt 
+cd ..
+cp -r AndroidStudio Skype
+cp -r AndroidStudio Eclipse
+cp -r AndroidStudio Chrome
+cp -r AndroidStudio Firefox
+cd ..
+ls
+cd Windows/
+ls
+cd IME
+ls
+rm file6.txt file7.txt file8.txt file9.txt file10.txt
+ls
+nano file1.txt file2.txt file3.txt file4.txt file5.txt
+cd ..
+rm -r Fonts Help INF logs
+cp -r IME INF
+cp -r IME logs
+cp -r IME Fonts
+cd System32/
+ls
+rm file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt file14.txt file15.txt
+ls
+nano file1.txt file2.txt file3.txt file4.txt file5.txt
+cd carpeta1
+ls
+rm file6.txt file7.txt file8.txt file9.txt
+nano file1.txt file2.txt file3.txt file4.txt file5.txt
+ls
+cd ..
+ls
+rm -r carpeta2 carpeta3 carpeta4 carpeta5
+cp -r carpeta1/ carpeta2/ carpeta3/ carpeta4/ carpeta5/
+cp -r carpeta1/ carpeta2/ 
+rm -r carpeta2/
+cd carpeta1
+rm file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt
+cd ..
+ls
+cp -r carpeta1/ carpeta2/ 
+cp -r carpeta1/ carpeta3
+cp -r carpeta1/ carpeta4
+cp -r carpeta1/ carpeta5
+cd ..
+cd Users/
+cd Luis/
+ls
+cd Documents/
+ls
+nano proyecto.doc
+nano ensayo.doc notas.txt proyectofinal.doc receta.txt
+rm archivos.rar exposicion.ppt precios.xsl tabla.xsl tareaciencias.doc catalogo.xsl exposicion-final.ppt mates.xsl porcentaje.xsl
+ls
+rm tarea.doc
+cd ..
+ls
+rm -r Favoritos Juegos Vinculos Videos
+ls
+rm -r Desktop Downloads Picture
+ls
+rm -r Pictures
+cp -r Documents Desktop
+cp -r Documents Downloads
+cp -r Documents Pictures
+ls
+cd Music
+ls
+rm -r Recodo LaArrolladora SergioVega ValentinElizalde LaBrissa
+ls
+rm -r JuanGabriel/ LaOrejaVanGogh/ TheWho/ MartinGarrix/ BillyJoel/
+ls
+cd ABBA
+ls
+mv *.mp3 *.txt
+ls
+install rename
+rename 's/\.mp3/\.txt' *.mp3
+sudo apt install rename
+rename 's/\.mp3/\.txt' *.mp3
+nano chiquitita.mp3 
+cat chiquitita.mp3 
+mv * 'ABBA - '*
+ls
+nano dancing-queen.mp3 
+nano fernando.mp3 
+nano mamma-mia.mp3 
+nano super-truper.mp3 
+cd ..
+ls
+cd Beatles
+ls
+nano come-together.mp3 
+nano dont-let-me-down.mp3 
+nano help.mp3 
+nano hey-jude.mp3 
+nano let-it-be.mp3 
+cd ..
+ls
+cd Buki/
+ls
+nano amigos
+nano donde-vamos-a-parar.mp3 
+nano inventame.mp3 
+nano quiereme.mp3 
+nano tu-carcel.mp3 
+cd ..
+ls
+cd Canserbero/
+ls
+nano 'Es epico.mp3' 
+nano 'Perdiendo la fe.mp3' 
+nano 'Stupid love history.mp3' 
+nano 'Jeremias 17 5.mp3' 
+nano 'Querer querernos.mp3' 
+cd ..
+cd Coldplay/
+ls
+nano 'Fix you.mp3' 
+nano Paradise.mp3 
+nano 'The scientist.mp3' 
+nano Viva-la-via.mp3 
+nano Yellow.mp3 
+cd ..
+ls
+cd Jesed
+ls
+nano Desolada.mp3 
+nano 'Mi razón de ser.mp3' 
+nano Miserere.mp3 
+nano 'Noche oscura.mp3' 
+naono 'pidiendo limosna.mp3' 
+nano 'pidiendo limosna.mp3' 
+cd ..
+ls
+cd LuisMiguel/
+ls
+nano bikina.mp3 
+nano culpable-o-no.mp3 
+nano incondicional.mp3 
+nano sueña.mp3 
+nano te-puedes-marchar.mp3 
+cd ..
+ls
+cd Oasis
+ls
+nano dont-loock-back-together.mp3 
+nano stop-crying.mp3 
+nano supersonic.mp3 
+nano the-hindu.mp3 
+nano wonderwall.mp3 
+cd ..
+ls
+cd Queen/
+ls
+nano another-one-bites-the-dust.mp3 
+nano hammer-to-fall.mp3 
+nano somebody-to-love.mp3 
+nano bohemian-raphsody.mp3 
+nano radio-gaga.mp3 
+cd ..
+ls
+cd 'The Credence'/
+ls
+nano Cottonfields.mp3 
+nano Jambalaya.mp3 
+nano 'Run trough the jungle.mp3' 
+nano 'Have you ever seen the rain.mp3' 
+nano 'Proud Mary.mp3' 
+cd..
+cd ..
+ls
+rm luiscarlos-bash-exercise1 
+rm -r Invitados/ Miguel/
+cp -r Luis Miguel
+cp -r Luis Invitados
+ls
+history > luiscarlos-bash-homework1
+cd C
+cd Users/
+cd Luis/
+cd Music/
+cd The\ Credence/
+ls
+mv 'Cottonfields.mp3' 'The Credence - Cottonfields.txt'
+ls
+mv 'Have you ever seen the rain.mp3' 'The Credence - Have you ever seen the rain.txt'
+mv 'Jambalaya.mp3' 'The Credence - Jambalaya.txt'
+mv 'Proud Mary.mp3' 'The Credence - Proud Mary.txt'
+mv 'Run trough the jungle.mp3' 'The Credence - Run trough the jungle.txt'
+ls
+mv 'The Credence - Cottonfields.txt' 'The Credence - Have you ever seen the rain.txt' 'The Credence - Jambalaya.txt' 'The Credence - Proud Mary.txt' 'The Credence - Run trough the jungle.txt' ../Canserbero/
+ls
+cd ..
+cd Canserbero/
+ls
+mv 'Es epico.mp3' 'Canserbero - Es epico.txt'
+mv 'Perdiendo la fe.mp3' 'Perdiendo la fe.txt'
+mv 'Stupid love history.mp3' 'Canserbero - Stupid love history.txt'
+mv 'Perdiendo la fe.txt' 'Canserbero - Perdiendo la fe.txt'
+mv 'Querer querernos.mp3' 'Canserbero - Querer querernos.txt'
+cd ..
+ls
+rm -r ABBA/ Beatles/ Buki/ Coldplay/ Jesed/ LuisMiguel/ Oasis/ Queen/ 'The Credence'/
+ls
+mv Canserbero/ Carpeta1/
+ls
+cp -r carpeta1/ carpeta2
+cp -r Carpeta1/ Carpeta2
+ls
+cp -r Carpeta1/ Carpeta3
+cp -r Carpeta1/ Carpeta4
+cp -r Carpeta1/ Carpeta5
+cp -r Carpeta1/ Carpeta6
+cp -r Carpeta1/ Carpeta7
+cp -r Carpeta1/ Carpeta8
+cp -r Carpeta1/ Carpeta9
+cp -r Carpeta1/ Carpeta10
+cd ..
+rm -r Miguel/ Invitados/
+cp -r Luis/ Miguel
+cp -r Luis/ Juan
+history > luiscarlos-bash-homework1.txt
