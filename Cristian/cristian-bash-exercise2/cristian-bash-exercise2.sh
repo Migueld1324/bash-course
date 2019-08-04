a8c999f6ade977c4fec4bc8b0a055f0d4cc4166d
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 17:17:05 -0500

    Added Cristian's exercise 2

---
+++ Cristian/cristian-bash-exercise2/cristian-bash-exercise2.sh
@@ -0,0 +1,80 @@
+ls
+nano practicanaono.txt
+ls
+nano practicanano.txt
+grep -c 'amar' practicanano.txt 
+cat practicanano.txt 
+grep -ic 'amar' practicanano.txt 
+grep 'Amar' practicanano.txt 
+ps aux
+ps aux | grep chrome
+ls -la
+cp practicanano.txt Users/cristian/
+cd Users/cristian/
+lsç
+ls
+ls -la
+chmod u=rwx,g=rw,u=r practicanano.txt 
+ls -la
+chmod u=rwx,g=rw,o=r practicanano.txt 
+ls -la
+kill -l
+ps aux | grep chrome
+cat /etc/passwd
+sudo groupadd admin2
+cat/etc/group
+cat /etc/group
+sudo groupadd guest
+sudo useradd owner
+sudo useradd crisss
+sudo useradd humberto
+sudo useradd kimberly
+cat /etc/group
+sudo adduser owner owner
+sudo adduser owner crisss
+sudo adduser owner humberto
+sudo adduser owner kimberly
+cd
+ls
+cd Programs/
+ls
+cd
+ls
+sudo chown -R owner Programs/
+sudo chgrp -R admin2 Programs/
+ls -l Programs/
+ls
+sudo chown owner Users/
+ls -l
+sudo chgrp -R guest Users/
+cd Users/
+ls
+sudo chown crisss cristian/
+sudo chown 
+sudo chown -R crisss cristian/
+sudo chown -R humberto  humberto/
+sudo chown -R kimberly kimberly/
+history -w cristian_bash_exercise2
+cd cristian/
+ls
+cd Music/
+ls
+cd arca/
+ls
+ls -a
+ls -la
+chmod u=,g=,o= cambio.mp3 
+sudo chmod u=,g=,o= cambio.mp3 
+ls -la
+sudo chmod u=r,g=,o= comotumetocas.mp3 
+sudo chmod u=rw,g=,o= diles.mp3 
+sudo chmod u=rwx,g=,o= elgranjero.mp3 
+sudo chmod u=rwx,g=r,o= hacemuchotiempo.mp3 
+sudo chmod u=rwx,g=rw,o= losfavoritos.mp3 
+sudo chmod u=rwx,g=rwx,o= meamameodia.mp3 
+sudo chmod u=rwx,g=rwx,o=r meprefiereami.mp3 
+sudo chmod u=rwx,g=rwx,o=rw paquelapasesbien.mp3 
+sudo chmod u=rwx,g=rwx,o=rwx privado.mp3 
+ls -la
+cd
+history -w cristian_bash_exercise2
