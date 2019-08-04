4f058cd3d74fa0d54ee58604a00dc67fe0cedbb7
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 02:21:22 -0500

    Added Urbano's exercise 1

---
+++ Urbano/urbano-bash-exercise1/urbano-bash-exercise1.sh
@@ -0,0 +1,349 @@
+#!/bin/bash
+
+mkdir C
+cd C
+mkdir Prgrams Windows Users
+mv Prgrams Programs
+ls
+cd Programs/
+mkdir {folder1,folder2,folder3,folder4,folder5,}
+ls
+mkdir {folder6,folder7,folder8,folder9,folder10,}
+touch folder1 {file1.txt,file2}
+cd folder1
+cp file1.txt file2.txt ../folder2
+ls
+touch file1.txt file2.txt
+ls
+cp file1.txt file2.txt ../folder2
+cp file1.txt file2.txt ../folder3
+cp file1.txt file2.txt ../folder4
+cp file1.txt file2.txt ../folder5
+cp file1.txt file2.txt ../folder6
+cp file1.txt file2.txt ../folder7
+cp file1.txt file2.txt ../folder8
+cp file1.txt file2.txt ../folder9
+cp file1.txt file2.txt ../folder10
+cp file1.txt file2.txt ../folder11
+cd ..
+ls folder2
+ls folder3
+cd ..
+cd Windows/
+mkdir folder1 folder2 folder3 foilder4 folder5
+cd folder1}
+cd folder1
+touch file1.txt file2.txt file3.txt file4.txt file5.txt
+touch file6.txt file7.txt file8.txt file9.txt file10.txt
+ls
+cp .*file ../folder2
+cp .*file ../folder3
+cp .*file ../folder4}
+cp .*file ../folder4
+ls
+cp file* ../folder2/
+cp file* ../folder3
+cp file* ../folder4
+cp file* ../folder5
+cd ..
+ls
+mv foilder4 folder4
+cd folder1
+cp file* ../folder4
+cd ..
+ls folder2
+ls folder5
+cd ..
+cd Windows/
+ls
+mkdir System32
+cd System32/
+cd ..
+ls
+cp folder* System32/
+cp -r folder* System32/
+cd System32/
+ls
+cd folder1
+cp file* ../
+cd ..
+ls
+touch file11.txt file12.txt file13txt file14.txt file15.txt
+ls
+cd ..
+ls
+cd ..
+cd Users/
+mkdir urbano jose pedro
+cd urbano
+mkdir {Desktop,Documents,Downloads,Music,Pictures,Videos,Games, Internship Git}
+ls
+rmdir
+rmdir ./
+rmdir .*
+rmdir \{Desktop\,Documents\,Downloads\,Music\,Pictures\,Videos\,Games\,/
+ls
+mkdir Desktop Documents Downloads Music Pictures Videos
+ls
+cd Desktop/
+touch 
+ls
+rmdir \{Chrome.ink\,Papelera 
+rmdir rf \{Chrome.ink\,Papelera 
+ls
+rmdir \{Chrome.ink\,Papelera 
+rmdir {Chrome.ink\,Papelera 
+rmdir {Chrome.ink\,Papelera}
+ls
+rmdir .*
+rmdir Re*
+ls
+touch PapeleraDeReciclaje.ink Google_Chorme.ink Word.ink PowerPoint.ink VisualStudioCode.ink PhpStrom.ink Edge.ink MiEquipo.ink Skype.ink Discord.ink NetBins.ink Sublime.ink CallOfDuty.ink PhotoShop.ink Illustrator.ink SonyVegas.ink
+ls
+rm -rf Reciclaje}
+ls
+rm -rf {Chrome.ink\,Papelera 
+ls
+rm -rf de
+ls
+cd ..
+ls
+cd Documents/
+touch tareaFisica.docx tareaCalculo.docx tareaViernes.docx presentacionObjetivos.pptx Problematica.pptx hipotesis.pptx marcoteorico.pptx trucosGTA_SA.docx tareaCalculo2.docx expoProyecto.pptx 
+ls
+touch tareaEcuacionesDif.docx Exponentes.docx tareaFiananzas.xls puntoEquilibrio.xls ensayoFilosofia.docx
+ls
+cd ..
+ls
+cd Downloads/
+touch CableDeRed.pdf CableDeFibraExt.pdf fotoTarea2.jpg antenaProyecto.jpg Antena.pptx Anteproyecto.docx bug.png demo.rar bootstrap_4.zip progit.pdf VEGAS16_261.rar winrar-x64es.exe YDKWJS.pdf
+ls
+touch tarjetaNSS.pdf reporte.xls 
+ls
+cd ..
+ls
+mv Git} Git
+cd Git/
+cd ..
+ls
+cd ../../
+ls
+cd Programs/
+ls
+cp folder* ../Users/urbano/Git/
+cp -r }folder* ../Users/urbano/Git/
+cp -r folder* ../Users/urbano/Git/
+cd ..
+cd Users/urbano/
+cd Git/
+ls
+cd ..
+mv folder* proyecto
+mv folder* proyecto*
+ls
+cd Git/
+mv folder* proyecto*
+mv folder* proyecto
+mv folder* proyecto*
+mv folder1 proyecto1
+ls
+mv folder2 proyecto2
+mv folder3 proyecto3
+mv folder4 proyecto4
+mv folder5 proyecto5
+mv folder6 proyecto6
+rm -rf folder*
+ls
+cd ..
+ls
+Internship
+cd Internship/
+touch progit.pdf YDKW_JS1 YDKN_JS2 YDKN_JS3 YDKN_JS4 YDKN_JS5 YDKN_JS6 pragProgram.pdf plumbingCommands.docx porcelainCommands.docx gitBasics.docx gitUnderHood.docx bashCommands.docx mantenimiento.pptx diseno.pptx requerimientos.pptx testing.pptx desarrollo.pptx
+ls
+cd ..
+ls
+cd Pictures/
+touch Vacacaciones/2015-06-15-45
+mkdir Vacacaciones
+cd Vacacaciones/
+touch 2015-06-15-4585.jpg 2015-06-15-5821.jpg 2015-06-15-4896.jpg 2015-06-15-8965.jpg 2015-06-15-4897.jpg
+touch 2015-06-16-5585.jpg 2015-06-16-7821.jpgcd 2015-06-16-47996.jpg 2015-06-16-7875.jpg 2015-06-16-7877.jpg
+ls
+cd..
+cd ..
+happyFace.png bug.png userIcon.png memedeBatman.jpg platilladeMeme10.jpg
+ls
+touch happyFace.png bug.png userIcon.png memedeBatman.jpg platilladeMeme10.jpg
+ls
+cd ..
+ls
+cd Videos/
+touch viajeMichoacan.mp4 losAzufres.mp4 morelia.mp4 cdHidalgo.mp4 intererstellar.mp4 aquaman.mp4 avengerIWar.mp4 avengerEndGame.mp
+ls
+ren avengerEndGame.mp avengersEndGame.mp4
+mv avengerEndGame.mp avengersEndGame.mp4
+ls
+touch 15115648dsada.mp4 videoLosValoresEticos.mp4 corrientresFilosoficas.mp4 antman.mp4 JLDC.mp4 shazam.mp4 anabell.mp4 conjuring.mp4 us.mp4 heredeitary.mp4 
+ls
+cd ..
+ls
+cd Music/
+mkdir DivisionMinuscula Caifanes DLD Bunkers Molotov CafeTacvba SodaEstereo Queen TheKillers GusanaCiega PanteonRococo OneOkRock AC_DC Gorillaz RedHotChilliPepers
+ls
+touch TheStrokes PXNDX Enjambre
+ls
+rm TheStrokes PXNDX Enjambre
+mkdir TheStrokes PXNDX Enjambre
+ls
+cd AC_DC/
+touch thunderstruck.mp3 tnt.mp3 backInBlack.mp3 highwayToHell.mp3 warmachine.mp3
+ls
+cd ..
+ls
+cd Bunkers/
+touch llueveSobreLaCiudad.mp3 mientele.mp3 quienFuera.mp3 velocidadDeLaLuz.mp3 baialandoSolo.mp3 nadaNuevoBajoELSol.mp3
+ls
+cd ..
+ls
+cd CafeTacvba/
+touch futuro.mp3 esaNoche.mp3 elBaileYelSAlon.mp3 esaNoche.mp3 ingrata.mp3 eres.mp3
+ls
+cd ..
+cd Caifanes/
+touch ojoDeVeando.mp3 laNegraToamsa.mp3 viento.mp3 losDiosesOcultos.mp3 laCelulaQueExplota.mp3
+ls
+cd ..
+ls
+cd DLD/
+touch estare.mp3 miVida.mp3 arsenico.mp3 cruzadas.mp3 todoCuenta.mp3
+ls
+cd ..
+ls
+cd DivisionMinuscula/
+touch lucesDeEstaCiudad.mp3 sognare.mp3 anioNuevo.mp3 cadaMartes.mp3 humanosComoTu.mp3 
+ls
+cd ..
+ls
+cd Enjambre/
+touch  sabadoPerpetuo.mp3 soledad.mp3 madrugada.mp3 daltonico.mp3 somoAjenos.mp3
+ls
+cd ..
+sl
+ls
+cd Gorillaz/
+touch clintEastwood.mp3 feelGoodInc.mp3 onMelancolyHill.mp3 dare.mp3 humility.mp3
+ls
+cd ..
+ls
+cd GusanaCiega/
+touch tornasol.mp3 hey.mp3 ellaEstrella.mp3 sanMiguel.mp3 laDistancia.mp3
+ls
+cd ..
+ls
+cd Molotov/
+touch frijolero.mp3 noko.mp3 votoLatino.mp3 matateTete.mp3 hereWeKum.mp3
+ls
+cd ..
+ls
+cd OneOkRock/
+touch pierce.mp3 theBegining.mp3 heartache.mp3 nothingHelps 
+ls
+mv nothingHelps nothingHelps.mp3
+ls
+touch Re:make.mp3
+ls
+cd ..
+ls
+cd PXNDX/
+touch saludosDesdeTurquia.mp3 losMalventuradosNoLloran.mp3 usted.mp3 citaQuirofano.mp3 nuestraAfliccion.mp3
+ls
+cd ..
+ls
+cd PanteonRococo/
+touch laCerencia.mp3 vendedoraDeCarcicas.mp3 elUtlimoSka.mp3 laDosisPerfecta.mp3
+ls
+touch infiernos.mp3
+cd ..
+ls
+cd Queen/
+touch bohemianRapsody.mp3 killerQueen.mp3 sheerHerathAtack.mp3 bitesTheDust.mp3 radioGaga.mp3
+ls
+cd ..
+ls
+RedHotChilliPepers/
+cd RedHotChilliPepers/
+touch cantStop.mp3 aroundTheWorld.mp3 californication.mp3 underTheBridge.mp3 suckMyKiss.mp3
+sl
+ls
+cd ..
+ls
+cd SodaEstereo/
+ls
+touch musicaLigerea.mp3 cuandoPaseElTemblor.mp3 persianaAmericana.mp3 tratameSuavemente.mp3 enLaCiudadDelaFuria.mp3 
+ls}
+ls
+cd ..
+ls
+cd TheKillers/
+touch whenYouWereYoung.mp3 somebodyToldMe.mp3 human.mp3 onTop.mp3 theMan.mp3
+ls
+cd ..
+ls
+TheStrokes/
+cd TheStrokes/
+touch reptilia.mp3 lastNite.mp3 someday.mp3 tapOut.mp3 chances.mp3
+ls
+cd ...
+cd ..
+ls
+cd ..
+ls
+cp -r Desktop Documents Downloads Git Internship Music Pictures Videos ../jose
+cp -r Desktop Documents Downloads Git Internship Music Pictures Videos ../pedro
+cd ..
+ls pedro/
+ls jose/
+cd
+cd Documents/
+history >> bashExercise.txt
+ls
+# nano bashExercise.txt 
+cd C/Users/urbano/Internship/
+mv YDKN_JS1 YDKN_JS1.pdf  
+mv YDKN_JS1 YDKN_JS1.pdf}
+mv YDKN_JS1 YDKN_JS1.pdf
+ls
+mv YDKW_JS1 YDKW_JS1.pdf
+mv YDKN_JS2 YDKN_JS2.pdf YDKN_JS3 YDKN_JS3.pdf
+mv YDKN_JS2 YDKN_JS2.pdf
+ls
+mv YDKN_JS3 YDKN_JS3.pdf
+mv YDKN_JS4 YDKN_JS4.pdf
+mv YDKN_JS5 YDKN_JS5.pdf
+mv YDKN_JS6 YDKN_JS6.pdf
+cd ..
+cd pedro/
+cd Internship/
+mv YDKN_JS6 YDKN_JS6.pdf
+mv YDKN_JS5 YDKN_JS5.pdf
+mv YDKN_JS4 YDKN_JS4.pdf
+mv YDKN_JS3 YDKN_JS3.pdf
+mv YDKN_JS2 YDKN_JS2.pdf
+mv YDKW_JS1 YDKW_JS1.pdf
+ls}
+ls
+cd
+cd Documents/C/Users/jose/Internship/
+mv YDKW_JS1 YDKW_JS1.pdf
+mv YDKN_JS2 YDKN_JS2.pdf
+mv YDKN_JS3 YDKN_JS3.pdf
+mv YDKN_JS4 YDKN_JS4.pdf
+mv YDKN_JS5 YDKN_JS5.pdf
+mv YDKN_JS6 YDKN_JS6.pdf
+ls
+cd
+cd Doc
+cd Documents/
+ls
+rm -rf bashExercise.txt 
+history >> bashExercise.txt
