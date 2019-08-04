baed6cbff13bedf80e12000a5dddb475ef1c00a1
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 15:35:27 -0500

    Added Hector's Homework 1

---
+++ Hector/hector-bash-homework1/hector-bash-homework1.sh
@@ -0,0 +1,691 @@
+#!/bin/bash
+
+ls
+cd C
+ls
+cd Programs/
+ls
+rm -r ares/
+rm -r calendar/
+rm -r edge/
+rm -r word/
+rm -r excel/
+ls
+cd calculator/
+cd ../../
+cd Users/User1/
+ls
+rm -r BTS/
+rm -r Books/
+rm -r Games/
+rm -r Videos/
+ls
+cd Desktop/
+ls
+rm AdobeReader 
+rm Office 
+rm Firefox 
+rm Ubuntu 
+rm Sketch 
+rm Spotify 
+rm Steam 
+rm Photoshop 
+rm Paint 
+rm Blizzard 
+ls
+cd ..
+ls
+cd Documents/
+ls
+rm horarios.xlsx 
+rm recetas-abuela.docx 
+rm tesis-ahora-si-ya.docx 
+rm tesis.docx 
+rm tesis-no-se-controlar-versiones.docx 
+rm tesis-final.docx 
+rm tesis-final-final.docx 
+rm tesis-este-si.docx 
+rm tesis-ultimate-edition.docx 
+rm pro-git.pdf 
+ls
+cd ..
+ls
+cd Downloads/
+ls
+rm chrome.exe 
+rm slack.exe 
+rm foto2.jpg 
+rm foto3.jpg 
+rm foto4.jpg 
+rm foto.png 
+rm tesis.pdf 
+rm spotify.zip 
+rm cmder.exe 
+rm firacode.ttf 
+ls
+cd ..
+cd Pictures/
+ls
+rm foto1.jpg 
+rm foto2.jpg 
+rm foto3.jpg 
+rm foto4.jpg 
+rm foto5.jpg 
+rm foto6.jpg 
+rm foto7.jpg 
+rm foto8.jpg 
+rm foto9.jpg 
+rm foto10.jpg 
+ls
+cd ../Music/
+ls
+rm -r Dragonforce/
+rm -r Steve_Aoki/
+rm -r Rio_Roma/
+rm -r Toto/
+rm -r Franz_Ferdinand/
+# rm -r Avenged_Sevenfold/
+rm -r Linkin_Park/
+rm -r Eminem/
+rm -r Pepe_Aguilar/
+rm -r Ninja_Sex_Party/
+ls
+cd ../../User2/
+ls
+rm -r BTS/
+rm -r Books/
+rm -r Games/
+rm -r Videos/
+# ls
+# cp -r ../User1/ .
+ls
+cd Desktop/
+ls
+cd ..
+rm -r Desktop/
+rm -r Documents/
+rm -r Downloads/
+rm -r Music/
+rm -r Pictures/
+ls
+cp -r ../User1/ .
+ls
+cd Desktop/
+ls
+cd ..
+cp -r ../User1/Documents .
+cp -r ../User1/Downloads .
+cp -r ../User1/Music .
+cp -r ../User1/Pictures .
+ls
+cd ../User3
+rm -r Desktop/
+rm -r Documents/
+rm -r Downloads/
+rm -r Music/
+rm -r Pictures/
+ls
+rm -r BTS/
+rm -r Books/
+rm -r Games/
+rm -r Videos/
+ls
+cp -r ../User1/Documents .
+cp -r ../User1/Desktop .
+cp -r ../User1/Downloads .
+cp -r ../User1/Music .
+cp -r ../User1/Pictures .
+ls
+cd Pictures/
+ls
+cd ..
+cd ../../
+cd ..
+history >> homework_1_history.txt
+# ls
+# cd C
+# ls
+# cd Programs/
+# ls
+# rm -r ares/
+# rm -r calendar/
+# rm -r edge/
+# rm -r word/
+# rm -r excel/
+# ls
+# cd calculator/
+# cd ..
+# cd Users/User1/
+# ls
+# rm -r BTS/
+# rm -r Books/
+# rm -r Games/
+# rm -r Videos/
+# ls
+# cd Desktop/
+# ls
+# rm AdobeReader 
+# rm Office 
+# rm Firefox 
+# rm Ubuntu 
+# rm Sketch 
+# rm Spotify 
+# rm Steam 
+# rm Photoshop 
+# rm Paint 
+# rm Blizzard 
+# ls
+# cd ..
+# ls
+# cd Documents/
+# ls
+# rm horarios.xlsx 
+# rm recetas-abuela.docx 
+# rm tesis-ahora-si-ya.docx 
+# rm tesis.docx 
+# rm tesis-no-se-controlar-versiones.docx 
+# rm tesis-final.docx 
+# rm tesis-final-final.docx 
+# rm tesis-este-si.docx 
+# rm tesis-ultimate-edition.docx 
+# rm pro-git.pdf 
+# ls
+# cd ..
+# ls
+# cd Downloads/
+# ls
+# rm chrome.exe 
+# rm slack.exe 
+# rm foto2.jpg 
+# rm foto3.jpg 
+# rm foto4.jpg 
+# rm foto.png 
+# rm tesis.pdf 
+# rm spotify.zip 
+# rm cmder.exe 
+# rm firacode.ttf 
+# ls
+# cd ..
+# cd Pictures/
+# ls
+# rm foto1.jpg 
+# rm foto2.jpg 
+# rm foto3.jpg 
+# rm foto4.jpg 
+# rm foto5.jpg 
+# rm foto6.jpg 
+# rm foto7.jpg 
+# rm foto8.jpg 
+# rm foto9.jpg 
+# rm foto10.jpg 
+# ls
+# cd ../Music/
+# ls
+# rm -r Dragonforce/
+# rm -r Steve_Aoki/
+# rm -r Rio_Roma/
+# rm -r Toto/
+# rm -r Franz_Ferdinand/
+# rm -r Avenged_Sevenfold/
+# rm -r Linkin_Park/
+# rm -r Eminem/
+# rm -r Pepe_Aguilar/
+# rm -r Ninja_Sex_Party/
+# ls
+# cd ../../User2/
+# ls
+# rm -r BTS/
+# rm -r Books/
+# rm -r Games/
+# rm -r Videos/
+# ls
+# cp -r ../User1/Desktop/ .
+# ls
+# cd Desktop/
+# ls
+# cd ..
+# rm -r Desktop/
+# rm -r Documents/
+# rm -r Downloads/
+# rm -r Music/
+# rm -r Pictures/
+# ls
+# cp -r ../User1/Desktop/ .
+# ls
+# cd Desktop/
+# ls
+# cd ..
+# cp -r ../User1/Documents/ .
+# cp -r ../User1/Downloads/ .
+# cp -r ../User1/Music/ .
+# cp -r ../User1/Pictures/ .
+# ls
+# cd ../User3
+# rm -r Desktop/
+# rm -r Documents/
+# rm -r Downloads/
+# rm -r Music/
+# rm -r Pictures/
+# ls
+# rm -r BTS/
+# rm -r Books/
+# rm -r Games/
+# rm -r Videos/
+# ls
+# cp -r ../User1/Documents/ .
+# cp -r ../User1/Desktop/ .
+# cp -r ../User1/Downloads/ .
+# cp -r ../User1/Music/ .
+# cp -r ../User1/Pictures/ .
+# ls
+# cd Pictures/
+# ls
+# cd ..
+# cd ../../
+# cd ..
+# history >> homework_1_history.txt
+cd C/Windows/
+ls
+rm -r help/
+ls
+cd System32/
+ls
+rm file1
+rm file2
+rm file3
+rm file4
+rm file5
+rm file6
+rm file7
+rm file8
+rm file9
+rm file10
+ls
+cd ..
+ls
+cd app/
+ls
+rm file1
+rm file2
+rm file3
+rm file4
+rm file5
+ls
+cd ..
+cd boot/
+ls
+rm file1
+rm file2
+rm file3
+rm file4
+rm file5
+ls
+cd ..
+ls
+cd debug/
+ls
+rm file1
+rm file2
+rm file3
+rm file4
+rm file5
+ls
+cd ..
+cd diagnostics/
+ls
+rm file1
+rm file2
+rm file3
+rm file4
+rm file5
+ls
+cd ..
+ls
+cd ../Users/User1/Music
+history >> homework_1_history.txt 
+cd Five_Finger_Death_Punch/
+touch "Five Finger Death Punch - Wrong Side of Heaven"
+ls
+touch "Five Finger Death Punch - Wrong Side of Heaven"
+touch "Five Finger Death Punch - Bad Company"
+ls
+touch "Five Finger Death Punch - Bad Company"
+touch "Five Finger Death Punch - Wash It All Away"
+touch "Five Finger Death Punch - Blue On Black"
+touch "Five Finger Death Punch - The Bleeding"
+touch "Five Finger Death Punch - Remember Everything"
+touch "Five Finger Death Punch - Under and Over It"
+touch "Five Finger Death Punch - Lift Me Up"
+touch "Five Finger Death Punch - Coming Down"
+touch "Five Finger Death Punch - I Apologize"
+ls
+cd ..
+ls
+cd AC_DC/
+touch "AC/DC - Thunderstruck"
+touch "AC\/DC - Thunderstruck"
+touch "AC DC - Thunderstruck"
+touch "AC DC - Highway to Hell"
+touch "AC DC - Back In Black"
+touch "AC DC - You Shook Me All Night Long"
+touch "AC DC - Hells Bells"
+touch "AC DC - Let There Be Rock"
+touch "AC DC - Shoot to Thrill"
+touch "AC DC - Whole Lotta Rosie"
+touch "AC DC - It's a Long Way to the Top"
+touch "AC DC - Moneytalks"
+ls
+cd ..
+ls
+cd Avenged_Sevenfold/
+touch "Avenged Sevenfold - Dear God"
+touch "Avenged Sevenfold - So Far Away"
+touch "Avenged Sevenfold - Seize the Day"
+touch "Avenged Sevenfold - Hail to the King"
+touch "Avenged Sevenfold - A Little Piece of Heaven"
+touch "Avenged Sevenfold - Bat Country"
+touch "Avenged Sevenfold - Afterlife"
+touch "Avenged Sevenfold - Buried Alive"
+touch "Avenged Sevenfold - Sheperd of Fire"
+touch "Avenged Sevenfold - Unholy Confessions"
+ls
+cd ..
+ls
+cd Daft_Punk/
+ls
+touch "Daft Punk - Get Lucky"
+touch "Daft Punk - Harder, Better, Faster, Stronger"
+touch "Daft Punk - One More Time"
+touch "Daft Punk - Around the World"
+touch "Daft Punk - Lose Yourself to Dance"
+touch "Daft Punk - Something About Us"
+touch "Daft Punk - Instant Crush"
+touch "Daft Punk - Da Funk"
+touch "Daft Punk - Robot Rock"
+touch "Daft Punk - Digital Love"
+ls
+cd ..
+ls
+cd Fall_Out_Boy/
+touch "Fall Out Boy - Centuries"
+touch "Fall Out Boy - My Songs Know What You Did in the Dark"
+touch "Fall Out Boy - Sugar, We're Going Down"
+touch "Fall Out Boy - Dance, Dance"
+touch "Fall Out Boy - Thnks fr th Mmrs"
+touch "Fall Out Boy - The Phoenix"
+touch "Fall Out Boy - Uma Thurman"
+touch "Fall Out Boy - This Ain't a Scene, It's a Goddamn Arms Race"
+touch "Fall Out Boy - Immortals"
+touch "Fall Out Boy - Irresistible"
+ls
+cd ..
+ls
+cd Luis_Miguel/
+touch "Luis Miguel - La Incondicional"
+touch "Luis Miguel - Ahora Te Puedes Marchar"
+touch "Luis Miguel - Hasta Que Me Olvides"
+touch "Luis Miguel - Tengo Todo Excepto a Ti"
+touch "Luis Miguel - La Bikina"
+touch "Luis Miguel - Entregate"
+touch "Luis Miguel - La Media Vuelta"
+touch "Luis Miguel - No Se Tu"
+touch "Luis Miguel - Fria Como el Viento"
+touch "Luis Miguel - Amarte Es Un Placer"
+ls
+cd ..
+ls
+cd Queen/
+touch "Queen - Bohemian Rhapsody"
+touch "Queen - We Will Rock You"
+touch "Queen - Don't Stop Me Now"
+touch "Queen - Another One Bites The Dust"
+touch "Queen - We Are the Champions"
+touch "Queen - Radio Ga Ga"
+touch "Queen - Somebody To Love"
+touch "Queen - I Want To Break Free"
+touch "Queen - Killer Queen"
+touch "Queen - Love of My Life"
+ls
+cd ..
+ls
+cd Romeo_Santos/
+touch "Romeo Santos - Aullando"
+touch "Romeo Santos - You"
+touch "Romeo Santos - Propuesta Indecente"
+touch "Romeo Santos - Eres Mia"
+touch "Romeo Santos - Enseñame a Olvidar"
+touch "Romeo Santos - Ella Y Yo"
+touch "Romeo Santos - Sobredosis"
+touch "Romeo Santos - Imitadora"
+touch "Romeo Santos - Amor de Madre"
+touch "Romeo Santos - La Diabla"
+ls
+cd ..
+ls
+cd Skillet/
+touch "Skillet - Monster"
+touch "Skillet - Hero"
+touch "Skillet - Feel Invincible"
+touch "Skillet - Awake and Alive"
+touch "Skillet - Not Gonna Die"
+touch "Skillet - Whispers in the Dark"
+touch "Skillet - The Resistance"
+touch "Skillet - Rebirthing"
+touch "Skillet - Sick of It"
+touch "Skillet - Legendary"
+ls
+cd ..
+ls
+cd TWRP/
+touch "TWRP - Starlight Brigade"
+touch "TWRP - Atomic Karate"
+touch "TWRP - The No Pants Dance"
+touch "TWRP - Synthesize Her"
+touch "TWRP - The Hit"
+touch "TWRP - Phantom Racer"
+touch "TWRP - Rock N Roll Best Friends"
+touch "TWRP - Makin' a Move"
+touch "TWRP - Groove Crusaders"
+touch "TWRP - FOOD BAR"
+touch "TWRP - Body Image"
+ls
+rm -rf "TWRP - FOOD BAR"
+ls
+cd ..
+# cd User1/Music/
+ls
+cd Avenged_Sevenfold/
+ls
+cd ..
+cd Five_Finger_Death_Punch/
+ls
+cd ../../../
+cd User2/
+cd Music/
+ls
+cd Five_Finger_Death_Punch/
+ls
+touch "Five Finger Death Punch - Bad Company"
+cd ..
+cp -r Five_Finger_Death_Punch/ ../../User1/Music/
+cd ../../
+cd User1/
+cd Music/
+ls
+cd Five_Finger_Death_Punch/
+ls
+cd ../../../
+cd User2/Music/
+ls
+cp -r AC_DC/ ../../User1/Music/
+cp -r Avenged_Sevenfold/ ../../User1/Music/
+cp -r Daft_Punk/ ../../User1/Music/
+cp -r Fall_Out_Boy/ ../../User1/Music/
+cp -r Luis_Miguel/ ../../User1/Music/
+cp -r Queen/ ../../User1/Music/
+cp -r Romeo_Santos/ ../../User1/Music/
+cp -r Skillet/ ../../User1/Music/
+cp -r TWRP/ ../../User1/Music/
+cd ../../
+cd User1/Music/Luis_Miguel/
+ls
+touch "Luis Miguel - Ahora Te Puedes Marchar"
+cd ../../
+cd Music/
+cp -r Five_Finger_Death_Punch ../../User3/Music/
+cp -r AC_DC ../../User3/Music/
+cp -r Avenged_Sevenfold ../../User3/Music/
+cp -r Daft_Punk ../../User3/Music/
+cp -r Fall_Out_Boy ../../User3/Music/
+cp -r Luis_Miguel ../../User3/Music/
+cp -r Queen ../../User3/Music/
+cp -r Romeo_Santos ../../User3/Music/
+cp -r Skillet ../../User3/Music/
+cp -r TWRP ../../User3/Music/
+cd ..
+cd User3/Music/Queen/
+ls
+touch "Queen - Radio Ga Ga"
+cd ..
+cd C/Programs/access/
+touch file1
+cat file1 > file2
+touch file2
+cat file1 > ../calculator/file1
+cat file1 > ../calculator/file2
+cat file1 > ../chrome/file1
+cat file1 > ../chrome/file2
+cat file1 > ../powerpoint/file1
+cat file1 > ../powerpoint/file2
+cat file1 > ../spotify/file1
+cat file1 > ../spotify/file2
+touch ../spotify/file2
+cat file1 > ../../Users/User1/Desktop/AdobeXD 
+cat file1 > ../../Users/User1/Desktop/Chrome 
+cat file1 > ../../Users/User1/Desktop/Drive
+cat file1 > ../../Users/User1/Desktop/Postman
+cat file1 > ../../Users/User1/Desktop/RecycleBin 
+cat file1 > ../../Users/User1/Documents/pragmatic-programmer.pdf 
+touch ../../Users/User1/Documents/pragmatic-programmer.pdf 
+cat file1 > ../../Users/User1/Documents/presupuesto.xlsx 
+cat file1 > ../../Users/User1/Documents/registro-aprendizaje.xlsx 
+cat file1 > ../../Users/User1/Documents/registro-ocio.xlsx 
+cat file1 > ../../Users/User1/Documents/tesis-entrega-final.docx 
+cat file1 > ../../Users/User1/Downloads/feedback-asesor.docx 
+cat file1 > ../../Users/User1/Downloads/index.html 
+cat file1 > ../../Users/User1/Downloads/filezilla.zip 
+touch ../../Users/User1/Downloads/filezilla.zip 
+cat file1 > ../../Users/User1/Downloads/node-v-10.zip 
+cat file1 > ../../Users/User1/Downloads/xampp.zip 
+cat file1 > ../../Users/User1/Pictures/foto11.jpg 
+cat file1 > ../../Users/User1/Pictures/foto12.jpg 
+cat file1 > ../../Users/User1/Pictures/foto13.jpg 
+cat file1 > ../../Users/User1/Pictures/foto14.jpg 
+cat file1 > ../../Users/User1/Pictures/foto15.jpg 
+cd ../../
+cd Users/
+cp -r User1/Desktop/ User2/Desktop/
+cd User2/
+ls
+cd Desktop/
+ls
+cd ..
+rm -r Desktop/
+ls
+cp -r User1/Desktop/ User2/
+cd ..
+cp -r User1/Desktop/ User2/
+cd User2/
+ls
+cd Desktop/
+ls
+touch AdobeXD 
+cd ..
+cp -r User1/Documents/ User2/
+cd User2/Documents/
+ls
+touch tesis-entrega-final.docx 
+cd ..
+ls
+cd..
+cd ..
+cp -r User1/Downloads/ User2/
+cp -r User1/Pictures/ User2/
+cp -r User1/Desktop/ User3/
+cp -r User1/Documents/ User3/
+cp -r User1/Downloads/ User3/
+cp -r User1/Pictures/ User3/
+cd User3/
+cd Downloads/
+touch filezilla.zip 
+cd ..
+cd Windows/app/
+cd .. 
+cat ../Programs/access/file1 > app/file6
+cat ../Programs/access/file1 > app/file7
+cat ../Programs/access/file1 > app/file8
+cat ../Programs/access/file1 > app/file9
+cat ../Programs/access/file1 > app/file10
+touch app/file8
+cat ../Programs/access/file1 > boot/file6
+cat ../Programs/access/file1 > boot/file7
+cat ../Programs/access/file1 > boot/file8
+cat ../Programs/access/file1 > boot/file9
+cat ../Programs/access/file1 > boot/file10
+touch boot/file10
+cat ../Programs/access/file1 > debug/file6
+cat ../Programs/access/file1 > debug/file7
+cat ../Programs/access/file1 > debug/file8
+cat ../Programs/access/file1 > debug/file9
+cat ../Programs/access/file1 > debug/file10
+touch debug/file6
+cat ../Programs/access/file1 > diagnostics/file6 
+cat ../Programs/access/file1 > diagnostics/file7
+cat ../Programs/access/file1 > diagnostics/file8
+cat ../Programs/access/file1 > diagnostics/file9
+cat ../Programs/access/file1 > diagnostics/file10
+touch diagnostics/file7
+cat ../Programs/access/file1 > System32/file11
+cat ../Programs/access/file1 > System32/file12
+cat ../Programs/access/file1 > System32/file13
+cat ../Programs/access/file1 > System32/file14
+cat ../Programs/access/file1 > System32/file15
+touch System32/file13
+cp -r app/ System32/
+touch System32/app/file1
+touch System32/app/file6
+rm System32/app/file1
+rm System32/app/file2
+rm System32/app/file3
+rm System32/app/file4
+rm System32/app/file5
+cp -r boot/ System32/
+cp -r debug/ System32/
+cp -r diagnostics/ System32/
+rm System32/boot/file1
+rm System32/boot/file2
+rm System32/boot/file3
+rm System32/boot/file4
+rm System32/boot/file5
+rm System32/debug/file1
+rm System32/debug/file2
+rm System32/debug/file3
+rm System32/debug/file4
+rm System32/debug/file5
+rm System32/diagnostics/file1
+rm System32/diagnostics/file2
+rm System32/diagnostics/file3
+rm System32/diagnostics/file4
+rm System32/diagnostics/file5
+touch System32/diagnostics/file7
+rm System32/help/file1
+rm System32/help/file2
+rm System32/help/file3
+rm System32/help/file4
+rm System32/help/file5
+cat ../Programs/access/file1 > System32/help/file6
+cat ../Programs/access/file1 > System32/help/file7
+cat ../Programs/access/file1 > System32/help/file8
+cat ../Programs/access/file1 > System32/help/file9
+cat ../Programs/access/file1 > System32/help/file10
+touch System32/help/file8
