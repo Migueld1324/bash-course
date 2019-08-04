0feeb44be6fd242ea12fd3a921e9b73b1f7ba2f9
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 16:53:15 -0500

    Added Sergio's homework1

---
+++ Sergio/sergio-bash-homework1/sergio-bash-homework1.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+
+history
+ls
+cd c
+ls
+cd programFiles/
+ls
+rmdir -r OFFICE/
+rmdir -rf OFFICE/
+rm -r OFFICE/
+ls
+rm -r cinco/
+rm -r cuatro/ tres/ dos/
+ls
+cd diez/
+ls
+cd ..
+cd nueve/
+ls
+cd ..
+cd ocho/
+ls
+cd ..
+ls
+cd seis/
+ls
+cd ..
+ls
+cd Windows/
+ls
+rm -r seis/
+ls
+cd diez/
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+cd ..
+cd nueve/
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+cd ..
+ls
+cd ocho/
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+cd ..
+ls
+cd siete/
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+cd ..
+ls
+cd System32/
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+cd diez/
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+cd ..
+cd nueve/
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+cd ..
+cd siete/
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+rm -r 15.txt 14.txt 13.txt 12.txt 11.txt 
+ls
+cd ..
+ls
+cd seis/
+ls
+rm -r 15.txt 14.txt 13.txt 12.txt 11.txt 
+ls
+rm -r 1.txt 2.txt 3.txt 4.txt 5.txt 
+ls
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+cd User/
+ls
+rm -r cancion10.mp3 cancion9.mp3 cancion8.mp3
+ls
+rm -r cancion7.mp3
+ls
+cd Sergio/
+ls
+rm -r Desktop/ Download/ Pictures/ Documents/
+ls
+cd ..
+cd Niko/
+rm -r Desktop/ Download/ Pictures/ Documents/
+ls
+cd ..
+cd Rabanito/
+rm -r Desktop/ Download/ Pictures/ Documents/
+cd ..
+cd Sergio/
+ls
+cd Music/
+ls
+rm -r AlejandroFernandez/ AlejandroSanz/ AlexSitenck/ Apocaliptica/ Dora/  
+ls
+rm -r VicenteFernandez/ Ramstein/ SystemOFADown/ Molotov/
+ls
+rm -r Moenia/
+rm -r music15.mp3 music14.mp3 music13.mp3 music12.mp3 music11.mp3 
+ls
+rm -r virus2.mp3 virus3.mp3 virus4.mp3 virus5.mp3 virus6.mp3 virus7.mp3 virus8.mp3 virus9.mp3 virus10.mp3 virus11.mp3 virus12.mp3 virus13.mp3 virus14.mp3 virus15.mp3 
+ls
+cd Adele/
+ls
+cd ..
+cd Adele/
+ls
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 
+ls
+cd ..
+cd EnriqueIglesias/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 
+cd ..
+cd FrancoDeVita/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 
+cd ..
+cd GrupoMarranos/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 
+cd ..
+cd Intocable/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 
+ls
+cd ..
+cd JonaLaExploradora/
+ls
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 cancion6.mp3 cancion7.mp3 cancion8.mp3 cancion9.mp3 cancion10.mp3 
+cd ..
+cd JoseJose/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 cancion6.mp3 cancion7.mp3 cancion8.mp3 cancion9.mp3 cancion10.mp3 
+ls
+cd ..
+cd MiAmista/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 cancion6.mp3 cancion7.mp3 cancion8.mp3 cancion9.mp3 cancion10.mp3 
+cd ..
+cd Nek/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 cancion6.mp3 cancion7.mp3 cancion8.mp3 cancion9.mp3 cancion10.mp3 
+cd ..
+cd RicardoArjona/
+rm -r cancion1.mp3 cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3 cancion6.mp3 cancion7.mp3 cancion8.mp3 cancion9.mp3 cancion10.mp3 
+cd ..
+rm -r virus.mp3 
+cd ..
+ls
+cd User/
+ls
+rm -r cancion2.mp3 cancion3.mp3 cancion4.mp3 cancion5.mp3  cancion6.mp3 
+ls
+touch file.txt
+nano file.txt 
+cp file.txt Niko/
+cp file.txt Sergio/
+cp file.txt Rabanito/
+cp file.txt Sergio/Area51/ Sergio/Meteorito/ Sergio/Music/ Sergio/Meteorito/ Sergio/Videos/ Sergio/Porno/
+cp file.txt Sergio/Meteorito/
+cp file.txt Sergio/Music/
+cp file.txt Sergio/Videos/
+cd Sergio/
+cd Music/
+ls
+rm -r muisc6.mp6 
+rm -r music1.mp3 
+ls
+rm -r music10.mp3 music2.mp3 music3.mp3 music4.mp3 music5.mp3 music7.mp3 music8.mp3 music9.mp3 
+ls
+cd Adele/
+ls
+touch RollungInTheDeep.txt
+nano RollungInTheDeep.txt 
+notepad RollungInTheDeep.txt 
+wordpad RollungInTheDeep.txt 
+nano RollungInTheDeep.txt 
+touch Adele-RollingInTheDeep.txt
+nano Adele-RollingInTheDeep.txt 
+touch Adele-SetFireToTheRain.txt
+nano Adele-SetFireToTheRain.txt 
+touch Adele-SomeoneLikeYou.txt
+nano Adele-SomeoneLikeYou.txt 
+cd ..
+ls
+cd EnriqueIglesias/
+cd ..
+cd Adele/
+touch EnriqueIglesias-Trapesista.txt
+nano EnriqueIglesias-Trapesista.txt 
+touch FrancoDeVita-tengo.txt
+nano FrancoDeVita-tengo.txt 
+touch GrupoMarranos-Ancioso.txt
+nano GrupoMarranos-Ancioso.txt 
+touch Intocable-HoyMeVasAPerder.txt
+nano Intocable-HoyMeVasAPerder.txt 
+touch JonaLaEcploradora.txt
+nano JonaLaEcploradora.txt 
+ls
+cd ..
+ls
+touch JoseJose-Desesperado.txt
+nano JoseJose-Desesperado.txt 
+touch MiAmista-MiAsmita
+nano MiAmista-MiAsmita
+touch Nek-TuNombre.txt
+nano Nek-TuNombre.txt 
+touch RicardoArjona-TuReputacion.txt
+nano RicardoArjona-TuReputacion.txt 
+cd ..
+ls
+cd vi
+cd Videos/
+ls
+cd ..
+ls
+rm -r Niko/
+ls
+rm -r Rabanito/
+mkdir Rabbanito
+mkdir Niko
+cd Niko/
+ls
+cd ..
+cd Sergio/
+ls
+cd ..
+cp Sergio/Area51/ Niko/
+cp -r Sergio/Area51/ Niko/
+cp -r Sergio/Meteorito/ Niko/
+cp -r Sergio/Music/ Niko/
+cp -r Sergio/Porno/ Niko/
+cp -r Sergio/Videos/ Niko/
+cp -r Sergio/Videos/ Rabbanito/
+cp -r Sergio/Porno/ Rabbanito/
+cp -r Sergio/Music/ Rabbanito/
+cp -r Sergio/Meteorito/ Rabbanito/
+cp -r Sergio/Area51/ Rabbanito/
+cp -r Sergio/Music/Adele/ Sergio/Music/EnriqueIglesias/
+cp -r Sergio/Music/Adele/Adele-RollingInTheDeep.txt
+cd Sergio/
+cs Music/
+cd Music/
+cd Adele/
+ls
+cp -r Adele-RollingInTheDeep.txt Adele-SetFireToTheRain.txt Adele-SomeoneLikeYou.txt EnriqueIglesias-Trapesista.txt FrancoDeVita-tengo.txt GrupoMarranos-Ancioso.txt Intocable-HoyMeVasAPerder.txt JonaLaEcploradora.txt RollungInTheDeep.txt c/User/Music/JoseJose
+cd ..
+cp -r Adele-RollingInTheDeep.txt Adele-SetFireToTheRain.txt Adele-SomeoneLikeYou.txt EnriqueIglesias-Trapesista.txt FrancoDeVita-tengo.txt GrupoMarranos-Ancioso.txt Intocable-HoyMeVasAPerder.txt JonaLaEcploradora.txt RollungInTheDeep.txt EnriqueIglesias/
+ls
+rm -r EnriqueIglesias/ FrancoDeVita/ GrupoMarranos/ Intocable/ JonaLaExploradora/ JoseJose/ MiAmista/ Nek/ RicardoArjona/
+ls
+cp -r Adele/ EnriqueIglesias
+ls
+cp -r Adele/ GrupoMarranos
+cp -r Adele/ JonaLaExploradora
+cp -r Adele/ GrupoOleguarro
+cp -r Adele/ Pink
+cp -r Adele/ Cuahutemocblanco
+cp -r Adele/ BatmanSongs
+cp -r Adele/ AmloMusic
+cp -r Adele/ vicenteFernandez
+ls
+rm -r JoseJose-Desesperado.txt MiAmista-MiAsmita Nek-TuNombre.txt RicardoArjona-TuReputacion.txt 
+ls
+cd ..
+ls
+rm -r Niko/ Rabbanito/
+cp -r Sergio/ Niko
+cp -r Sergio/ Rabbanito
+history -w Tarea1.txt
