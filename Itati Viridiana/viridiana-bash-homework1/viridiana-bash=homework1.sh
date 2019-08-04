4567d68b477a7db09566b23712fe86d70a6e7bfd
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 15:49:28 -0500

    Added Viridiana's homework 1

---
+++ Itati Viridiana/viridiana-bash-homework1/viridiana-bash=homework1.sh
@@ -0,0 +1,521 @@
+cd/
+cd /
+ls
+cd mnt
+ls
+cd c
+ls
+cd Users
+ls
+cd 'Lenovo 1'
+ls
+cd Documents/
+ls
+cd bash-course/
+ls
+clear
+cd ..
+ls
+cd C
+ls
+cd Programs/
+ls
+rm -r Carpeta1/ Carpeta2/ Carpeta3/ Carpeta4/ Carpeta5/
+cd ..
+cd Windows/
+ls
+rm -r Carpeta1/
+ls
+cd System32/
+ls
+cd ..
+ls
+cd Carpeta2/
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt 
+ls
+cd ..
+ls
+cd Carpeta3/
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt 
+ls
+cd ..
+cd Carpeta4/
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt 
+ls
+cd ..
+cd Carpeta5/
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt 
+cd ..
+cd System32/
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt 
+ls
+rm -r Archivo6.txt Archivo7.txt  Archivo8.txt Archivo9.txt  Archivo10.txt 
+ls
+cd Carpeta1
+ls
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt Archivo6.txt  Archivo7.txt Archivo8.txt  Archivo9.txt  Archivo10.txt 
+ls
+cd ..
+cd Carpeta2
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt Archivo6.txt  Archivo7.txt Archivo8.txt  Archivo9.txt  Archivo10.txt 
+ls
+cd ..
+cd Carpeta3/
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt Archivo6.txt  Archivo7.txt Archivo8.txt  Archivo9.txt  Archivo10.txt 
+ls
+cd ..
+cd Carpeta4/
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt Archivo6.txt  Archivo7.txt Archivo8.txt  Archivo9.txt  Archivo10.txt 
+ls
+cd ..
+cd Carpeta5/
+rm -r Archivo1.txt Archivo2.txt  Archivo3.txt Archivo4.txt  Archivo5.txt Archivo6.txt  Archivo7.txt Archivo8.txt  Archivo9.txt  Archivo10.txt 
+ls
+cd ..
+ls
+cd ..
+cd Users/
+ls
+cd UserOne/
+LS
+ls
+rm -r Carpeta1/ Carpeta2/ Carpeta3/ Desktop/
+ls
+cd ..
+UserTwo/
+cd UserTwo/
+ls
+rm -r Carpeta1/ Carpeta2/ Carpeta3/ Desktop/
+ls
+cd ..
+ls
+cd UseThree/
+ls
+rm -r Carpeta1/ Carpeta2/ Carpeta3/ Desktop/
+ls
+cd Documents/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+cd Downloads/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+cd Music/
+ls
+cd ..
+cd Video/
+ls
+rm -r video1.mp4  video2.mp4  video3.mp4 video4.mp4 video5.mp4 video6.mp4 video7.mp4 video8.mp4 video9.mp4 video10.mp4 
+ls
+cd ..
+cd UserOne/
+ls
+cd Documents/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+cd Downloads/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+Pictures/
+cd Pictures/
+ls
+rm -r Araña.png  Gato.jpg Leopardo.png  Nube.png  Perro.png  Sol.jpg  dedo.jpg  mesa.jpg  teclado.png  Cocodrilo.jpg Leon.png  Mariposa.jpg  Pajaro.jpg 
+ls
+cd Video/
+ls
+rm -r video1.mp4  video2.mp4  video3.mp4 video4.mp4 video5.mp4 video6.mp4 video7.mp4 video8.mp4 video9.mp4 video10.mp4 
+ls
+cd ..
+cd UseThree/
+cd Pictures/
+ls
+rm -r Araña.png  Gato.jpg Leopardo.png  Nube.png  Perro.png  Sol.jpg  dedo.jpg  mesa.jpg  teclado.png  Cocodrilo.jpg Leon.png  Mariposa.jpg  Pajaro.jpg 
+ls
+cd ..
+UserTwo/
+cd UserTwo/
+cd Documents/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+cd Downloads/
+ls
+rm -r Aguila.ppt  Burro.pdf Gato.txt Leopardo.ppt Oso.txt  Pato.pdf  Vaca.docx Araña.txt  
+ls
+cd ..
+cd Pictures/
+ls
+rm -r Araña.png  Gato.jpg Leopardo.png  Nube.png  Perro.png  Sol.jpg  dedo.jpg  mesa.jpg  teclado.png  Cocodrilo.jpg Leon.png  Mariposa.jpg  Pajaro.jpg 
+ls
+cd ..
+cd Video/
+ls
+rm -r video1.mp4  video2.mp4  video3.mp4 video4.mp4 video5.mp4 video6.mp4 video7.mp4 video8.mp4 video9.mp4 video10.mp4 
+ls
+cd ..
+cd Music/
+ls
+cd Alejandro_Sanz/ David_Bisbal/ Jennifer_Lopez/ Laura_Pausini/ Pablo_Alvoran/ Ricardo_Arjona/ Thalia/ Belinda/ Enrique_iglesias/ Juanes/ 
+rm -r Alejandro_Sanz/ David_Bisbal/ Jennifer_Lopez/ Laura_Pausini/ Pablo_Alvoran/ Ricardo_Arjona/ Thalia/ Belinda/ Enrique_iglesias/ Juanes/ 
+ls
+cd ..
+cd UserOne/
+cd Music/
+ls
+rm -r Alejandro_Sanz/ David_Bisbal/ Jennifer_Lopez/ Laura_Pausini/ Pablo_Alvoran/ Ricardo_Arjona/ Thalia/ Belinda/ Enrique_iglesias/ Juanes/ 
+ls
+cd ..
+cd UseThree/
+ls
+cd Music/
+ls
+rm -r Alejandro_Sanz/ David_Bisbal/ Jennifer_Lopez/ Laura_Pausini/ Pablo_Alvoran/ Ricardo_Arjona/ Thalia/ Belinda/ Enrique_iglesias/ Juanes/ 
+ls
+cd ..
+ls
+cd ..
+ls
+de Programs/
+cd Programs/
+ls
+cd Carpeta6/
+ls
+nano Archivo1.txt 
+cp -r Archivo1.txt Archivo2.txt 
+cd ..
+cd Carpeta7/
+cp -r Archivo1.txt Archivo2.txt 
+cd ..
+cp -r Carpeta6/. Carpeta7/
+cp -r Carpeta6/. Carpeta8/
+cp -r Carpeta6/. Carpeta9/
+cp -r Carpeta6/. Carpeta10}/
+cp -r Carpeta6/. Carpeta10/
+cd ..
+cd Users/
+ls
+UserOne/
+cd UserOne/
+ls
+nano Documents/
+cat Documents/
+cd Documents/
+ls
+nano Gallina.doc 
+cp -r Gallina.doc  Leon.xls 
+cp -r Gallina.doc  Mariposa.pdf 
+cp -r Gallina.doc  Pajaro.ppt 
+cp -r Gallina.doc  Perro.docx 
+cat Leon.xls 
+cd ..
+cp -r Documents/. Downloads/
+cd ..
+cd UserTwo/
+ls
+cd Documents/
+ls
+nano Gallina.doc 
+cp -r Gallina.doc  Leon.xls 
+cp -r Gallina.doc  Mariposa.pdf 
+cp -r Gallina.doc  Pajaro.ppt 
+cp -r Gallina.doc  Perro.docx 
+cd ..
+cd UseThre
+cd UseThree/
+ls
+cd Documents/
+ls
+nano Gallina.doc 
+cp -r Gallina.doc  Perro.docx 
+cp -r Gallina.doc  Pajaro.ppt 
+cp -r Gallina.doc  Mariposa.pdf 
+cp -r Gallina.doc  Leon.xls 
+cd ..
+cd Windows/
+ls
+cd Carpeta2/
+ls
+nano Archivo6.txt 
+cp -r Archivo6.txt  Archivo7.txt 
+cp -r Archivo6.txt  Archivo8.txt 
+cp -r Archivo6.txt  Archivo9.txt 
+cp -r Archivo6.txt  Archivo10.txt 
+cd ..
+cp -r Carpeta2/. Carpeta3/
+cp -r Carpeta2/. Carpeta4/
+cp -r Carpeta2/. Carpeta5/
+cd System32/
+cd Carpeta1/
+nano Archivo11.txt 
+cp -r Archivo11.txt  Archivo12.txt
+cp -r Archivo11.txt  Archivo13.txt
+cp -r Archivo11.txt  Archivo14.txt
+cp -r Archivo11.txt  Archivo15.txt
+cd ..
+cp -r Carpeta1/. Carpeta2/
+cp -r Carpeta1/. Carpeta3/
+cp -r Carpeta1/. Carpeta4/
+cp -r Carpeta1/. Carpeta5/
+nano Archivo11.txt 
+cp -r Archivo11.txt  Archivo12.txt
+cp -r Archivo11.txt  Archivo13.txt
+cp -r Archivo11.txt  Archivo14.txt
+cp -r Archivo11.txt  Archivo15.txt
+cd ..
+ls
+cd ..
+ls
+cd Users/
+ls
+cd UserOne
+ls
+cd Music/
+ls
+mv track
+mv track1.mp3 'Chayanne-Te pido perdon.txt'
+mv track3.mp3 'Me enamore de ti.txt'
+cat >> 'Chayanne-Me enamore de ti.txt'
+mv 'Me enamore de ti.txt' 'Chayanne-Me enamore de ti.txt'
+mv track4.mp3 'Chayanne-Un siglo sin ti'
+cat >> 'Chayanne-Un siglo sin ti' 
+cat >> 'Chayanne-Un siglo sin ti.txt' 
+mv track5.mp3 'Chayanne-Atado a tu amor.txt'
+cat >> 'Chayanne-Atado a tu amor.txt' 
+cd ..
+cd Gloria_Trevi/
+mv track1.mp3 'Gloria Trevi-Todos me miran.txt'
+ 
+mv track2.mp3 'Gloria Trevi-Soledad'
+cat >> 'Gloria Trevi-Soledad.txt'
+mv track3.mp3 'Gloria Trevi-con los ojos cerrados.txt'
+cat >> 'Gloria Trevi-con los ojos cerrados.txt' 
+
+cat >> 'Gloria Trevi-Rompeme el corazon.txt' 
+mv track5.mp3 'Gloria Trevi-Besame aqui.txt'
+cat >> 'Gloria Trevi-Besame aqui.txt' 
+history -w Respaldo.txt
+cd ..
+cd Lady_Gaga/
+mv track1.mp3 'Lady Gaga-Shallow.txt'
+cat >> 'Lady Gaga-Shallow.txt' 
+
+cat >> 'Lady Gaga-Always Remember Us This Way.txt' 
+mv track3.mp3 'Lady Gaga-Ill Never Love Again.txt' >> 'Lady Gaga-Ill Never Love Again.txt' 
+mv track3.mp3 'Lady Gaga-Ill Never Love Again.txt'
+
+mv track3.mp3 'Lady Gaga-Ill Never'
+mv track3.mp3 'Lady Gaga-Bad Romance.txt'
+ 
+mv track4.mp3 'Lady Gaga-Poker face.txt'
+cat >> 'Lady Gaga-Poker face.txt' 
+mv track5.mp3 'Lady Gaga-Born this way.txt'
+cat >> 'Lady Gaga-Born this way.txt' 
+mv track5.mp3 'Marc_Anthony-flor palida.txt'
+cd ..
+cd Marc_Anthony/
+mv track5.mp3 'Marc_Anthony-flor palida.txt'
+cat >> 'Marc_Anthony-flor palida.txt' 
+
+cat >> 'Marc_Anthony-Hasta ayer.txt' 
+mv track3.mp3 'Marc_Anthony-Vivir mi vida.txt'
+cat >> 'Marc_Anthony-Vivir mi vida.txt' 
+mv track2.mp3 'Marc_Anthony-Muy dentro de mi.txt'
+cat >> 'Marc_Anthony-Muy dentro de mi.txt' 
+mv track2.mp1 'Marc_Anthony-Vivir lo nuestro.txt'
+mv track1.mp3 'Marc_Anthony-Vivir lo nuestro.txt'
+cat >> 'Marc_Anthony-Vivir lo nuestro.txt' 
+cd ..
+cd Michael_jackson/
+mv track5.mp3 'Michael_jackson-I want you back.txt'
+
+mv track4.mp3 'Michael_jackson-ABC.txt'
+cat >> 'Michael_jackson-ABC.txt' 
+mv track3.mp3 'Michael_jackson-The love you save.txt'
+cat >> 'Michael_jackson-The love you save.txt' 
+mv track2.mp3 'Michael_jackson-Mamas pearl.txt'
+mv track2.mp3 'Michael_jackson-Mama´s pearl.txt'
+mv track1.mp3 'Michael_jackson-Mama s pearl.txt'
+cat >> 'Michael_jackson-Mama s pearl.txt' 
+cd ..
+ls
+cd Paulina_Rubio/
+mv track1.mp3 'Paulina_Rubio-Ya no me engañas.txt'
+mv track1.mp3 'Paulina_Rubio-Ya no me engañas.txt'
+cat >> 'Paulina_Rubio-Ya no me engañas.txt' 
+mv track2.mp3 'Paulina_Rubio-Ni una sola palabra.txt'
+cat >> 'Paulina_Rubio-Ni una sola palabra.txt' 
+mv track3.mp3 'Paulina_Rubio-Tal vez, quiza.txt'
+cat >> 'Paulina_Rubio-Tal vez, quiza.txt' 
+mv track4.mp3 'Paulina_Rubio-Ni rosas ni juguetes.txt'
+cat >> 'Paulina_Rubio-Ni rosas ni juguetes.txt' 
+mv track5.mp3 'Paulina_Rubio-Causa y efecto.txt'
+cat >> 'Paulina_Rubio-Causa y efecto.txt' 
+cd ..
+cd Pablo_Montero/
+mv track5.mp3 'Pablo_Montero-Y me he equivocado.txt'
+cat >> 'Pablo_Montero-Y me he equivocado.txt' 
+mv track4.mp3 'Pablo_Montero-Que voy hacer sin ti.txt'
+cat >> 'Pablo_Montero-Que voy hacer sin ti.txt' 
+mv track3.mp3 'Pablo_Montero-Hay otra en tu lugar.txt'
+cat >> 'Pablo_Montero-Hay otra en tu lugar.txt' 
+mv track4.mp3 'Pablo_Montero-Piquito de oro.txt'
+mv track1.mp3 'Pablo_Montero-Piquito de oro.txt'
+cat >> 'Pablo_Montero-Piquito de oro.txt' 
+mv track2.mp3 'Pablo_Montero-Dicen por ahi.txt'
+cat >> 'Pablo_Montero-Dicen por ahi.txt' 
+cd ..
+cd Ricky_Martin/
+mv track2.mp3 'Ricky_Martin-Pegate.txt'
+cat >> 'Ricky_Martin-Pegate.txt' 
+mv track3.mp3 'Ricky_Martin-Lo mejor de mi vida este tu.txt'
+cat >> 'Ricky_Martin-Lo mejor de mi vida este tu.txt' 
+mv track1.mp3 'Ricky_Martin-Tal vez.txt'
+cat >> 'Ricky_Martin-Tal vez.txt' 
+mv track4.mp3 'Ricky_Martin-Vuelve.txt'
+cat >> 'Ricky_Martin-Vuelve.txt' 
+mv track5.mp3 'Ricky_Martin-Disparo al corazon.txt'
+cat >> 'Ricky_Martin-Disparo al corazon.txt' 
+cd ..
+cd Shakira/
+ls
+mv track5.mp3 'Shakira-Antologia.txt'
+cat >> 'Shakira-Antologia.txt' 
+mv track4.mp3 'Shakira-Me enamore.txt'
+mv track4.mp3 'Shakira-Me enamore.txt'
+cat >> 'Shakira-Me enamore.txt' 
+mv track3.mp3 'Shakira-estoy aqui.txt'
+cat >> 'Shakira-estoy aqui.txt' 
+mv track1.mp3 'Shakira-la tortura.txt'
+cat >> 'Shakira-la tortura.txt' 
+mv track2.mp3 'Shakira-inevitable.txt'
+cat >> 'Shakira-inevitable.txt' 
+cd ..
+cd Wisin_Yandel/
+mv track2.mp3 'Wisin_Yandel-estoy enamorado.txt'
+cat >> 'Wisin_Yandel-estoy enamorado.txt' 
+
+cat >> 'Wisin_Yandel-Gracias a ti.txt' 
+mv track3.mp3 'Wisin_Yandel-algo me gusta de ti.txt'
+cat >> 'Wisin_Yandel-algo me gusta de ti.txt' 
+mv track5.mp3 'Wisin_Yandel-Yo te quiero.txt'
+
+mv track4.mp3 'Wisin_Yandel-estoy enamorado.txt'
+cat >> 'Wisin_Yandel-estoy enamorado.txt'
+mv 'Shakira-inevitable.txt' 'el telefono.txt'
+cd ..
+ls
+cd Chayanne/
+ls
+mv 'Amada amante.txt'
+cat >> 'Chayanne-Amada amante.txt'
+cat >> 'Chayanne-Un siglo sin ti.txt'
+cat >> 'Chayanne-A donde van tus sueños.txt'
+cat >> 'Chayanne-A donde van tus sueños.txt'
+cat >> 'Chayanne-Madre tierra.txt'
+cat >> 'Chayanne-Torero.txt'
+cat >> 'Chayanne-Ay Mama.txt'
+cd ..
+
+ls
+cat >> 'Gloria_Trevi-A media calle.txt'
+cat >> 'Gloria_Trevi-Bipolar.txt'
+cat >> 'Gloria_Trevi-Cama y mesa.txt.'
+cat >> 'Gloria_Trevi-Cama y mesa.txt'
+cat >> 'Gloria_Trevi-Aurora.txt'
+cat >> 'Gloria_Trevi-Caro.txt'
+cd ..
+cd Lady_Gaga/
+cat >> 'Lady Gaga-Alejandro.txt'
+cat >> 'Lady Gaga-Love Game.txt'
+cat >> 'Lady Gaga-Love Game.txt'
+cat >> 'Lady Gaga-You and I.txt'
+cat >> 'Lady Gaga-Appause.txt'
+cat >> 'Lady Gaga-The cure.txt'
+cd ..
+cd Marc_Anthony/
+cat >> 'Marc_Anthony-Abrazame muy fuerte.txt'
+cat >> 'Marc_Anthony-Abrazame muy fuerte.txt'
+cat >> 'Marc_Anthony-Celos.txt'
+cat >> 'Marc_Anthony-Celos.txt'
+cat >> 'Marc_Anthony-Aunque me voy.txt'
+cat >> 'Marc_Anthony-Caminar.txt'
+cat >> 'Marc_Anthony-De la vuelta.txt'
+cat >> 'Marc_Anthony-De la vuelta.txt'
+ls
+cd ..
+cd Michael_jackson/
+cat >> 'Michael_jackson-Beat it.txt'
+cat >> 'Michael_jackson-All I need.txt'
+cat >> 'Michael_jackson-'
+cat >> 'Michael_jackson-2000 watts.txt'
+ls
+cat >> 'Michael_jackson-Al capone.txt'
+ls
+cd ..
+cd Pablo_Montero/
+cat >> 'Pablo_Montero-Con ganas de amar.txt'
+cat >> 'Pablo_Montero-Despreciame.txt'
+cat >> 'Pablo_Montero-Entrega total.txt'
+cat >> 'Pablo_Montero-Doce rosas.txt'
+cat >> 'Pablo_Montero-Te odio.txt'
+cd ..
+cd Paulina_Rubio/
+cat >> 'Paulina_Rubio-Algo de ti.txt'
+cat >> 'Paulina_Rubio-Algo tienes.txt'
+cat >> 'Paulina_Rubio-Amor secreto.txt'
+cat >> 'Paulina_Rubio-Aun.txt'
+cat >> 'Paulina_Rubio-Compañia.txt'
+cd ..
+cd Ricky_Martin/
+ls
+cat >> 'Ricky_Martin-Ayudame.txt'
+cat >> 'Ricky_Martin-Corazon.txt'
+cat >> 'Ricky_Martin-Buenos aires.txt'
+cat >> 'Ricky_Martin-Diana.txt'
+cat >> 'Ricky_Martin-El ultimo adios.txt'
+cd..
+cd ..
+cd Shakira/
+cat >> 'Shakira-Brujeria.txt'
+cat >> 'Shakira-Cazador de amor.txt'
+cat >> 'Shakira-Dia de enero.txt'
+cat >> 'Shakira-Amor.txt'
+cat >> 'Shakira-El oasis.txt'
+cd ..
+cd Wisin_Yandel/
+cat >> 'Wisin_Yandel-El telefono.txt'
+cat >> 'Wisin_Yandel-Algo paso.txt'
+cat >> 'Wisin_Yandel-Adrenalina.txt'
+cat >> 'Wisin_Yandel-Avanza.txt'
+cat >> 'Wisin_Yandel-Bailame.txt'
+cat >> 'Wisin_Yandel-Ahora es.txt'
+cd..
+cd ..
+ls
+cd ..
+history -w Viridiana-bash-excercise2.1.txt
+clear
+cd ..
+rm -r UserTwo/
+rm -r UseThree/
+ls
+cp -r UserOne/. UserTwo/
+cp -r UserOne/. UserThree/
+ls
+cd ..
+history Viridiana-bash-homework.tx
+history -w Viridiana-bash-homework.tx
+history -w Viridiana-bash-homework.txt
