bd1d79f290ee0d806cff8b94c824acb82171cdc3
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 18:09:13 -0500

    Added Ricardo's exerise 2

---
+++ Ricardo/ricardo-bash-exercise2/ricardo-bash-exercise.sh
@@ -0,0 +1,341 @@
+sudo groupadd admin2
+sudo groupadd guest
+adduser user1
+sudo adduser user1
+adduser user2
+sudo adduser user2
+sudo adduser user3
+sudo adduser owner
+user
+users
+users --help
+ls
+chmod --help
+chown --help
+ls
+sudo chown -R owner:admin2 programs/
+ls -la
+sudo chown -R owner:admin2 windows/
+sudo chown -R owner:guest windows/
+cd users/
+chown -R usuario1 Ricardo/
+chown -R user1 Ricardo/
+sudo chown -R user1 Ricardo/
+sudo chown -R user2 Emmanuel/
+sudo chown -R user3 Hector/
+ls
+cd Ricardo
+cd music/
+ls
+cd top
+cd ..
+cd ed_sheeran/
+ls
+history
+ls
+cd ..
+okokok
+
+ls -la
+ls -l
+
+ls
+cd ed_sheeran/
+
+ls
+ls -l
+chmod 400 ateam 
+ls -l
+chmod 600 coldcoffee 
+chmod 700 dont
+chmod 740 givemelove
+chmod 760 kissme 
+chmod 770 legohouse 
+chmod 774 one
+chmod 776 shapeofyou
+chmod 777 she 
+ls-l
+ls -l
+chmod 776 she 
+chmod 400 ateam
+chmod 600 coldcoffee 
+chmod 700 dont 
+chmod 740 givemelove 
+chmod 760 kissme 
+chmod 770 legohouse 
+chmod 774 one 
+chmod 777 sing 
+chmod 776 she 
+chmod 774 shapeofyou 
+chmod 770 one 
+chmod 760 legohouse 
+chmod 740 kissme 
+chmod 700 givemelove 
+chmod 600 dont 
+chmod 400 coldcoffee 
+chmod 000 ateam 
+ls -l
+cd ..
+ls
+cd top
+okokok
+ls
+
+ls
+ls -l
+schmod 777 chlorine
+chmod 776 fairlylocal
+chmod 774 heathens
+chmod 770 houseofgold
+chmod 760 laneboy
+chmod 740 myblood
+chmod 700 ride
+chmod 600 stressedout
+chmod 400 truce
+chmod 000 wdbwotvchmod 777 chlorine
+chmod 776 fairlylocal
+chmod 774 heathens
+chmod 770 houseofgold
+chmod 760 laneboy
+chmod 740 myblood
+chmod 700 ride
+chmod 600 stressedout
+chmod 400 truce
+chmod 000 wdbwotv
+chmod 777 chlorine
+chmod 776 fairlylocal
+chmod 774 heathens
+chmod 770 houseofgold
+chmod 760 laneboy
+chmod 740 myblood
+chmod 700 ride
+chmod 600 stressedout
+chmod 400 truce
+chmod 000 wdbwotv ls -l
+ls -l
+chmod 777 wdbwotv
+chmod 776 truce
+chmod 774 stressedout
+chmod 770 ride
+chmod 760 myblood
+chmod 740 laneboy
+chmod 700 houseofgold
+chmod 600 heathens
+chmod 400 fairlylocal
+chmod 000 chlorine
+ls -l
+cd ..
+ls
+cd kodaline/
+ls -l
+okokok
+
+ls -l
+touch highhopes
+ls -l
+chmod 777 theone
+chmod 776 theanswer
+chmod 774 ready
+chmod 770 raging
+chmod 760 oneday
+chmod 740 movingon
+chmod 700 lovelikethis
+chmod 600 highhopes
+chmod 400 brother
+chmod 000 alliwant
+ls -l
+cd ..
+ls
+cd black_keys/
+
+ls -l
+chmod 777 weightoflove
+chmod 776 toafraidtoloveyou
+chmod 774 tightenup
+chmod 770 theonlyone
+chmod 760 nextgirl
+chmod 740 lonelyboy
+chmod 700 littleblacksubmarines
+chmod 600 goldontheceiling
+chmod 400 go
+chmod 000 fever
+ls -l
+cd ..
+ls
+cd arctic_monkeys/
+ls
+okokok
+
+ls -l
+touch onlyones
+ls -l
+chmod 777 wyocmwyh
+chmod 776 sooi
+chmod 774 rumine
+chmod 770 onlyones
+chmod 760 mardybum
+chmod 740 kneesox
+chmod 700 fouroutoffive
+chmod 600 doiwannaknow
+chmod 400 arabella.mp4
+chmod 000 505.mp4
+ls -l
+cd ..
+cd ../../
+cd Emmanuel/music/
+cd top
+
+chmod 777 wdbwotv
+chmod 776 truce
+chmod 774 stressedout
+chmod 770 ride
+chmod 760 myblood
+chmod 740 laneboy
+chmod 700 houseofgold
+chmod 600 heathens
+chmod 400 fairlylocal
+chmod 000 chlorine
+cd ..
+cd ed_sheeran/
+
+chmod 777 sing
+chmod 776 she
+chmod 774 shapeofyou
+chmod 770 one
+chmod 760 legohouse
+chmod 740 kissme
+chmod 700 givemelove
+chmod 600 dont
+chmod 400 coldcoffee
+chmod 000 ateam
+cd ..
+cd kodaline/
+
+chmod 777 theone
+chmod 776 theanswer
+chmod 774 ready
+chmod 770 raging
+chmod 760 oneday
+chmod 740 movingon
+chmod 700 lovelikethis
+chmod 600 highhopes
+chmod 400 brother
+chmod 000 alliwant
+cd ..
+cd black_keys/
+
+chmod 777 weightoflove
+chmod 776 toafraidtoloveyou
+chmod 774 tightenup
+chmod 770 theonlyone
+chmod 760 nextgirl
+chmod 740 lonelyboy
+chmod 700 littleblacksubmarines
+chmod 600 goldontheceiling
+chmod 400 go
+chmod 000 fever
+cd ..
+cd arctic_monkeys/
+
+chmod 777 wyocmwyh
+chmod 776 sooi
+chmod 774 rumine
+chmod 770 onlyones
+chmod 760 mardybum
+chmod 740 kneesox
+chmod 700 fouroutoffive
+chmod 600 doiwannaknow
+chmod 400 arabella.mp4
+chmod 000 505.mp4
+cd ../../..
+cd Hector/
+cd music/
+cd arctic_monkeys/
+
+chmod 777 wyocmwyh
+chmod 776 sooi
+chmod 774 rumine
+chmod 770 onlyones
+chmod 760 mardybum
+chmod 740 kneesox
+chmod 700 fouroutoffive
+chmod 600 doiwannaknow
+chmod 400 arabella.mp4
+chmod 000 505.mp4
+cd ..
+cd black_keys/
+
+chmod 777 weightoflove
+chmod 776 toafraidtoloveyou
+chmod 774 tightenup
+chmod 770 theonlyone
+chmod 760 nextgirl
+chmod 740 lonelyboy
+chmod 700 littleblacksubmarines
+chmod 600 goldontheceiling
+chmod 400 go
+chmod 000 fever
+cd ..
+
+chmod 777 theone
+chmod 776 theanswer
+chmod 774 ready
+chmod 770 raging
+chmod 760 oneday
+chmod 740 movingon
+chmod 700 lovelikethis
+chmod 600 highhopes
+chmod 400 brother
+chmod 000 alliwant
+rm alliwant brother lovelikethis movingon oneday raging ready theanswer theone highhopes
+y
+ls
+cd kodaline/
+chmod 777 theone
+chmod 776 theanswer
+chmod 774 ready
+chmod 770 raging
+chmod 760 oneday
+chmod 740 movingon
+chmod 700 lovelikethis
+chmod 600 highhopes
+chmod 400 brother
+chmod 000 alliwant
+cd ..
+cd ed_sheeran/
+
+chmod 777 sing
+chmod 776 she
+chmod 774 shapeofyou
+chmod 770 one
+chmod 760 legohouse
+chmod 740 kissme
+chmod 700 givemelove
+chmod 600 dont
+chmod 400 coldcoffee
+chmod 000 ateam
+cd ..
+cd top
+
+chmod 777 wdbwotv
+chmod 776 truce
+chmod 774 stressedout
+chmod 770 ride
+chmod 760 myblood
+chmod 740 laneboy
+chmod 700 houseofgold
+chmod 600 heathens
+chmod 400 fairlylocal
+chmod 000 chlorine
+cd ..
+ls arctic_monkeys/
+ls -a arctic_monkeys/
+ls -l arctic_monkeys/
+ls -l black_keys/
+ls -l ed_sheeran/
+ls- l kodaline/
+ls -l kodaline/
+ls -l top
+cd ../../..
+cd ..
+history -w ricardo-bash-excersise2
