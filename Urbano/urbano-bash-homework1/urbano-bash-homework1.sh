a87c6b5bbd74fa727d7108aab52e7150d125ccaa
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 17:06:02 -0500

    Added Urbano's homework1

---
+++ Urbano/urbano-bash-homework1/urbano-bash-homework1.sh
@@ -0,0 +1,552 @@
+#!/bin/bash
+
+cd Programs/
+ls
+rm -rf folder10 folder9 folder8 folder7 folder6
+ls
+cd ..
+cd Windows/
+ls
+cd rm -rf folder5
+rm -rf folder5
+ls
+rm folder4/{file10,file9}
+rm folder4{file10,file9}
+cd folder
+cd folderls
+ls
+cd folder4
+rm file10 file9 file8 file7 file6
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+ls
+cd ..
+ls
+cd folder2/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+cd folder3/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+ls folder4
+ls
+cd System32/
+ls
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+ls
+rm file11.txt file12.txt file13.txt file14.txt file15.txt 
+ls
+rm -rf file13txt 
+ls
+cd folder
+cd folder1
+ls
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+cd folder2
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+cd folder3
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+cd folder4
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+cd ..
+cd folder5
+rm file10.txt file9.txt file8.txt file7.txt file6.txt 
+ls
+cd ..
+ls
+cd ...
+ls
+cd ..
+ls
+cd..
+cd ..
+ls
+cd Programs/
+ls
+rm file1.txt file2 
+ls
+cd folder1/
+nano file1.txt 
+cat file1.txt 
+cat file1.txt >> file2.txt 
+cat file1.txt >> file3.txt 
+cat file3.txt 
+cat file1.txt >> file4.txt 
+cat file1.txt >> file5.txt 
+ls
+cd ..
+ls
+cd folder2/
+nano file1.txt 
+cat file1.txt >> file2.txt 
+cat file1.txt >> file3.txt 
+cat file1.txt >> file4.txt 
+cat file1.txt >> file5.txt 
+ls
+cat file5.txt }
+cat file5.txt 
+cat file1.txt >> ../folder3/file5.txt 
+cat file1.txt >> ../folder3/file4.txt 
+cat file1.txt >> ../folder3/file3.txt 
+cat file1.txt >> ../folder3/file2.txt 
+cat file1.txt >> ../folder3/file1.txt 
+cat file1.txt >> ../folder4/file1.txt 
+cat file1.txt >> ../folder4/file2.txt 
+cat file1.txt >> ../folder4/file3.txt 
+cat file1.txt >> ../folder4/file4.txt 
+cat file1.txt >> ../folder4/file5.txt 
+cat file1.txt >> ../folder5/file5.txt 
+cat file1.txt >> ../folder5/file4.txt 
+cat file1.txt >> ../folder5/file3.txt 
+cat file1.txt >> ../folder5/file2.txt 
+cat file1.txt >> ../folder5/file1.txt 
+cd ..
+cd folder2/
+ls
+cat file1.txt 
+cat file5.txt 
+cd ..
+cat folder5/file5.txt 
+ls
+cd ..
+ls
+cd Programs/
+ls
+cd folder1/
+cat file1.txt >> ../../Windows/folder1/file1
+cat file1.txt >> ../../Windows/folder1/file2
+cat file1.txt >> ../../Windows/folder1/file3
+cat file1.txt >> ../../Windows/folder1/file4
+cat file1.txt >> ../../Windows/folder1/file
+cat file1.txt >> ../../Windows/folder1/file5
+rm ../../Windows/folder1/file
+rm ../../Windows/folder1/file6
+rm ../../Windows/folder1/file6.txt 
+rm ../../Windows/folder1/file7.txt 
+rm ../../Windows/folder1/file8.txt 
+rm ../../Windows/folder1/file9.txt 
+rm ../../Windows/folder1/file10.txt 
+ls../../Windows/folder1/file
+cat file1.txt >> ../../Windows/folder2/
+cat file1.txt >> ../../Windows/folder2/file1
+cat file1.txt >> ../../Windows/folder2/file2
+cat file1.txt >> ../../Windows/folder2/file3
+cat file1.txt >> ../../Windows/folder2/file
+rm ../../Windows/folder2/file1
+rm ../../Windows/folder2/file2
+rm ../../Windows/folder2/file3
+rm ../../Windows/folder2/file4
+rm ../../Windows/folder2/file5
+cat file1.txt >> ../../Windows/folder2/file1.txt 
+cat file1.txt >> ../../Windows/folder2/file2.txt 
+cat file1.txt >> ../../Windows/folder2/file3.txt 
+cat file1.txt >> ../../Windows/folder2/file4.txt 
+cat file1.txt >> ../../Windows/folder2/file.txt 
+cat file1.txt >> ../../Windows/folder2/file5.txt 
+rm ../../Windows/folder2/file
+rm ../../Windows/folder2/file.txt 
+cat file1.txt >> ../../Windows/folder3/file5.txt 
+cat file1.txt >> ../../Windows/folder3/file4.txt 
+cat file1.txt >> ../../Windows/folder3/file3.txt 
+cat file1.txt >> ../../Windows/folder3/file2.txt 
+cat file1.txt >> ../../Windows/folder3/file1.txt 
+cat file1.txt >> ../../Windows/folder4/file1.txt 
+cat file1.txt >> ../../Windows/folder4/file2.txt 
+cat file1.txt >> ../../Windows/folder4/file3.txt 
+cat file1.txt >> ../../Windows/folder4/file4.txt 
+cat file1.txt >> ../../Windows/folder4/file5.txt 
+cd ..
+ls
+cat folder5/file5.txt 
+cd ..
+ls
+cd users
+cd Users/
+cd
+cd Documents/C/Windows/folder1/
+ls
+rm -rf file1 file2 file3 file4 file5
+ls
+cd ..
+cd System32/
+ls
+nano file1.txt 
+cat file1.txt >> file2.txt 
+cat file1.txt >> file3.txt 
+cat file1.txt >> file4.txt 
+cat file1.txt >> file5.txt 
+cat file1.txt >> folder5/file1.txt 
+cat file1.txt >> folder5/file2.txt 
+cat file1.txt >> folder5/file3.txt 
+cat file1.txt >> folder5/file4.txt 
+cat file1.txt >> folder5/file5.txt 
+cat file1.txt >> folder4/file5.txt 
+cat file1.txt >> folder4/file4.txt 
+cat file1.txt >> folder4/file3.txt 
+cat file1.txt >> folder4/file2.txt 
+cat file1.txt >> folder4/file1.txt 
+cat file1.txt >> folder3/file1.txt 
+cat file1.txt >> folder3/file2.txt 
+cat file1.txt >> folder3/file3.txt 
+cat file1.txt >> folder3/file4.txt 
+cat file1.txt >> folder3/file5.txt 
+cat file1.txt >> folder2/file5.txt 
+cat file1.txt >> folder2/file4.txt 
+cat file1.txt >> folder2/file3.txt 
+cat file1.txt >> folder2/file2.txt 
+cat file1.txt >> folder2/file1.txt 
+cat file1.txt >> folder1/file1.txt 
+cat file1.txt >> folder1/file2.txt 
+cat file1.txt >> folder1/file3.txt 
+cat file1.txt >> folder1/file2.txt 
+cat file1.txt >> folder1/file3.txt 
+cat file1.txt >> folder1/file4.txt 
+cat file1.txt >> folder1/file5.txt 
+cat file1.txt >> ../folder1/file1.txt 
+cat file1.txt >> ../folder1/file2.txt 
+cat file1.txt >> ../folder1/file3.txt 
+cat file1.txt >> ../folder1/file4.txt 
+cat file1.txt >> ../folder1/file5.txt 
+cd ..
+cd..
+cd ..
+ls
+cd Users/
+ls
+cd urbano/
+ls
+cd Desktop/
+ls
+nano CallOfDuty.ink 
+cat CallOfDuty.ink >> Discord.ink 
+cat CallOfDuty.ink >> Edge.ink 
+cat CallOfDuty.ink >> Google_Chorme.ink 
+cat CallOfDuty.ink >> Illustrator.ink 
+cat CallOfDuty.ink >> MiEquipo.ink 
+cat CallOfDuty.ink >> NetBins.ink 
+cat CallOfDuty.ink >> PapeleraDeReciclaje.ink 
+cat CallOfDuty.ink >> PhotoShop.ink 
+cat CallOfDuty.ink >> PhpStrom.ink 
+cat CallOfDuty.ink >> PowerPoint.ink 
+cat CallOfDuty.ink >> Skype.ink 
+cat CallOfDuty.ink >> SonyVegas.ink 
+cat CallOfDuty.ink >> Sublime.ink 
+cat CallOfDuty.ink >> VisualStudioCode.ink 
+cat CallOfDuty.ink >> Word.ink 
+cat CallOfDuty.ink >> ../Documents/Exponentes.docx 
+cat CallOfDuty.ink >> ../Documents/Problematica.pptx 
+cat CallOfDuty.ink >> ../Documents/ensayoFilosofia.docx.pptx 
+cat CallOfDuty.ink >> ../Documents
+rm ../Documents/ensayoFilosofia.docx.pptx 
+ls
+cat CallOfDuty.ink >> ../Documents/expoProyecto.pptx 
+cat CallOfDuty.ink >> ../Documents/hipotesis.pptx 
+cat CallOfDuty.ink >> ../Documents/marcoteorico.pptx 
+cat CallOfDuty.ink >> ../Documents/presentacionObjetivos.pptx 
+cat CallOfDuty.ink >> ../Documents/puntoEquilibrio.xls 
+cat CallOfDuty.ink >> ../Documents/tareaCalculo
+cat CallOfDuty.ink >> ../Documents/tareaCalculo.docx 
+rm  ../Documents/tareaCalculo
+cat CallOfDuty.ink >> ../Documents/tareaCalculo2.docx 
+cat CallOfDuty.ink >> ../Documents/tareaEcuacionesDif.docx 
+cat CallOfDuty.ink >> ../Documents/tareaFiananzas.xls 
+cat CallOfDuty.ink >> ../Documents/tareaFisica.docx 
+cat CallOfDuty.ink >> ../Documents/tareaViernes.docx 
+cat CallOfDuty.ink >> ../Documents/trucosGTA_SA.docx 
+ls
+cd ...
+cd ..
+ls
+cat Documents/tareaEcuacionesDif.docx 
+ls
+cd Downloads/
+ls
+nano Antena.pptx 
+cat Antena.pptx >> Anteproyecto.docx 
+cat Antena.pptx >> CableDeFibraExt.pdf 
+cat Antena.pptx >> CableDeRed.pdf 
+cat Antena.pptx >> VEGAS16_261.rar 
+cat Antena.pptx >> YDKWJS.pdf 
+cat Antena.pptx >> antenaProyecto.jpg 
+cat Antena.pptx >> bootstrap_4.zip 
+cat Antena.pptx >> bug.png 
+cat Antena.pptx >> demo.rar 
+cat Antena.pptx >> fotoTarea2.jpg 
+cat Antena.pptx >> progit.pdf 
+cat Antena.pptx >> reporte.xls 
+cat Antena.pptx >> tarjetaNSS.pdf 
+cat Antena.pptx >> winrar-x64es.exe 
+ls
+cd
+history >> bashTareaBackup1
+nano bashTareaBackup1 
+cd Documents/C/Users/
+cd urbano/
+ls
+cd Git/
+ls
+cd proyecto6/
+ls
+nano file1.txt 
+cat file1.txt >> file2.txt 
+cat file1.txt >> ../proyecto5/file1.txt 
+cat file1.txt >> ../proyecto5/file2.txt 
+cat file1.txt >> ../proyecto4/file1.txt 
+cat file1.txt >> ../proyecto4/file2.txt 
+cat file1.txt >> ../proyecto3/file2.txt 
+cat file1.txt >> ../proyecto3/file1.txt 
+cat file1.txt >> ../proyecto2/file1.txt 
+cat file1.txt >> ../proyecto2/file2.txt 
+cat file1.txt >> ../proyecto1/file1.txt 
+cat file1.txt >> ../proyecto1/file2.txt 
+ls
+cd ..
+rm -r proyecto6/
+ls
+cd ..
+ls}
+ls
+cd Internship/
+ls
+nano YDKW_JS1.pdf 
+cat YDKW_JS1.pdf >> YDKN_JS2.pdf 
+ls
+cat YDKW_JS1.pdf >> YDKN_JS3.pdf 
+cat YDKW_JS1.pdf >> YDKN_JS4.pdf 
+cat YDKW_JS1.pdf >> YDKN_JS5.pdf 
+cat YDKW_JS1.pdf >> YDKN_JS6.pdf 
+cat YDKW_JS1.pdf >> bashCommands.docx 
+cat YDKW_JS1.pdf >> desarrollo.pptx }
+cat YDKW_JS1.pdf >> desarrollo.pptx 
+cat YDKW_JS1.pdf >> diseno.pptx 
+cat YDKW_JS1.pdf >> gitBasics.docx 
+cat YDKW_JS1.pdf >> gitUnderHood.docx 
+cat YDKW_JS1.pdf >> mantenimiento.pptx 
+cat YDKW_JS1.pdf >> plumbingCommands.docx 
+cat YDKW_JS1.pdf >> porcelainCommands.docx 
+cat YDKW_JS1.pdf >> pragProgram.pdf 
+cat YDKW_JS1.pdf >> progit.pdf 
+cat YDKW_JS1.pdf >> requerimientos.pptx 
+cat YDKW_JS1.pdf >> testing.pptx 
+cd ..
+cd Pictures/
+ls
+nano bug.png 
+ls
+nano bug.png >> happyFace.png 
+cat bug.png >> happyFace.png 
+ls
+cat bug.png >> memedeBatman.jpg 
+cat bug.png >> platilladeMeme10.jpg 
+cat bug.png >> userIcon.png 
+cat bug.png >> Vacacaciones/2015-06-15-4585.jpg 
+cat bug.png >> Vacacaciones/2015-06-15-4896.jpg 
+cat bug.png >> Vacacaciones/2015-06-15-4897
+cat bug.png >> Vacacaciones/2015-06-15-5821.jpg 
+cat bug.png >> Vacacaciones/2015-06-15-8965.jpg 
+cat bug.png >> Vacacaciones/2015-06-16-47896.jpg 
+cat bug.png >> Vacacaciones/2015-06-16-4877.jpg 
+cat bug.png >> Vacacaciones/2015-06-16-5585.jpg 
+cat bug.png >> Vacacaciones/2015-06-16-7821.jpg 
+cat bug.png >> Vacacaciones/2015-06-16-7965.jpg 
+rm Vacacaciones/2015-06-15-4897
+ls
+cd ..
+ls
+cd Videos/
+ls
+nano JLDC.mp4 
+cat JLDC.mp4 >> 15115648dsada.mp4 
+cat JLDC.mp4 >> anabell.mp4 
+cat JLDC.mp4 >> antman.mp4 
+cat JLDC.mp4 >> aquaman.mp4 
+cat JLDC.mp4 >> avengerIWar.mp4 
+cat JLDC.mp4 >> avengersEndGame.mp4 
+cat JLDC.mp4 >> cdHidalgo.mp4 
+cat JLDC.mp4 >> conjuring.mp4 
+cat JLDC.mp4 >> corrientresFilosoficas.mp4 
+cat JLDC.mp4 >> heredeitary.mp4 
+cat JLDC.mp4 >> intererstellar.mp4 
+cat JLDC.mp4 >> losAzufres.mp4 
+cat JLDC.mp4 >> morelia.mp4 
+cat JLDC.mp4 >> shazam.mp4 
+cat JLDC.mp4 >> us.mp4 
+cat JLDC.mp4 >> viajeMichoacan.mp4 
+cat JLDC.mp4 >> videoLosValoresEticos.mp4 
+cd
+cd Documents/
+ls
+history >> bashExercise2.txt 
+cd C/Users/urbano/Music/
+ls
+rm -rf TheStrokes Queen Gorillaz GusanaCiega TheKillers RedHotChilliPepers OneOkRock
+ls
+rm -rf AC_DC/
+ls
+cd Bunkers/
+ld
+
+ls
+mv baialandoSolo.mp3 baialandoSolo.txt
+mv llueveSobreLaCiudad.mp3 llueveSobreLaCiudad.txt
+mv mientele.mp3 mientele.txt
+mv nadaNuevoBajoELSol.mp3 nadaNuevoBajoELSol.txt
+mv quienFuera.mp3 quienFuera.txt
+mv quienFuera.mp3 velocidadDeLaLuz.mp3 
+mv quienFuera.mp3 velocidadDeLaLuz.txt
+ls
+mv velocidadDeLaLuz.mp3 velocidadDeLaLuz.txt
+ls
+mv velocidadDeLaLuz.mp3 Bunkers-Velocidad_de_la_Luz
+mv velocidadDeLaLuz.mp3 Bunkers-Velocidad_de_la_Luz.txt
+mv velocidadDeLaLuz.txt Bunkers-Velocidad_de_la_Luz.txt
+ls
+mv velocidadDeLaLuz.txt Bunkers-Velocidad_de_la_Luz.txt
+mv 4Bunkers-Velocidad_de_la_Luz.txt Bunkers-Velocidad_de_la_Luz.txt 
+ls
+mv baialandoSolo.txt Bunkers-Baialando_solo.txt
+mv mientele.txt Bunkers-mientele.txt
+mv llueveSobreLaCiudad.txt Bunkers-Llueve_sobre_la_ciudad
+mv nadaNuevoBajoELSol.txt Bunkers-Nada_nuevo_bajo_el_sol.txt
+mv quienFuera.txt Bunkers-Quien_fuera.txt
+ls
+touch Bunkers-No_hables_de_sufrir.txt
+touch Bunkers-Miño
+touch Bu
+rm Bunkers-Miño
+ls
+touch Bunkers-Sur.txt
+touch Bunkers-Ven_aqui.txt
+mv Bunkers-Llueve_sobre_la_ciudad Bunkers-Llueve_sobre_la_ciudad.txt
+cat Bunkers-Baialando_solo.txt >>
+cat Bunkers-Baialando_solo.txt 
+cat Bunkers-Baialando_solo.txt Ahora que estás aquí ahogado en medio de la gente 
+Y estás dispuesto a más de lo que puedes soportar
+Los altavoces van amplificando las verdades
+Ahora que estás aquí en medio de la soledad
+Bailando solo en la oscuridad 
+Te vas a acostumbrar a ver toda la vida pasar
+Igual que una promesa al anochecer 
+Te puedes disolver al borde de un vaso de cristal
+Ahora que estás aquí y que el futuro se te esconde, 
+Ya has rasguñado el suelo y las paredes de tu habitación
+No quieres ver el sol, no quieres nada por las tardes
+Ninguna sensación que nos haga sentir mejor
+nano Bunkers-Baialando_solo.txt 
+nano Bunkers-Llueve_sobre_la_ciudad.txt 
+nano Bunkers-mientele.txt 
+nano Bunkers-Miño.txt 
+nano Bunkers-Nada_nuevo_bajo_el_sol.txt 
+nano Bunkers-No_hables_de_sufrir.txt 
+nano Bunkers-Quien_fuera.txt 
+nano Bunkers-Sur.txt 
+ls
+nano Bunkers-Velocidad_de_la_Luz.txt 
+
+nano Bunkers-Ven_aqui.txt 
+cd ..
+ls
+cd CafeTacvba/
+ls
+mv elBaileYelSAlon.mp3 CafeTacvba-EL_baile_y_el_salon.txt
+mv eres.mp3 CafeTacvba-Eres.txt
+mv esaNoche.mp3 CafeTacvba-Esa_noche.txt
+mv futuro.mp3 CafeTacvba-Futuro.txt
+mv ingrata.mp3 CafeTacvba-Ingrata.txt
+touch CafeTacvba-El_tlatoani_del_barrio.txt
+touch CafeTacvba-El_aparato-.txt
+touch CafeTacvba-24_horas.txt
+touch CafeTacvba-El_metro.txt
+touch CafeTacvba-Pez.txt
+cd
+cd Documents/
+history >> bashBackup3.txt
+cd Users/urbano/Music/
+ls
+rm -rf Caifanes DivisionMinuscula DLD Enjambre Molotov PanteonRococo PXNDX SodaEstereo/
+ls
+cd CafeTacvba/
+ls
+cp CafeTacvba-El_metro.txt 
+cp CafeTacvba-24_horas.txt CafeTacvba-Eres.txt CafeTacvba-Esa_noche.txt 
+cd ..
+rm -rf CafeTacvba/
+ls
+mv Bunkers/ Playlist1
+ls
+mkdir Playlist2 Playlist3 Playlist4 Playlist5 Playlist6 Playlist7 Playlist8 Playlist9 Playlist10
+ls
+cd Playlist1
+ls
+touch CafeTacvba-24_horas.txt CafeTacvba-Eres.txt CafeTacvba-Esa_noche.txt 
+touch CafeTacvba-El_metro.txt 
+ls
+nano CafeTacvba-24_horas.txt 
+nano CafeTacvba-El_metro.txt 
+nano CafeTacvba-Eres.txt 
+nano CafeTacvba-Esa_noche.txt 
+ls
+rm -rf Bunkers-Sur.txt Bunkers-Miño.txt Bunkers-Ven_aqui.txt Bunkers-No_hables_de_sufrir.txt 
+cp Bunkers-* ../Playlist2
+cp Bunkers-* ../Playlist3
+cp Bunkers-* ../Playlist4
+cp Bunkers-* ../Playlist5
+cp Bunkers-* ../Playlist6
+cp Bunkers-* ../Playlist7
+cp Bunkers-* ../Playlist8
+cp Bunkers-* ../Playlist9
+cp Bunkers-* ../Playlist10
+cp CafeTacvba-* ../Playlist2
+cp CafeTacvba-* ../Playlist3
+cp CafeTacvba-* ../Playlist4
+cp CafeTacvba-* ../Playlist5
+cp CafeTacvba-* ../Playlist6
+cp CafeTacvba-* ../Playlist7
+cp CafeTacvba-* ../Playlist8
+cp CafeTacvba-* ../Playlist9
+cp CafeTacvba-* ../Playlist10
+cd ..
+ls
+cd ..
+ls
+cd ..
+rm -rf pedro jose/
+mkdir pedro jose
+ls
+cd urbano
+ls
+rm -rf Desktop Git Internship/
+cd Documents
+rm .*pptx
+rm *.pptx
+rm *.xls
+rm tareaViernes.docx Exponentes.docx tareaCalculo.docx 
+rm -rf *.pdf *.xls *.exe
+cd ..
+cd Downloads/
+rm -rf *.pdf *.xls *.exe
+rm demo.rar bug.png 
+rm fotoTarea2.jpg 
+cd ..
+cd Pictures/
+ls
+rm -rf Vacacaciones/
+cd ..
+cd Videos/
+ls
+rm 15115648dsada.mp4 
+rm heredeitary.mp4 JLDC.mp4 morelia.mp4 videoLosValoresEticos.mp4 shazam.mp4 conjuring.mp4 us.mp4 anabell.mp4 avengerIWar.mp4 
+rm antman.mp4 aquaman.mp4 corrientresFilosoficas.mp4 
+cd ..
+cd Pictures/
+ls
+cd ..
+cp Documents/ Downloads/ Music/ Pictures/ Videos/ ../pedro/
+cp -r Documents/ Downloads/ Music/ Pictures/ Videos/ ../pedro/
+cp -r Documents/ Downloads/ Music/ Pictures/ Videos/ ../jose/
+cd ..
+cd ..
+cd ..
+history >> bashBackup3.txt 
