e007e51efc7e858255172d5d5f08817d50b663b7
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 00:28:30 -0500

    Added Nestor's exercise1

---
+++ Nestor/nestor-bash-exercise1/nestor-bash-exercise1.sh
@@ -0,0 +1,362 @@
+#!/bin/bash
+
+mkdir C
+cd c
+ls
+cd c
+ls
+cd C/
+ls
+mkdir Program Windows Users
+ls
+mv Program Programs
+ls
+cd Programs/
+mkdir folder*10
+ls
+rm -rf folder*10
+ls
+ls
+mkdir folder folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9
+ls
+touch folder/file.txt file1.txt folder1/file.txt file1.txt folder2/file.txt file1.txt folder3/file.txt file1.txt folder4/file.txt file1.txt folder5/file.txt file1.txt folder6/file.txt file1.txt folder7/file.txt file1.txt folder8/file.txt file1.txt  folder9/file.txt file1.txt
+ls
+rm file1.txt 
+ls
+cd folder
+ls
+touch file1.txt
+ls
+cd ..
+ls folder1/
+ls folder2/
+touch folder1/file1.txt folder2/file1.txt folder3/file1.txt folder4/file1.txt folder5/file1.txt folder6/file1.txt folder7/file1.txt folder8/file1.txt  folder9/file1.txt
+ls
+ls folder4
+ls folder3 folder5
+ls folder folder1 folder2 folde3 folder4 folder5 folder6 folder7 folder8 folder9
+cd ..
+ls folder3
+cd programs/
+cd Programs/
+ls folder3
+cd..
+cd ..
+ls
+cd windows
+cd Windows/
+mkdir folder
+touch folder/file.txt file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt
+ls folder
+cd folder/
+ls
+touch file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt
+ls
+touch file1.txt
+ls
+cd ..
+cp folder folder1
+ls
+cd folder/
+ls
+cd ..
+rm -rf file.txt file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt
+ls
+cp folder folder1
+cp -r folder folder1
+ls
+cp -r folder folder2
+cp -r folder folder3
+cp -r folder folde
+ls
+ls folder folder1 folder2 folder3 folder4
+mkdir System32
+ls
+cd System32/
+touch file.txt file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt file14.txt
+ls
+mkdir folder
+cd folder/
+ls
+touch file.txt file1.txt file2.txt file3.txt file4.txt file5.txt file6.txt file7.txt file8.txt file9.txt
+ls
+cd ..
+cp -r folder folder1
+ls
+cp -r folder folder2
+ls
+cp -r folder folder3
+cp -r folder folder4
+cd ../../
+ls
+cd Users/
+mkdir user user1 user2
+ls
+cd user
+mkdir desktop documents downloads music pictures videos contactos busqueda vinculos juegos
+ls
+cd ..
+cd user
+ls
+touch desktop/trabajo.txt ejercicio.txt libro.pdf apuntes.txt atom.exe chrome.exe firefox.exe skype.exegit-bash.exe visual-studio.exe garbage.exe git.txt softwareProcess.txt softwareArchitecture.txt tarea.txt 
+ls
+rm -rf ejercicio.txt libro.pdf apuntes.txt atom.exe chrome.exe firefox.exe skype.exegit-bash.exe visual-studio.exe garbage.exe git.txt softwareProcess.txt softwareArchitecture.txt tarea.txt 
+ls
+cd desktop/
+ls
+touch ejercicio.txt libro.pdf apuntes.txt atom.exe chrome.exe firefox.exe skype.exegit-bash.exe visual-studio.exe garbage.exe git.txt softwareProcess.txt softwareArchitecture.txt tarea.txt 
+ls
+rm -rf skype.exegit-bash.exe 
+ls
+touch skype.exe git-bash.exe 
+ls
+path
+pwd
+ls
+cd ..
+ls
+cd documents/
+cd ..
+ls
+cp desktop documents
+cp -r desktop documents
+ls documents/
+cd document
+cd documents/
+ls
+ls desktop
+cd ..
+ls
+rm -r downloads
+ls
+cp -r desktop documents
+ls
+mkdir downloads
+ls
+rm -r documents/
+ls
+cp -r desktop documents}
+ls
+rm -r documents}
+ls
+cp -r desktop documents
+ls
+ls desktop documents
+ls
+cd downloads
+touch winrar.exe git-bash.exe info.pdf guia.pdf eloquent.pdf pelicula.rar culebrita.png tacos.png paqeuteImagenes.zip subtitlos.str quimica.docs fisica.docs nudes.zip burritos.rar tarea.docs
+ls
+cd ..
+cd music/
+ls
+mkdir queen pearljam pinkFloyd joanSebastian beatles rollingStones ledZeppelin nirvana ac/dc bonJovi aeorsmith ramones u2 kiss greenDay rem coldplay radiohead slayer oasis
+ls
+touch aeorsmith/music.mp4 music1.mp4 music2.mp4 music3.mp4 music4.mp4
+ls
+rm -rf music.mp4 music1.mp4 music2.mp4 music3.mp4 music4.mp4
+ls
+cd aeorsmith/
+ls
+touch music1.mp4 music2.mp4 music3.mp4 music4.mp4
+ls
+cd ..
+ls
+cp -r aeorsmith/ coldplay/
+ls
+cd coldplay/
+ls
+rm -raeorsmith
+ls
+rm -raeorsmith
+rm -R aeorsmith
+ls
+cp aeorsmith/ coldplay/
+ls
+cd ..
+cp aeorsmith/ coldplay/
+ls
+cd ..
+# pwd
+# cd ..
+# ls
+# cd ..
+# ls
+# cd home/
+# ls
+# cd rabbin
+# ls
+# cd intershipBTS/
+# ls
+# # git init
+# ls
+# cd bash-class/
+# ls
+# cd ..
+# ls
+# cd .. 
+# ls
+# cd intershipBTS/
+# ls
+# cd bash-class/
+# ls
+# cd C/
+ls
+cd Users/
+ls
+cd user
+ls
+cd music/
+ls
+ls aeorsmith/
+touch beatles/music.mp4, music1.mp4, music2.mp4, music3.mp4, music4.mp4
+ls
+rm -rf music1.mp4, music2.mp4, music3.mp4, music4.mp4
+ls
+cd beatles/
+ls
+rm -rf music.mp4, 
+ls
+pwd
+cd ..
+ls
+ls -l
+ls
+clear
+ls
+cd aeorsimth
+cd aeorsmith/
+ls
+cd ..
+ls
+ls beatles/
+ls coldplay/
+touch coldplay/[music.mp3, music1.mp3, music2.mp3, music3.mp3, music4.mp3]
+ls
+rm -rf music1.mp3, music2.mp3, music3.mp3, music4.mp3]
+ls
+ls coldplay/
+cd coldplay/
+ls
+ls
+cd ..
+cd bonJovi/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+ls
+cd ..
+cd greenDay/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd kiss/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd nirvana/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+ls
+cd pearljam/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd queen/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd ramones/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd rollingStones/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd u2/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+ls beatles/
+cd beatles/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+ls coldplay/
+cd coldplay/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd joanSebastian/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd ledZeppelin/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd oasis/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd pinkFloyd/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd radiohead/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+ls
+cd rem/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+cd ..
+cd slayer/
+touch music.mp3 music1.mp3 music2.mp3 music3.mp3 music4.mp3
+ls
+cd ..
+cd.
+cd ..
+ls
+ls documents/
+ls downloads/
+ls pictures/
+cd pictures
+touch playa.png rio.png familia.png hermana.png mamá.png papá.png hermano.png hermanos.png hermanas.png casada.png nudes.png echhi.png stunade.png sakura.png hinata.png
+ls
+cd ..
+ls
+cd videos
+ls
+touch playa.mp4 rio.mp4 familia.mp4 hermana.mp4 mamá.mp4 papá.mp4 hermano.mp4 hermanos.mp4 hermanas.mp4 casada.mp4 nudes.mp4 echhi.mp4 stunade.mp4 sakura.mp4 hinata.mp4
+ls
+cd ..
+ls
+cp busqueda office
+cp -r busqueda office
+ls
+rm -rf busqueda/
+ls
+cd office/
+ls
+touch quimica.docs fisica.docs geografia.docs matematicas.docs tarea.docs architecture.docs git.docs
+ls
+touch bash.docs node.docs javaScript.docs commands.docs tarea2.docs tarea1.docs java.docs anime.docs
+ls
+cd ..
+ls
+cd contactos/
+ls
+touch cristain.txt marlon.txt migue.txt ricardo.txt peregrino.txt urbano.txt cristian.txt sergio.txt david.txt fany.txt viri.txt luis.txt erik.txt emmanuel.txt hector.txt
+ls}
+ls
+cd ..
+ls
+ls contactos/ desktop/ documents/ downloads/ juegos/ music/ office/ pictures/ videos/ vinculos/
+ls
+rm -r vinculos/
+ls
+cp -r documents/ vinculos
+ls
+cd vinculos/
+ls
+cd ..
+cd juegos/
+touch lol.exe freefire.exe gta.exe bomberman.exe shaiya.exe pokemon.exe fornight.exe metalslug.exe metalslug2.exe metalslug3.exe mariobros.exe battlefield.exe battlefield2.exe battlefield3.exe battlefield4.exe
+ls
+cd ../../
+ls
+rm -r user1 user2
+ls
+cp user user1
+cp -r user/. user1
+ls
+cp -r user user2
+ls
+cd ..
+ls
+history -w historial.txt
