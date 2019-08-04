504851ec0e4767b73859dc85d39a1ee911ab51e6
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 14:58:14 -0500

    Added Estefania's homework 1

---
+++ Estefania/estefania-bash-homework1/estefania-bash-homework1.sh
@@ -0,0 +1,539 @@
+#!/bin/bash
+
+ls
+cd C
+ls
+mkdir Programs
+cd Programs/
+mkdir Intel/{license.txt,media.txt}
+mkdir -p Intel/{license.txt,media.txt}
+ls
+ls -R
+mkdir -p Lenovo/{license.txt,history.txt},nodejs/{file.txt,npm.txt},spotify/{readme.txt,file.txt},slack/{file.txt,license.txt},skype/{version.txt,file.txt},windowsOffice{license.txt,version.txt},windowsDefender/{version.txt,license.txt},Realtek/{file.txt,service.txt},Mail/{file.txt,version.txt}
+ls
+cd Lenovo
+ls
+rm -r Lenovo
+cd ..
+rm -r Lenovo
+ls
+mkdir -p Lenovo/{license.txt,history.txt}
+ls
+cd Lenovo/
+ls
+mkdir -p nodejs/{file.txt,npm.txt}
+rm -r nodejs
+ls
+cd ..
+mkdir -p nodejs/{file.txt,npm.txt}
+ls
+mkdir -p spotify/{readme.txt,file.txt}, slack/{file.txt,license.txt}
+ls
+mkdir -p Skype/{version.txt,file.txt}, windowsOffice{license.txt,version.txt}, windowsDefender/{version.txt,license.txt}, Realtek/{file.txt,service.txt}, Mail/{file.txt,version.txt}
+ls
+rm -r windowsOfficelicense.txt,
+la
+rm -r windowsOfficeversion.txt,
+ls
+mkdir -p windowsOficce/{license.txt,version.txt}
+ls
+ls -R
+# cd /
+# cd mnt
+# cd c/Users
+# cd/estef
+# ls
+# cd estef
+# ls
+# cd OneDrive/
+# cd Escritorio/
+# cd cmder
+# clear
+# cd ../../C
+# ls
+# cd Programs/
+ls
+cd Intel/
+ls
+cd ../../
+cd Windows/
+ls
+cd PrimerFolder/
+ls
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+cd SegundoFolder/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+cd TercerFolder/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+cd CuartoFolder/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+cd QuintoFolder/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+ls -R
+cd System32/
+rm file15.txt file14.txt file13.txt file12.txt file11.txt file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+cd ..
+rm -r QuintoFolder/
+ls
+cd ..
+cd Users/
+ls
+cd estefania/
+ls
+rm Desktop Mail Homework Projects Downloads
+rm -r Mail Homework Projects Downloads
+ls
+cd ..
+ls
+cd usuario2
+ls
+rm -r Mail Homework Projects Downloads
+ls
+cd ..
+cd usuario3
+ls
+rm -r Mail Homework Projects Downloads
+ls
+cd ..
+cd estefania
+ls
+ls Desktop/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+cd Desktop/
+rm file10.txt file9.txt file8.txt file7.txt file6.txt
+ls
+rm file11.txt file12.txt file13.txt file14.txt 
+ls
+cd ..
+cd Documents/
+ls
+rm file6.txt file7.txt file8.txt file9.txt file10.txt file11.txt file12.txt file13.txt file14.txt file15.txt
+ls
+file1.pdf file11.docx file13.pdf file15.pdf file3.pdf file5.docx file8.docx file10.pdf file12.docx
+rm file1.pdf file11.docx file13.pdf file15.pdf file3.pdf file5.docx file8.docx file10.pdf file12.docx
+ls
+# cd .
+cd ..
+ls
+cd Pictures/
+ls
+rm arboles.gif  flores.jpg  gatos.jpg    meme.gif   pingüino.png      planta.png   polonio2.png  vacaciones.jpg cutecat.jpg gatito.png
+ls
+cd ..
+cd Videos/
+ls
+rm aladdin.mp4         hombresNegro.mp4  johnWick.mp4       mentiras.mp4         rocketman.mp4
+# apolo.mp4           iceCalling.mp4    leto.mp4           paranormal.mp4       spiderMan.mp4
+# bajosInstintos.mp4  intimidad.mp4     mascotas.mp4       reinoBajoLaLuna.mp4  thor.mp4
+ls
+rm apolo.mp4 iceCalling.mp4 leto.mp4 paranormal.mp4 spiderMan.mp4
+ls
+rm toyStory.mp4 thor.mp4 mascotas.mp4 
+ls
+rm reyLeon.mp4 mejorQueNunca.mp4 
+ls
+cd ..
+ls -R
+ls
+cd ..
+rm -r usuario2
+ls
+rm -r usuario3/
+ls
+cp -r estefania/. usuario2/
+ls
+cp -r estefania/. usuario3/
+ls
+cd ..
+history -w historial2
+cat historial2
+cd c
+ls
+cd C
+cd Windows
+ls
+cd PrimerFolder/
+ls
+touch file1.txt
+cat file1.txt
+# cd .
+cd ..
+cd SegundoFolder/
+cd ..
+cd PrimerFolder/
+cat file2.txt
+touch file2.txt
+car file2.txt
+cat file2.txt
+touch file2.txt
+cat file2.txt
+ls
+cat file2.txt
+touch file2.txt
+cat file2.txt
+touch file3.txt
+cat file3.txt
+ls
+touch file4.txt
+touch file5.txt
+cat file5.txt
+cd ..
+rm -r SegundoFolder/
+cp -r PrimerFolder/. SegundoFolder/
+ls
+rm -r TercerFolder CuartoFolder
+ls
+cp -r PrimerFolder/. TercerFolder/ CuartoFolder
+ls
+cp -r PrimerFolder/. TercerFolder/
+cp -r PrimerFolder/. CuartoFolder/
+ls
+cd System32/
+ls
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch file4.txt
+touch file5.txt
+ls
+cat file5.txt
+cd ../../
+cd Programs
+cd Lenovo
+cat license.txt
+touch license.txt
+cat license.txt >> version.txt
+cd ..
+cp -r Lenovo/. Intel/
+cp -r Lenovo/. Skype/
+cp -r Lenovo/. Mail/
+cp -r Lenovo/. mcafee/
+cp -r Lenovo/. Media/
+cp -r Lenovo/. nodejs/
+cp -r Lenovo/. Slack/
+cp -r Lenovo/. Spotify/
+cp -r Lenovo/. WindowsDefender/
+cd ..
+cd Users/
+cd estefania/
+cd Desktop/
+touch file1.txt
+cat file1.txt >> file2.txt file3.txt
+cat file1.txt >> file3.txt
+cat file1.txt >> file4.txt
+cat file1.txt >> file5.txt
+cd ..
+ls
+cd Documents/
+ls
+touch file14.docx
+cat file14.docx >> file2.docx
+cat file14.docx >> file4.txt
+cat file14.docx >> file6.pdf
+cat file14.docx >> file9.pdf
+cd ..
+cd Music/
+cd Muse/
+cd ..
+ls
+rm Björk Disturbed JoyDivision FranzFerdinand BrunoMars Babasonicos Adele CapitalCities FooFighters Dorian
+rm -r Björk Disturbed JoyDivision FranzFerdinand BrunoMars Babasonicos Adele CapitalCities FooFighters Dorian
+ls
+cd ArcticMonkeys/
+mv track1 'ArcticMonkeys-Do I wanna know'
+ls
+mv track1 'ArcticMonkeys-Do I wanna know'
+mv ArcticMonkeys-Do I wanna know.txt
+ls
+mvArctic 'ArcticMonkeys-Do I wanna know'
+mv 'ArcticMonkeys-Do I wanna know.mp3' ArcticMonkeys-Do I wanna know.txt
+ls
+mv track2.mp3 'ArcticMonkeys-I wanna be yours.txt'
+ls
+touch ArcticMonkeys-Do I wanna know.txt
+touch 'ArcticMonkeys-Do I wanna know.txt'
+cat 'ArcticMonkeys-I wanna be yours.txt'
+ls
+rm ArcticMonkeys-Do
+ls
+touch 'ArcticMonkeys-I wanna be yours.txt'
+cat 'ArcticMonkeys-I wanna be yours.txt' 
+ls
+mv track3.mp3 'ArcticMonkeys-505.txt'
+touch 'ArcticMonkeys-505.txt' 
+ls
+mv track4.mp3 'ArcticMonkeys-Arabella.txt'
+touch 'ArcticMonkeys-Arabella.txt' 
+mv track5.mp3 'ArcticMonkeys-R U mine.txt'
+touch 'ArcticMonkeys-R U mine.txt' 
+touch 'ArcticMonkeys-R U mine.txt' 
+touch 'ArcticMonkeys-R U mine.txt' 
+ls
+touch 'ArcticMonkeys-Knee Socks.txt'
+ls
+touch 'ArcticMonkeys-Fluorescent Adolescent.txt'
+touch 'ArcticMonkeys-A Certain Romance.txt'
+touch 'ArcticMonkeys-Mardy Bum.txt'
+ls
+touch 'ArcticMonkeys-Cornestone.txt'
+cd ..
+cd Blink182/
+mv track1.mp3 'Blink 182-I miss you.txt'
+touch 'Blink 182-I miss you.txt' 
+mv track2.mp3 'Blink 182-All the small things.txt'
+touch 'Blink 182-All the small things.txt' 
+mv track3.mp3 'Blink 182-Whats my age again.txt'
+touch 'Blink 182-Whats my age again.txt' 
+mv track4.mp3 'Blink 182-Always.txt'
+touch 'Blink 182-Always.txt' 
+ls
+mv track5.mp3 'Blink 182-Happy Days.txt'
+touch 'Blink 182-Happy Days.txt' 
+touch 'Blink 182-First Date.txt'
+touch 'Blink 182-Adams Song.txt'
+touch 'Blink 182-Dumpweed.txt'
+ls
+touch 'Blink 182-Dammit.txt'
+touch 'Blink 182-Misery.txt'
+cd ..
+mv track1.mp3 'Blur-Song 2.txt'
+cd Blur/
+mv track1.mp3 'Blur-Song 2.txt'
+touch 'Blur-Song 2.txt' 
+mv track2.mp3 'Blur-Coffee & TV.txt'
+touch 'Blur-Coffee & TV.txt' 
+mv track3.mp3 'Blur-Girls and Boys.txt'
+touch 'Blur-Girls and Boys.txt' 
+mv track4.mp3 'Blur-Sweet Song.txt'
+touch 'Blur-Sweet Song.txt' 
+mv track5.mp3 'Blur-Ghost Ship.txt'
+touch 'Blur-Ghost Ship.txt' 
+touch 'Blur-Parklife.txt'
+touch 'Blur-The Universal.txt'
+touch 'Blur-Tender.txt'
+touch 'Blur-Country House.txt'
+touch 'Blur-Beetlebum.txt'
+ls
+cd ..
+cd BrandonFlowers/
+mv track1.mp3 'Brandon Flowers-Magdalena.txt'
+touch 'Brandon Flowers-Magdalena.txt' 
+mv track2.mp3 'Brandon Flowers-Only the Young.txt' 
+touch 'Brandon Flowers-Only the Young.txt' 
+mv track3.mp3 'Brandon Flowers-Crossfire.txt'
+touch 'Brandon Flowers-Crossfire.txt' 
+mv track4.mp3 'Brandon Flowers-Dreams Come True.txt'
+touch 'Brandon Flowers-Dreams Come True.txt' 
+mv track5.mp3 'Brandon Flowers-Between Me and You.txt'
+touch 'Brandon Flowers-Between Me and You.txt' 
+touch 'Brandon Flowers-Still Want You.txt'
+touch 'Brandon Flowers-Right Behind You.txt'
+touch 'Brandon Flowers-On the Floor.txt'
+touch 'Brandon Flowers-Bette Davis Eyes.txt'
+touch 'Brandon Flowers-I Can Change.txt'
+ls
+cd ..
+cd Caifanes/
+mv track1.mp3 'Caifanes-La Celula que explota.txt'
+touch 'Caifanes-La Celula que explota.txt' 
+mv track2.mp3 'Caifanes-Viento.txt' 
+touch 'Caifanes-Viento.txt' 
+mv track3.mp3 'Caifanes-Matenme porque me muero.txt'
+touch 'Caifanes-Matenme porque me muero.txt' 
+mv track3.mp3 'Caifanes-No dejes que.txt'
+ls
+mv track4.mp3 'Caifanes-No dejes que.txt'
+touch 'Caifanes-No dejes que.txt' 
+mv track5.mp3 'Caifanes-Afuera.txt'
+touch 'Caifanes-Afuera.txt' 
+touch 'Caifanes-Ayer me dijo un ave.txt'
+touch 'Caifanes-Los Dioses Ocultos.txt'
+touch 'Caifanes-Aqui no es asi.txt'
+touch 'Caifanes-La Negra Tomasa.txt'
+touch 'Caifanes-Antes de que nos olviden.txt'
+ls
+cd ..
+cd ChemicalBrothers/
+mv track1.mp3 'Chemical Brothers-Do It Again.txt'
+touch 'Chemical Brothers-Do It Again.txt' 
+mv track2.mp3 'Chemical Brothers-Hey Boy Hey Girl.txt'
+touch 'Chemical Brothers-Hey Boy Hey Girl.txt' 
+mv track3.mp3 'Chemical Brothers-Go.txt'
+touch 'Chemical Brothers-Go.txt' 
+mv track4.mp3 'Chemical Brothers-The Test.txt'
+touch 'Chemical Brothers-The Test.txt' 
+mv track5.mp3 'Chemical Brothers-Free Yourself.txt'
+touch 'Chemical Brothers-Free Yourself.txt' 
+touch 'Chemical Brothers-Wide Open.txt'
+touch 'Chemical Brothers-Dissolve.txt'
+touch 'Chemical Brothers-Close Your Eyes.txt'
+touch 'Chemical Brothers-Block Rockin Beats.txt'
+touch 'Chemical Brothers-Leave Home.txt'
+ls
+cd ..
+cd Coldplay/
+mv track1.mp3 'Coldplay-The Scientist.txt'
+touch 'Coldplay-The Scientist.txt' 
+mv track2.mp3 'Coldplay-Yellow.txt'
+touch 'Coldplay-Yellow.txt' 
+mv track3.mp3 'Coldplay-A Sky Full Of Starts.txt'
+touch 'Coldplay-A Sky Full Of Starts.txt' 
+mv track4.mp3 'Coldplay-Fix You.txt'
+touch 'Coldplay-Fix You.txt' 
+mv track5.mp3 'Coldplay-Viva la Vida.txt'
+touch 'Coldplay-Viva la Vida.txt' 
+touch 'Coldplay-In My Place.txt'
+touch 'Coldplay-Paradise.txt'
+touch 'Coldplay-Trouble.txt'
+touch 'Coldplay-Clocks.txt'
+touch 'Coldplay-The Hardest Part.txt'
+ls
+cd ..
+cd Jaguares/
+mv track1.mp3 'Jaguares-Te lo pido por favor.txt'
+touch 'Jaguares-Te lo pido por favor.txt' 
+mv track2.mp3 'Jaguares-Asi como tu.txt'
+touch 'Jaguares-Asi como tu.txt' 
+mv track3.mp3 'Jaguares-Fin.txt'
+touch 'Jaguares-Fin.txt' 
+mv track4.mp3 'Jaguares-El Equilibrio.txt'
+touch 'Jaguares-El Equilibrio.txt' 
+mv track5.mp3 'Jaguares-Contigo.txt'
+touch 'Jaguares-Contigo.txt' 
+touch 'Jaguares-No Dejes Que.txt'
+touch 'Jaguares-Detras de los Cerros.txt'
+touch 'Jaguares-Por un Beso.txt'
+touch 'Jaguares-Viaje Astral.txt'
+touch 'Jaguares-Las Ratas no tienen Alas.txt'
+ls
+cd ..
+cd Maroon5/
+mv track1.mp3 'Maroon 5-Sugar.txt'
+touch 'Maroon 5-Sugar.txt' 
+mv track2.mp3 'Maroon 5-She Will Be Loved.txt'
+touch 'Maroon 5-She Will Be Loved.txt' 
+mv track3.mp3 'Maroon 5-Girls Like You.txt'
+touch 'Maroon 5-Girls Like You.txt' 
+mv track4.mp3 'Maroon 5-Maps.txt'
+touch 'Maroon 5-Maps.txt' 
+mv track5.mp3 'Maroon 5-This Love.txt'
+touch 'Maroon 5-This Love.txt' 
+touch 'Maroon 5-What Lovers Do.txt'
+touch 'Maroon 5-Wont Go Home Without You.txt'
+touch 'Maroon 5-Sunday Morning.txt'
+touch 'Maroon 5-Dont Wanna Know.txt'
+touch 'Maroon 5-Animals.txt'
+ls
+cd ..
+cd Muse/
+mv track1.mp3 'Muse-Cant Take My Eyes Off You.txt'
+touch 'Muse-Cant Take My Eyes Off You.txt' 
+mv track2.mp3 'Muse-Madness.txt'
+touch 'Muse-Madness.txt' 
+mv track3.mp3 'Muse-Resistance.txt'
+touch 'Muse-Resistance.txt' 
+mv track4.mp3 'Muse-Starlight.txt'
+touch 'Muse-Starlight.txt' 
+mv track5.mp3 'Muse-Pressure.txt'
+touch 'Muse-Pressure.txt' 
+touch 'Muse-Psycho.txt'
+touch 'Muse-Something Human.txt'
+touch 'Muse-The Dark Side.txt'
+touch 'Muse-Uprising.txt'
+ls
+touch 'Muse-Feeling Good.txt'
+cd ..
+cp -r estefania/. usuario2/
+cd usuario2
+ls
+cd Music/
+ls
+rm -r Adele/ Disturbed/ JoyDivision/ CapitalCities/ Dorian/ Babasonicos/ FooFighters/ Björk/ BrunoMars/ FranzFerdinand/
+ls
+cd ArcticMonkeys/
+LS
+ls
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+ls
+cd ..
+cd B
+cd Blink182/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+ls
+cd ..
+cd Blur/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd BrandonFlowers/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Caifanes/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd ChemicalBrothers/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Coldplay/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Jaguares/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Maroon5/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Muse/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd usuario3
+cd ..
+cp -r estefania/. usuario3/
+cd usuario3
+cd ArcticMonkeys/
+cd Music/
+cd ArcticMonkeys/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+ls
+rm -r Adele/ Disturbed/ JoyDivision/ CapitalCities/ Dorian/ Babasonicos/ FooFighters/ Björk/ BrunoMars/ FranzFerdinand/
+ls
+cd Blink182/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Blur/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd BrandonFlowers/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Caifanes/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd ChemicalBrothers/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Coldplay/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Jaguares/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Maroon5/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd Muse/
+rm track1.mp3 track2.mp3 track3.mp3 track4.mp3 track5.mp3
+cd ..
+cd ../..
+history -w estefania-bash-womework1.txt
