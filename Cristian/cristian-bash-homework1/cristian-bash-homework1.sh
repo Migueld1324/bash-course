ac840173be1c68a1b8ef386164cdb50e1a8d6ff9
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 11:31:17 -0500

    Added Cristian's Homework 1

---
+++ Cristian/cristian-bash-homework1/cristian-bash-homework1.sh
@@ -0,0 +1,237 @@
+#!/bin/bash
+
+cd C:
+ls
+cd Programs/
+ls
+rm -r McAfee/
+rm -r MicrosofOffice/ Realtek/ VulkanRT/ WindowsDefender/
+ls
+cd Adobe/
+ls
+# cat >> leadme.htm chingo de contenido
+# cat >> leadme.htm
+ls
+# cat >> ReadMe.htm
+ls
+cd ..
+ls
+cd Explorer/
+ls
+# cat >> hmmapi.dll
+# cat >> iexplore.exe
+cd ..
+cd Google/
+ls
+# cat >> chrome.exe
+# cat >> master_preferences
+cd ..
+cd Intel/
+ls
+# cat >> Lang
+# cat >> pt-Br
+cd ..
+cd Lenovo/
+ls
+# cat >> ImController
+# cat >> master_preferences
+cd ../../
+cd Windows/
+ls
+cd Boot/
+ls
+rm  bootmgr.mui efisys.bin efisys_nopromp.bin memtest.efi winsipolicy.p7b
+ls
+# cat >> BCD
+# cat >> BootDebug.ini
+# cat >> boot.sdi
+# cat >> boot bootmgf.efi
+# cat >> bootmgf.efi
+# cat >> bootmgf.efi.mui
+cd ..
+cd Cursors/
+ls
+rm aero_link_i.cur aero_move.cur aero_nes.cur aero_ns_l.cur aero_pin.cur
+ls
+# cat >> aero_arrow.cur
+# cat >> aero_busy.ani
+# cat >> aero_ew.cur
+# cat >> aero_helpsel.cur
+# cat >> aero_link.cur
+cd ..
+cd Fonts/
+ls
+rm BellMT BerlinSansFB BernadrMTComprimida BodoniMT Candara
+ls
+# cat >> AgencyFB
+# cat >> AlgerianNormal
+# cat >> Arial
+# cat >> ArialRoundedMT
+# cat >> Bahnshrift
+cd ..
+ls
+cd addins/
+ls
+rm drvmain.sdb frxmain.sdb msimain.sdb pacmain.sdb sysmain.sdb
+# cat >> Amcache.hve
+# cat >> Amcache.hve.tmp
+# cat >> Amcache.hve.tmp.LOG1
+# cat >> Amcache.hve.tmp.LOG2
+# cat >> FXSEXT.ecf
+cd ..
+cd assembly/
+ls
+rm helppane.exe.mui hh.exe.mui notepad.exe.mui twain_32.dll.miu winhlp32.exe.mui
+# cat >> ADOB.dll
+# cat >> AssemblyInfo.ini
+# cat >> bfsvc.exe.mui
+# cat >> bootfix.bin
+# cat >> explorer.exe.mui
+cd ..
+cd System32/
+ls
+rm C_1146.NLS CallButtons.dll adsnt.dll bcd.dll bi.dll cacls.exe desk.cpl C_864.NLS Defrag.exe activeds.dll aeevts.dll basecsp.dll
+ls
+# cat >> ActiveSyncCsp.dll
+# cat >> acluid.dll
+# cat >> bdeui.dll
+# cat >> cabapi.dll
+# cat >> devenum.dll
+cd AdvancedInstallers/
+ls
+rm Appraiser_telemetry_Run.xml Appriser_Data.ini RBDS045E.dic SyncRes.dll.mui
+ls
+# cat >> TransliterationComponent.dgml
+# cat >> appriser.sbd
+# cat >> cdosys.dll.mui
+# cat >> cmiv2.dll
+# cat >> comctl32.dll.mui
+cd ..
+cd Boot/
+ls
+rm winload.efi.mui winload.exe.mui winresume.efi.mui winresume.exe.mui winresume.exe
+ls
+# cat >> Bootcat.cache
+cat Bootcat.cache>SyncRes.dll.mui
+cat SyncRes.dll.mui
+cat Bootcat.cache>winload.efi>winload.exe>winresume.efi
+cat winresume.efi
+cd ..
+cd Bthprops/
+ls
+rm comempty.dat comrepl.exe comrepl.exe.mui  driver.stl  mtsadmin.tlb
+# cat >> BBI
+ls
+cat BBI>BCD-Templates>MigReg.mui>MigRegDb.exe>comadmin.dll
+ls
+cd ..
+ls
+cd appraiser/
+ls
+rm BBI.LOG1 SECURYTY SOFTWARE SYSTEM userdiff
+# cat >>BBI
+cat BBI > DEFAULT > DRIVERS > ELAM  > SAM
+cat SAM
+cd ..
+cd catroot2/
+ls
+rm AAD.Core.dll Add.Core.winmd AppxBlockMac.xml AppxManifest.xml appxSignature.p7x
+ls
+cat App.xaml
+# cat >> App.xaml
+cat App.xaml >Error.htm >Loading.htm >MainPage.xaml >resources.pri
+cd ..
+ls
+cd ../../
+cd Use
+cd Users/
+ls
+cd cristian/
+ls
+rm Desktop/ Documents/ Videos/ Tareas/
+rm -R Desktop/ Documents/ Videos/ Tareas/
+ls
+cd Download/
+ls
+rm .
+rm.
+ls
+rm 0505650.png algodo.pptx asdf.jpg instalador.exe libro.pdf practica.docs tarea_pasada.docs virus_mortal.exe 56454.pn alpha_portal.mp3
+ls
+# cat >> imagen.png
+cat imagen.png >instucciones.pdf >manual.pdf >rihanna.mp3 >virus.exe
+cd ..
+cd Familia/
+ls
+rm Arbol_familiar.pdf Con_barba.png Madre.img Precentacion_familiar.pptx Sinbarba.png TodosJuntos.jpg cris.png padre.img Barbacoa.mp4 Hermana.png
+ls
+# cat >> Navida.3gp
+cat Navida.3gp >Reucerdos.pdf >Testamento >VisitaCabos.mp4 >imagen.png
+cd ..
+cd Personal/
+ls
+rm bailando.mp4 cancun.jpg connovia.png labanda.mp4 paradedicar.mp3 peda.jpg pistiembre.png sinnovia.png bailando_banda.mp4 compas.png
+ls
+# cat >> documento_importante.pdf
+cat documento_importante.pdf >nometichar.pdf >parallorar.mp3 >pedota.mp4 >prohibido.mp4
+cd ..
+cd Pictures/
+ls
+rm asd.png canitasada.jpg conlabandera.png conloscompas.jpg imagen.jpg tecturagraba.png texturacarretera.png viajeplaya.png asdf.png carro.jpg
+ls
+# cat >> conlafamilia.jpg
+cat conlafamilia.jpg >image.png >murayachina.png >textura_piedras.png >torreifel.png
+cd ..
+cd Music/
+ls
+rm -R mark/ rauwn/ spininrecords/ tyga/ vinivici/ armin/ bluetunes/ housengyn/ majestic/ paolo/ rachthekid/ snorklebort/ tiesto/ ultra/
+ls
+cd arca/
+ls
+touch paquelapasesbien.mp3 elgranjero.mp3 privado.mp3 cambio.mp3 hacemuchotiempo.mp3
+ls
+# nano cambio.mp3
+# nano comotumetocas.mp3
+# nano diles.mp3
+# nano elgranjero.mp3
+# nano hacemuchotiempo.mp3
+# nano losfavoritos.mp3
+# nano meamameodia.mp3
+# nano meprefiereami.mp3
+# nano paquelapasesbien.mp3
+# nano privado.mp3
+cd ..
+ls
+rm -R bafko/ desconocido/ leonardo/ paulo/
+ls
+cp -R arca/ bafko
+ls
+ls bafko/
+cp -R arca/ desconocido
+cp -R arca/ leonardo
+cp -R arca/ paulo
+ls
+cd ../../../
+cd Users/cristian/Music/
+cp -R arca/ rauwn
+cp -R arca/ spininrecords
+cp -R arca/ tyga
+cp -R arca/ vinivici
+cp -R arca/ armin
+cp -R arca/ bluetunes
+cp -R arca/ housengyn
+cp -R arca/ paolo
+cp -R arca/ snorklebord
+ls
+rm -R rauwn/ snorklebord/ spininrecords/ tyga/ vinivici/
+ls
+cp -R arca/ snorklebord
+cd ../../../
+cd Users/
+ls
+rm -R humberto/ kimberly/
+ls
+cp -R cristian/ humberto
+cp -R cristian/ kimberly
+ls
+history
