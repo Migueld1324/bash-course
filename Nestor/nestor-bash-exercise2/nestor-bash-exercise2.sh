f9297124ec2f05f79c14c263832eb330fa418ea6
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 18:04:11 -0500

    Added Nestor's exercise 2

---
+++ Nestor/nestor-bash-exercise2/nestor-bash-exercise2.sh
@@ -0,0 +1,232 @@
+groupadd admin2
+sudo groupadd admin2
+sudo groupadd quest
+sudo useradd owner
+sudo useradd rabbin
+sudo useradd sergio
+sudo useradd urbano
+ls
+ll
+cd Programs/
+ls
+ll
+cd ..
+cd Programs/
+ls
+chown owner:admin folder
+ll
+chown -f owner:admin folder
+ll
+chown -r owner:admin folder
+ll
+chown -R owner:admin folder
+ll
+chown owner:admin2 folder
+ll
+chown -f  owner:admin2 folder
+ll
+sodu chown owner:admin2 folder
+ls
+ll
+sodu owner:admin2 folder
+Sodu owner:admin2 folder
+ll
+Sodu -R owner:admin2 folder
+Sodu -R owner:admin2 folder/
+sodu -R owner:admin2 folder/
+sodu chown -R owner:admin2 folder/
+sodu chown -R owner folder
+chown -R owner folder
+chown owne
+ll
+chown -f owner folder
+ll
+chown -f owner folder/
+ll
+cd ..
+sudo chgrp -R admin2 Programs/folder
+ll
+cd Program
+cd Programs/
+ll
+cd ..
+sudo chgrp -R admin2 Programs/folder1
+sudo chgrp -R admin2 Programs/folder2
+sudo chgrp -R admin2 Programs/folder3
+sudo chgrp -R admin2 Programs/folder4
+sudo chown -R owner Programs/folder
+sudo chown -R owner Programs/folder1
+sudo chown -R owner Programs/folder2
+sudo chown -R owner Programs/folder3
+sudo chown -R owner Programs/folder4
+cd Programs/
+ll
+ls
+cd folder
+ll
+cd ..
+cd Windows/
+ls
+cd ..
+sudo chown -R owner:admin2 Windows/folder
+ll
+cd Windows/
+ll
+cd ..
+ll
+sudo chown -R owner:admin2 Windows/folder1
+sudo chown -R owner:admin2 Windows/folder2
+sudo chown -R owner:admin2 Windows/folder3
+sudo chown -R owner:admin2 Windows/folder4
+cd Windows/
+ll
+cd ..
+ll
+sudo chown -R owner:quest Users/user
+cd Users/
+ll
+cd ..
+sudo chown -R owner:quest Users/user1
+sudo chown -R owner:quest Users/user2
+sudo chown -R owner:quest Users/user3
+sudo chown -R owner:quest Users/user4
+cd Users/
+ll
+cd ..
+ll
+sudo chown owner:quest Users/
+ll
+cd Users/
+ll
+cd ..
+ll
+sudo chown -R rabbin Users/user
+sudo chown -R sergio Users/user1
+sudo chown -R urbano Users/user2
+cd Users/
+ll
+cd ..
+ll
+sudo useradd peregrino
+sudo chown -R peregrino Users/user3
+cd Users/
+ll
+cd ..
+history -w nestor-bash-ejercicio2.txtsudo chmod 000folder
+sudo chmod 001 folder
+ll
+sudo chmod 400 folder1
+ll
+cd folder1
+cd folder
+sudo chmod 777 folder1
+sudo chmod 777 folder
+ll
+cd folder
+ll
+sudo chmod 000 abba\ -\ i\ saw\ it\ in\ the\ mirror.txt 
+ll
+sudo chmod 400 acdc-this\ house\ is\ on\ fire.txt 
+sudo chmod 600 alden\ richards-one\ way.txt 
+sudo chmod 700 andy\ brown-tequila.txt 
+sudo chmod 740 ash-jack\ names\ the\ plantes.txt 
+sudo chmod 760 john\ prine-far\ from\ me.txt 
+sudo chmod 770 juan\ miguel\ severo-you\ don\'t\ love\ me\,\ it\'s\ okay.txt 
+sudo chmod 774 maroon\ 5-this\ love.txt 
+sudo chmod 776 melisa\ o\'neil-let\ it\ go.txt 
+sudo chmod 777 michael\ jackson-love\ is\ here\ and\ now\ you\'re\ gone.txt 
+ll}
+ll
+cd ..
+ll
+cd ..
+ll
+cd ..
+ll
+cd user
+ll
+cd music
+ll
+cd folder
+ll
+cd ..
+rm -r folder1 folder2 folder3 folder4
+ll
+cp -r folder folder1
+ll
+cd folder
+ll
+cd ..
+cd folder1
+ll
+cd ..
+rm -r folder1
+ll
+rm -R folder1/
+ll
+sudo cp -r folder folder1
+ll
+cd folder1
+ll
+cd ..
+sudo chown -r rabbin:quest folder1
+sudo chown -R rabbin:quest folder1
+ll
+cd folder1
+ll
+cd ..
+sudo cp -r folder folder2
+sudo cp -r folder folder33
+sudo rm -R folder33
+ll
+sudo cp -r folder folder3
+sudo cp -r folder folder4
+sudo chown -R rabbin:quest folder2
+sudo chown -R rabbin:quest folder3
+sudo chown -R rabbin:quest folder4
+ll
+sodu chmod 777folder1
+suso chmod 777folder1
+sudo chmod 777folder1
+sudo chmod 777 folder1
+ll
+sudo chmod 777 folder2
+sudo chmod 777 folder3
+sudo chmod 777 folder4
+ll
+cd folder3
+ll
+cd ..
+ll
+cd ..
+ll
+rm -R user1 user2 user3 user4
+ll
+sudo cp -r user user1
+ll
+sudo cp -r user user2
+sudo cp -r user user3
+sudo cp -r user user4
+ll
+sudo chown -R sergio:quest user1/
+ll
+cd user1
+ll
+cd ..
+ll
+sudo chown -R urbano:quest user2/
+sudo chown -R peregrino:quest user3/
+sudo chown -R owner:quest user4
+ll
+sudo chmod 777 user1 user2 user3 user4
+ll
+cd user2
+cd music
+ll
+cd folder2
+ll
+cd ..
+ls
+cd ..
+history -w chmod-2parte.txt
+
