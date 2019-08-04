cde0fbcb987cd69cd41652ee0b33faa06f0f75b1
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 16:16:24 -0500

    Added Marlon's exercises

---
+++ Marlon/marlon-bash-homework1/marlon-bash-homework1.sh
@@ -0,0 +1,550 @@
+ls
+cd C\:/
+ls
+cd Programs/
+ls
+mv historial.txt ~/
+ls
+rm -r Program7 Program8 Program9
+ls
+cd Program0
+ls
+echo "Hello_World!" > Program0.exe
+echo "Hello_World!" > uninstal_Program0.exe
+cat Program0.exe 
+cat Program0.exe ../Program1/Program.exe
+cat Program0.exe ../Program1/Program1.exe
+cat Program0.exe ../Program1/uninstal_Program1.exe
+cat Program0.exe ../Program1/uninstall_Program1.exe
+cat Program0.exe ../Program2/uninstall_Program2.exe
+cat Program0.exe ../Program3/uninstall_Program3.exe
+cat Program0.exe ../Program4/uninstall_Program4.exe
+cat Program0.exe ../Program2/Program2.exe
+cat Program0.exe ../Program3/Program3.exe
+cat Program0.exe ../Program4/Program4.exe
+cd ..
+ls
+cd Users
+ls
+cd Jorge/
+ls
+rm -r Folder1 Folder2 Folder3 Downloads
+rm -r ../Marlon/Folder1 ../Marlon/Folder2 ../Marlon/Folder3 ../Marlon/Downloads
+rm -r ../Miriam/Folder1 ../Miriam/Folder2 ../Miriam/Folder3 ../Miriam/Downloads
+ls
+cd Desktop/
+ls
+rm Program5.lnk
+rm Program6+.lnk
+rm Program6.lnk
+rm Program7.lnk
+rm Program8.lnk
+rm Program9.lnk
+rm codeExample1.txt
+rm codeExample2.txt
+rm codeExample3.txt
+rm codeExample4.txt
+rm codeExample5.txt
+ls
+rm ../../Marlon/Desktop/Program5.lnk
+rm ../../Marlon/Desktop/Program6.lnk
+rm ../../Marlon/Desktop/Program7.lnk
+rm ../../Marlon/Desktop/Program8.lnk
+rm ../../Marlon/Desktop/Program9.lnk
+rm ../../Marlon/Desktop/codeExample1.txt
+rm ../../Marlon/Desktop/codeExample2.txt
+rm ../../Marlon/Desktop/codeExample3.txt
+rm ../../Marlon/Desktop/codeExample4.txt
+rm ../../Marlon/Desktop/codeExample5.txt
+rm ../../Miriam/Desktop/Program5.lnk
+rm ../../Miriam/Desktop/Program6.lnk
+rm ../../Miriam/Desktop/Program7.lnk
+rm ../../Miriam/Desktop/Program8.lnk
+rm ../../Miriam/Desktop/Program9.lnk
+rm ../../Miriam/Desktop/codeExample1.txt
+rm ../../Miriam/Desktop/codeExample2.txt
+rm ../../Miriam/Desktop/codeExample3.txt
+rm ../../Miriam/Desktop/codeExample4.txt
+rm ../../Miriam/Desktop/codeExample5.txt
+ls
+echo "Hello World!" > Program0.lnk
+echo "Hello World!" > Program1.lnk
+echo "Hello World!" > Program2.lnk
+echo "Hello World!" > Program3.lnk
+echo "Hello World!" > Program4.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program0.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program1.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program2.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program3.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program45.lnk
+echo "Hello World!" > ../../Marlon/Desktop/Program4.lnk
+echo "Hello World!" > ../../Miriam/Desktop/Program0.lnk
+echo "Hello World!" > ../../Miriam/Desktop/Program1.lnk
+echo "Hello World!" > ../../Miriam/Desktop/Program2.lnk
+echo "Hello World!" > ../../Miriam/Desktop/Program3.lnk
+
+rm ../../Marlon/Desktop/Program45.lnk
+cd ..
+ls
+cd Documents/
+ls
+rm *.docx
+rm ../../Marlon/Documents/*.docx
+rm ../../Miriam/Documents/*.docx
+ls
+echo "Hello World!" > *.pdf
+echo "Hello World!" > report0.pdf
+echo "Hello World!" > report1.pdf
+echo "Hello World!" > report2.pdf
+echo "Hello World!" > report3.pdf
+echo "Hello World!" > report4.pdf
+echo "Hello World!" > ../../Marlon/Documents/report0.pdf
+echo "Hello World!" > ../../Marlon/Documents/report1.pdf
+echo "Hello World!" > ../../Marlon/Documents/report2.pdf
+echo "Hello World!" > ../../Marlon/Documents/report3.pdf
+echo "Hello World!" > ../../Marlon/Documents/report4.pdf
+echo "Hello World!" > ../../Miriam/Documents/report0.pdf
+echo "Hello World!" > ../../Miriam/Documents/report1.pdf
+echo "Hello World!" > ../../Miriam/Documents/report2.pdf
+echo "Hello World!" > ../../Miriam/Documents/report3.pdf
+echo "Hello World!" > ../../Miriam/Documents/report4.pdf
+cd ..
+ls
+cd Music
+ls
+rm song06.mp3
+rm song07.mp3
+rm song08.mp3
+rm song09.mp3
+rm song10.mp3
+rm song11.mp3
+rm song12.mp3
+rm song13.mp3
+rm song14.mp3
+rm song15.mp3
+rm ../../Marlon/Music/song06.mp3
+rm ../../Marlon/Music/song07.mp3
+rm ../../Marlon/Music/song08.mp3
+rm ../../Marlon/Music/song09.mp3
+rm ../../Marlon/Music/song10.mp3
+rm ../../Marlon/Music/song11.mp3
+rm ../../Marlon/Music/song12.mp3
+rm ../../Marlon/Music/song13.mp3
+rm ../../Marlon/Music/song14.mp3
+rm ../../Marlon/Music/song15.mp3
+rm ../../Miriam/Music/song06.mp3
+rm ../../Miriam/Music/song07.mp3
+rm ../../Miriam/Music/song08.mp3
+rm ../../Miriam/Music/song09.mp3
+rm ../../Miriam/Music/song10.mp3
+rm ../../Miriam/Music/song11.mp3
+rm ../../Miriam/Music/song12.mp3
+rm ../../Miriam/Music/song13.mp3
+rm ../../Miriam/Music/song14.mp3
+rm ../../Miriam/Music/song15.mp3
+ls
+echo "Hello World" > song01.mp3
+echo "Hello World" > song02.mp3
+echo "Hello World" > song03.mp3
+echo "Hello World" > song04.mp3
+echo "Hello World" > song05.mp3
+echo "Hello World" > ../../Marlon/Music/song01.mp3
+echo "Hello World" > ../../Marlon/Music/song02.mp3
+echo "Hello World" > ../../Marlon/Music/song03.mp3
+echo "Hello World" > ../../Marlon/Music/song04.mp3
+echo "Hello World" > ../../Marlon/Music/song05.mp3
+echo "Hello World" > ../../Miriam/Music/song01.mp3
+echo "Hello World" > ../../Miriam/Music/song02.mp3
+echo "Hello World" > ../../Miriam/Music/song03.mp3
+echo "Hello World" > ../../Miriam/Music/song04.mp3
+echo "Hello World" > ../../Miriam/Music/song05.mp3
+rm -r ArsenalDeRimas Chojin Gondwana HijosDeLasHojas LionReggae LosAldeanos MikeDiaz Proof RafaelLechowski Rapsusklei
+cd ..
+cd Marlon/
+cd Music/
+ls
+rm -r ArsenalDeRimas Chojin Gondwana HijosDeLasHojas LionReggae LosAldeanos MikeDiaz Proof RafaelLechowski Rapsusklei
+cd ..
+cd Miriam/
+cd Music/
+ls
+rm -r ArsenalDeRimas Chojin Gondwana HijosDeLasHojas LionReggae LosAldeanos MikeDiaz Proof RafaelLechowski Rapsusklei
+ls
+cd ..
+cd Jorge
+ls
+cd Pictures/
+ls
+rm *.jpg
+rm ../../Marlon/Pictures/*.jpg
+rm ../../Miriam/Pictures/*.jpg
+ls
+echo "Hello World" > Animation1.gif
+echo "Hello World" > Animation2.gif
+echo "Hello World" > Animation3.gif
+echo "Hello World" > Animation4.gif
+echo "Hello World" > Animation5.gif
+echo "Hello World" > Animation6.gif
+rm Animation6.gif
+echo "Hello World" > ../../Marlon/Pictures/Animation1.gif
+echo "Hello World" > ../../Marlon/Pictures/Animation2.gif
+echo "Hello World" > ../../Marlon/Pictures/Animation3.gif
+echo "Hello World" > ../../Marlon/Pictures/Animation4.gif
+echo "Hello World" > ../../Marlon/Pictures/Animation5.gif
+echo "Hello World" > ../../Miriam/Pictures/Animation1.gif
+echo "Hello World" > ../../Miriam/Pictures/Animation2.gif
+echo "Hello World" > ../../Miriam/Pictures/Animation3.gif
+echo "Hello World" > ../../Miriam/Pictures/Animation4.gif
+echo "Hello World" > ../../Miriam/Pictures/Animation5.gif
+ls
+cd ..
+ls
+cd Videos/
+ls
+rm *.mp4
+rm ../../Marlon/Videos/*.mp4
+rm ../../Miriam/Videos/*.mp4
+ls
+echo "Hello World!" > Movie01.avi
+echo "Hello World!" > Movie02.avi
+echo "Hello World!" > Movie03.avi
+echo "Hello World!" > Movie04.avi
+echo "Hello World!" > Movie05.avi
+echo "Hello World!" > ../../Marlon/Videos/Movie01.avi
+echo "Hello World!" > ../../Marlon/Videos/Movie02.avi
+echo "Hello World!" > ../../Marlon/Videos/Movie03.avi
+echo "Hello World!" > ../../Marlon/Videos/Movie04.avi
+echo "Hello World!" > ../../Marlon/Videos/Movie05.avi
+echo "Hello World!" > ../../Miriam/Videos/Movie01.avi
+echo "Hello World!" > ../../Miriam/Videos/Movie02.avi
+echo "Hello World!" > ../../Miriam/Videos/Movie03.avi
+echo "Hello World!" > ../../Miriam/Videos/Movie04.avi
+echo "Hello World!" > ../../Miriam/Videos/Movie05.avi
+cd ..
+ls
+cd Music
+ls
+cd BobMarley/
+ls
+cat BobMarley-BadBoys.mp3 
+nano BobMarley-BadBoys.mp3 
+touch BobMarley-BadBoys.mp3 
+ls
+nano BobMarley-BadBoys.mp3 
+cat BobMarley-BadBoys.mp3 
+ls
+nano BobMarley-BuffaloSoldier.mp3 
+nano BobMarley-IsThisLove.mp3.mp3 
+ls 
+cat BobMarley-IsThisLove.mp3.mp3 BobMarley-IsThisLove.mp3
+rm BobMarley-IsThisLove.mp3.mp3
+ls
+nano BobMarley-OneLove.mp3 
+nano BobMarley-ThreeLittleBirds.mp3 
+nano BobMarley-RedemptionSong.mp3 
+ls
+nano BobMarley-RedemptionSong.mp3 
+cat BobMarley-RedemptionSong.mp3 
+cat BobMarley-NoWomanNoCry.mp3 
+nano BobMarley-NoWomanNoCry.mp3 
+nano BobMarley-CouldYouBeLoved.mp3 
+ls
+nano BobMarley-ALalalaLong.mp3 
+nano BobMarley-BurninAndLootin.mp3 
+ls
+cd ..
+ls
+cd Canserbero/
+ls
+nano Canserbero-.mp3 
+ls
+nano Canserbero-EsEpico.mp3 
+nano Canserbero-HastaCuando.mp3 
+nano Canserbero-Llovia.mp3 
+nano Canserbero-MundoDePiedra.mp3 
+nano Canserbero-YEnUnEspejoVi.mp3 
+nano Canserbero-PensandoEnTi.mp3 
+nano Canserbero-Jeremias17-5.mp3 
+nano Canserbero-YLaFelicidadQue.mp3 
+nano Canserbero-DeMiMuerte.mp3 
+nano Canserbero-ElPrimerTrago.mp3 
+cat Canserbero-ElPrimerTrago.mp3 
+ls
+cd ..
+ls
+cd Eminem
+ls
+nano Eminem-ImNotAfraid.mp3 
+nano Eminem-TillICollapse.mp3 
+nano Eminem-Venom.mp3 
+nano Eminem-WhenImGone.mp3 
+nano Eminem-WithOutMe.mp3 
+nano Eminem-RapGod.mp3
+nano Eminem-LoseYourself.mp3
+nano Eminem-LoveTheWayYouLie.mp3
+nano Eminem-Stan.mp3
+nano Eminem-TheRealSlimShady.mp3
+cd ..
+cd Gorillaz/
+ls
+nano Gorillaz-ClintEastwood.mp3 
+nano Gorillaz-FeelGoodInc.mp3 
+nano Gorillaz-KidsWithGuns.mp3 
+nano Gorillaz-OnMelancolicHill.mp3 
+nano Gorillaz-RockTheHouse.mp3 
+nano Gorillaz-Dare.mp3 
+nano Gorillaz-19-2000.mp3 
+nano Gorillaz-Humility.mp3 
+nano Gorillaz-Andromeda.mp3 
+nano Gorillaz-TomorrowComesToday.mp3 
+cd ..
+cd LirikaInversa/
+ls
+nano LirikaInversa-Humano.mp3 
+nano LirikaInversa-Mandala.mp3 
+nano LirikaInversa-Niebla.mp3 
+nano LirikaInversa-Statera.mp3 
+nano LirikaInversa-TempestadPrimigenia.mp3 
+nano LirikaInversa-DosCallesDespuesDelInfinito.mp3 
+nano LirikaInversa-DelAmorYCosasLindas.mp3 
+nano LirikaInversa-YoPodria.mp3 
+nano LirikaInversa-Tormenta.mp3 
+nano LirikaInversa-Animadversion.mp3 
+cd ..
+ls
+cd MichaelJackson/
+ls
+nano MichaelJackson-BeatIt.mp3 
+nano MichaelJackson-BillieJean.mp3 
+nano MichaelJackson-SmoothCriminal.mp3 
+nano MichaelJackson-TheWayYouMakeMeFeel.mp3 
+nano MichaelJackson-Thriller.mp3 
+nano MichaelJackson-WeAreTheWorld.mp3 
+nano MichaelJackson-TheyDontCareAboutUs.mp3 
+nano MichaelJackson-EarthSong.mp3 
+history > ~/historial_Tarea 
+cd ~
+ls
+cat historial_Tarea 
+mv historial_Tarea bash-course/Marlon
+ls
+cd bash-course/
+ls
+cd Marlon/
+ls
+git checkout -b marlon_Tarea
+git status
+git add .
+git commit -m "Tarea de Marlon"
+git status 
+git push origin marlon_Tarea
+cd ~
+ls
+ls -la
+cd .bashrc
+cat .bashrc
+nano .bashrc
+salvar
+alias salvar
+source .bashrc
+salvar
+nano .bashrc
+source .bashrc
+ls
+cd bash_class/
+ls
+touch file0.txt
+chmod u=rwx,g=rw,o=r file0.txt
+ls -l
+ll
+kill -l
+cd ..
+ls
+cat historial.txt 
+history
+cd bash-course/
+cd Marlon/
+ls
+cat historial_Tarea 
+historial
+history
+cd ..
+ls
+cd C\:/
+LS
+ls
+cd Users
+ls
+Jorge
+cd Jorge
+ls
+cd Music
+ls
+cd MichaelJackson/
+cd ~/bash-course/Marlon
+ls
+cat historial_Tarea 
+history
+cd ~/C\:/Users/Marlon/Music/MichaelJackson/
+ls
+cd ~
+ls
+cd bash-course/
+cd Marlon/
+ls
+cd ~/C\:/Users/Jorge/Music/MichaelJackson/
+ls
+history
+ls
+nano MichaelJackson-YouAreNotAlone.mp3
+nano MichaelJackson-BlackAndWhite.mp3
+ls
+cd ..
+ls
+cd Nach/
+ls
+ 
+nano Nach-ElIdiomaDeLosDioses.mp3 
+nano Nach-EfectosVocales.mp3 
+rm Nach-EfectosVocales.mp3 
+nano Nach-EfectosVocales.mp3 
+nano Nach-LosZurdosMuerenAntes.mp3 
+nano Nach-MiGranAmor.mp3 
+nano Nach-MilVidas.mp3 
+nano Nach-NiEstabasNiEstaras.mp3 
+nano Nach-ElAmorVieneYVa.mp3 
+nano Nach-Angel.mp3 
+nano Nach-Ellas.mp3 
+nano Nach-Anochece.mp3 
+ls
+cd ..
+ls
+cd Queen/
+ls
+nano Queen-BohemianRhapsody.mp3 
+nano Queen-IWantToBreakFree.mp3 
+nano Queen-SomebodyToLove.mp3 
+nano Queen-WeAreTheChampions.mp3 
+nano Queen-WeWillRockYou.mp3 
+nano Queen-LoveOfMyLife.mp3 
+nano Queen-DontStopMeNow.mp3 
+nano Queen-AnotherOneBitesTheDust.mp3 
+nano Queen-UnderPressure.mp3 
+nano Queen-CrazyLittleThingCalledLove.mp3 
+ls
+cd ..
+ls
+cd Residente
+ls
+nano Residente-DagombasEnTamale.mp3 
+nano Residente-Desencuentro.mp3 
+nano Residente-Guerra.mp3 
+nano Residente-SomosAnormales.mp3 
+nano Residente-UnaLeyendaChina.mp3 
+nano Calle13-MuerteEnHawaii.mp3 
+nano Calle13-OjosColorSol.mp3 
+nano Calle13-LaBala.mp3 
+nano Calle13-Latinoamerica.mp3 
+nano Calle13-ElAguante.mp3 
+cd ..
+ls
+cd TheBeatles/
+ls
+nano TheBeatles-DontLetMeDown.mp3 
+nano TheBeatles-HeyJude.mp3 
+nano TheBeatles-LetItBe.mp3 
+nano TheBeatles-LucyInTheSkyWithDiamonds.mp3 
+nano TheBeatles-Yesterday.mp3 
+nano TheBeatles-AndILoveHer.mp3 
+nano TheBeatles-ComeTogether.mp3 
+nano TheBeatles-Something.mp3 
+nano JohnLennon-Imagine.mp3 
+nano JohnLennon-ADayInTheLife.mp3 
+cat JohnLennon-ADayInTheLife.mp3 
+cd ls
+ls
+cd ..
+ls
+cat song01.mp3 
+cd ..
+cd .
+cd ..
+ls
+cd Marlon
+ls
+rm -r Music
+cd ..
+ls
+cd Miriam/
+ls
+rm -r Music
+cd ..
+cd Jorge/
+LS
+ls
+cp -r Music ../Marlon/
+cp -r Music ../Miriam/
+cd ..
+cd Marlon/
+ls
+cd ..
+ls
+cd Windows/
+ls
+rm -r System32/
+ls
+cd Folder0
+ls
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+ls
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+cd ..
+ls
+cd Folder1
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+cd ..
+cd Folder2
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+cd ..
+cd Folder3
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+cd ..
+cd Folder4
+rm archive5.txt archive6.txt archive7.txt archive8.txt archive9.txt
+cd ..
+ls -R
+cd Folder0
+echo "Hello World" > archive0.txt
+echo "Hello World" > archive1.txt
+echo "Hello World" > archive2.txt
+echo "Hello World" > archive3.txt
+echo "Hello World" > archive4.txt
+echo "Hello World" > ../Folder1/archive0.txt
+echo "Hello World" > ../Folder1/archive1.txt
+echo "Hello World" > ../Folder1/archive2.txt
+echo "Hello World" > ../Folder1/archive3.txt
+echo "Hello World" > ../Folder1/archive4.txt
+echo "Hello World" > ../Folder2/archive0.txt
+echo "Hello World" > ../Folder2/archive1.txt
+echo "Hello World" > ../Folder2/archive2.txt
+echo "Hello World" > ../Folder2/archive3.txt
+echo "Hello World" > ../Folder2/archive4.txt
+echo "Hello World" > ../Folder3/archive0.txt
+echo "Hello World" > ../Folder3/archive1.txt
+echo "Hello World" > ../Folder3/archive2.txt
+echo "Hello World" > ../Folder3/archive3.txt
+echo "Hello World" > ../Folder3/archive4.txt
+echo "Hello World" > ../Folder4/archive0.txt
+echo "Hello World" > ../Folder4/archive1.txt
+echo "Hello World" > ../Folder4/archive2.txt
+echo "Hello World" > ../Folder4/archive3.txt
+echo "Hello World" > ../Folder4/archive4.txt
+cd ..
+cd bash-course/
+cd Marlon/
+ls
+mkdir Tarea1
+git log
+ls
+cd Tarea1
+history > tarea1_historial.txt
