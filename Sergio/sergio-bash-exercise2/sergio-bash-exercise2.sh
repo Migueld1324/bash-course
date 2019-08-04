ae0f1ae7a8f08877994f66c02ff9ef3fa0f96446
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 18:17:28 -0500

    Added Sergio's exercise 2

---
+++ Sergio/sergio-bash-exercise2/sergio-bash-exercise2.sh
@@ -0,0 +1,452 @@
+sudo adduser rabbanito
+sudo adduser Hector
+sudo adduser peregrino01
+sudo adduser luiscarlos01
+sudo addgroup casaBts
+sudo addgroup casaBts01
+sudo addgroup casaBts2019
+sudo addgroup casaBts2019 --force-badname
+sudo adduser owner
+ls
+cd Documents/
+ls
+cd internship2019/
+cd migueD/
+ls
+cd ejercicio1/
+ls
+chown c owner
+chown 'c' own
+chown owner c
+chown owner c/
+chown owner 'c'
+sudo addgroup admin2 --force-badname
+sudo addgroup guest --force-badname
+sudo -R owner:admin2 c/
+sudo -u owner:admin2 c/
+sudo -u owner c/
+sudo -u owner 'c'
+sudo -R owner:admin2 c/
+cd c
+ll
+sudo -R owner:admin2 c/
+sudo -R owner:admin2 User/
+sudo -r owner:admin2 User/
+sudo -r owner:admin2 'User/'
+sudo -r owner:admin2 Historial.txt 
+sudo -R owner:admin2 User/ 
+sudo chown -r owner:admin2 User/ 
+sudo chown -R owner:admin2 User/ 
+ll
+sudo chown -R owner User/ 
+ll
+sudo chown -R admin2 User/ 
+sudo chgrp -R admin2 User/ 
+ll
+sudo chgrp -R admin2 Historial.txt 
+ll
+cd programFiles/
+ll
+sudo chgrp -R admin2 diez/ nueve/ ocho/ seis/ siete/ 
+ll
+sudo chgrp -R admin2 diez/
+ll
+cd ..
+sudo chgrp -R admin2 programFiles/diez/
+ll
+sudo chgrp -R owner:admin2 programFiles/diez/
+sudo chgrp -R admin2 programFiles/diez/
+ll
+sudo chgrp -R admin2 programFiles/diez
+cd programFiles/
+ll
+sudo chgrp -R admin2 diez/
+sudo chgrp -R admin2 diez
+ll
+cd ..
+sudo -R owner:admin2 programFiles/
+sudo chown -R owner:admin2 programFiles/
+ll
+sudo addgroup admin2
+sudo adduser owner
+sudo chown -R owner Historial.txt 
+ll
+ls
+cd ..
+ls
+cd sergio/
+ls
+cd Documents
+cd Documents/
+cd ..
+cd ...
+cd ..
+cd..
+cd ..
+cd..
+ls
+cd mnt
+ls
+cd c
+ls
+cd Users/
+ls
+cd BTS-013/
+ls
+cd Documents/
+ls
+cd internship2019/
+cd migueD/
+cd ejercicio1/
+ls
+cd c
+sudo chown -R owner Historial.txt 
+ll
+sudo chown -R owner Historial.txt 
+ll
+sudo chgrp -R admin2 programFiles/
+ll
+sudo chgrp admin2 programFiles
+ll
+sudo chgrp admin programFiles/
+sudo chgrp -r admin programFiles/
+sudo chgrp -R admin programFiles/
+ll
+sudo chgrp -R admin2 programFiles/diez/
+cd programFiles/
+ll
+sudo chgrp -R admin2 diez/
+ll
+cd ..
+sudo chgrp -R admin2 programFiles/
+ll
+sudo groupadd admin2
+sudo useradd sergio
+chgrp -R admin2 programFiles/
+sudo sergio chgrp -R admin2 programFiles/
+sudo chgrp -R admin2 programFiles/
+l
+ll
+cd programFiles/
+ll
+sudo chown diez owner
+sudo chown owner diez
+ll
+sudo chown -r owner diez
+sudo chown -R owner diez
+ll
+cd ..
+ls
+cd ..
+cd 
+ls
+mkdir Documentos
+cd Documentos/
+mkdir Bluetrail
+cd Bluetrail/
+mkdir bash
+cd bash/
+cp -r ../../../../../../mnt/c/Users/BTS-013/Documents/internship2019/migueD/ejercicio1/c/ .
+ls
+cd c/
+ls
+cd programFiles/
+sudo chown owner:admin2 diez/
+ll
+sudo chown owner:admin2 ocho/
+sudo chown owner:admin2 siete/
+sudo chown owner:admin2 seis/
+ll
+sudo chown owner:admin2 seis/
+ll7
+ll
+sudo chown owner:admin2 seis
+sudo chown owner:admin2 nueve/
+cd ..
+ls
+ll
+sudo chown owner:guest User/
+ll
+sudo chown -R owner:guest User/
+cd programFiles/
+ll
+cd diez
+ll
+cd ..
+sudo chown -R owner:admin2 diez/ nueve/ ocho/ seis/ siete/
+ll
+cd ..
+ll
+cd User/
+ll
+cd Niko/
+ll
+cd ..
+ll
+sudo chown -R rabbanito Rabbanito/
+ll
+sudo chown -R niko Niko/
+sudo adduser niko
+sudo chown -R niko Niko/
+ll
+history -w sergio-bash-exercise2.txt
+noano sergio-bash-exercise2.txt 
+nano sergio-bash-exercise2.txt 
+notepad sergio-bash-exercise2.txt 
+ls
+cd Documentos/
+ls
+cd Bluetrail/
+ls
+cd bash/
+ls
+cd c/
+ls
+cd User/
+ls
+cd Sergio/
+cd Music/
+ll
+sudo chmod -u -g o-
+sudo chmod -u,-g,o-
+sudo chmod 444 Adele/
+ll
+sudo chmod u= Adele/
+ll
+sudo chmod g= Adele/
+ll
+sudo chmod o= Adele/
+ll
+sudo chmod u=r g= o= AmloMusic/
+sudo chmod u=r,g=,o= AmloMusic/
+ll
+sudo chmod u=rw,g=,o= BatmanSongs/
+ll
+sudo chmod u=rwx,g=,o= Cuahutemocblanco/
+ll
+sudo chmod u=rwx,g=r,o= EnriqueIglesias/
+ll
+sudo chmod u=rwx,g=rw,o= GrupoMarranos/
+ll
+sudo chmod u=rwx,g=rwx,o= GrupoOleguarro/
+ll
+sudo chmod u=rwx,g=rwx,o=r GrupoOleguarro/
+ll
+sudo chmod u=rwx,g=rwx,o=rw GrupoOleguarro/
+ls
+ll0
+sudo chmod u=rwx,g=rwx,o= GrupoOleguarro/
+ll
+sudo chmod u=rwx,g=rwx,o=r JonaLaExploradora/
+ll
+sudo chmod u=rwx,g=rwx,o=rw Pink/
+ll
+cd Adele/
+CD JonaLaExploradora/
+cd JonaLaExploradora/
+cd Pink/
+cd vicenteFernandez/
+ll
+cd ..
+llc
+sudo chmod 777 Adele/ AmloMusic/ BatmanSongs/ Cuahutemocblanco/ EnriqueIglesias/ GrupoMarranos/ GrupoOleguarro/ JonaLaExploradora/ Pink/
+ll
+cd Adele/
+ll
+sudo chmod 444 Adele-RollingInTheDeep.txt 
+ll
+sudo chmod o=,g= Adele-RollingInTheDeep.txt 
+ll
+sudo chmod u=,o=,g= Adele-RollingInTheDeep.txt 
+ll
+sudo chmod u=r,o=,g= Adele-SetFireToTheRain.txt 
+ll
+sudo chmod u=rw,o=,g= Adele-SomeoneLikeYou.txt 
+sudo chmod u=rwx,o=,g= EnriqueIglesias-Trapesista.txt
+sudo chmod u=rwx,o=r,g= EnriqueIglesias-Trapesista.txtSaliste\ de\ entre\ el\ fondo\ de\ la\ noche 
+ll
+sudo chmod u=rwx,g=r,o= EnriqueIglesias-Trapesista.txtSaliste\ de\ entre\ el\ fondo\ de\ la\ noche 
+ll
+sudo chmod u=rwx,g=rw,o= FrancoDeVita-tengo.txt 
+sudo chmod u=rwx,g=rwx,o= GrupoMarranos-Ancioso.txt 
+ll
+sudo chmod u=rwx,g=rwx,o=r Intocable-HoyMeVasAPerder.txt 
+ll
+sudo chmod u=rwx,g=rwx,o=rw JonaLaEcploradora.txt 
+ll
+cd ..
+ll
+cd Adele/
+ls
+cd ..
+ls
+ll
+rm -r AmloMusic/ BatmanSongs/ Cuahutemocblanco/ EnriqueIglesias/ GrupoMarranos/ GrupoOleguarro/ JonaLaExploradora/ Pink/ vicenteFernandez/
+cp -r Adele/ AmloMusic/ BatmanSongs/ CuahutemocBlanco/ EnriqueIglesias/ GrupoMarranos/ GrupoOleguarro/ JonaLaExploradora/ Pink/ VicenteFernandez/
+cp -r Adele/ AmloMusic/ 
+sudo chmod -r Adele/
+ll
+cd Adele/
+ll
+cd ..
+sudo chmod -r 777 Adele/
+sudo chmod -r u=7 Adele/
+cd Adele/
+ll
+cd ..
+sudo chmod u=r Adele/
+ll
+sudo chmod 777 Adele/
+ll
+cd Adele/
+cd ..
+cd Adele/
+ll
+cd ..
+ls
+ll
+rm -r AmloMusic/
+sudo cp -r Adele/ AmloMusic/
+ll
+sudo chown sergio:guest AmloMusic/
+ll7
+ll
+sudo chmod u=rwx,g=rwx,o=rwx AmloMusic/
+ll
+cd AmloMusic/
+ll
+cd ..
+sergio cp -r Adele/ E
+sudo cp -r Adele/ EnriqueIglesias/
+ll
+sudo chmod u=rwx,g=rwx,o=rwx EnriqueIglesias/
+ll
+sudo cp -r Adele/ FrancoDeVita/
+ll
+sudo chmod u=rwx,g=rwx,o=rwx FrancoDeVita/
+ll
+sudo cp -r Adele/ GrupoOlegarro/
+ll
+sudo chmod u=rwx,g=rwx,o=rwx GrupoOlegarro/
+ll
+sudo cp -r Adele/ GrupoMarranos/
+ll
+sudo chmod u=rwx,g=rwx,o=rwx GrupoMarranos/
+ll
+sudo chown sergio:guest Adele/
+ll
+sudo chown sergio:guest EnriqueIglesias/
+sudo chown sergio:guest FrancoDeVita/
+ll
+sudo chown sergio:guest GrupoMarranos/
+ll
+sudo chown sergio:guest GrupoOlegarro/
+ll
+sudo chown sergio:guest file.txt 
+ll
+cd ..
+ll
+rm -r Niko/
+ll
+rm -r Rabbanito/
+sudo cp -r Sergio/ Rabbanito/
+ll
+sudo cp -r Sergio/ Niko/
+ll
+sudo chown niko:guest Niko/
+ll
+sudo chown rabbanito:guest Niko/
+sudo chown rabbanito:guest Rabbanito/
+sudo chown niko:guest Niko/
+ll
+sudo chown sergio:guest Sergio/
+ll
+sudo chmod 777 Niko/
+ll
+sudo chmod 777 Rabbanito/
+ll
+cd Rabbanito/
+ll
+cd ..
+sudo chown -R niko:guest Niko/
+ll
+cd Niko/
+ll
+cd ..
+sudo chown -R rabbanito:guest Rabbanito/
+ll
+cd Rabbanito/
+ll
+cd Music/
+ll
+cd Adele/
+ll
+cd ..
+ll
+sudo chmod 777 Adele/
+ll
+sudo chmod 777 AmloMusic/
+sudo chmod 777 EnriqueIglesias/
+sudo chmod 777 FrancoDeVita/
+sudo chmod 777 GrupoMarranos/
+sudo chmod 777 GrupoOlegarro/
+ll
+cd FrancoDeVita/
+ll
+cd ..
+cd Niko/
+cd Music/
+ll
+sudo chmod 777 GrupoMarranos/
+sudo chmod 777 GrupoOlegarro/
+sudo chmod 777 Adele/
+sudo chmod 777 FrancoDeVita/
+sudo chmod 777 EnriqueIglesias/
+ll
+sudo chmod 777 AmloMusic/
+ll
+cd Adele/
+ll
+cd ..
+cd Rabbanito/
+ll
+sudo chmod 777 Area51/
+sudo chmod 777 Meteorito/
+sudo chmod 777 Music/
+sudo chmod 777 Porno/
+sudo chmod 777 Videos/
+cd Music/
+ll
+cd A
+cd Adele/
+ll
+cd ..
+ls
+cd User/
+cd Sergio/
+ll
+cd Music/
+ll
+cd Adele/
+ll
+cd ..
+cd Niko/
+ll
+sudo chmod 777 Area51/
+sudo chmod 777 Meteorito/
+sudo chmod 777 Music/
+sudo chmod 777 Porno/
+sudo chmod 777 Videos/
+ll
+cd Music/
+ll
+cd A
+cd Ad
+cd ad
+cd Ad
+ll
+cd Adele/
+ll
+cd..
+cd ..
+history -w sergio-bash-exercise2.txt
