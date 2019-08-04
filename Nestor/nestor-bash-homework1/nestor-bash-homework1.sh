52355e70e062ddd6483724c09364049f460654a3
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 16:32:48 -0500

    Added Nestor's homework 1

---
+++ Nestor/nestor-bash-homework1/nestor-bash-homework1.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+ls
+cd
+ls
+cd ..
+7ls
+ls
+cd ..
+ls
+cd mnt/
+ls
+cd c
+ls
+cd intershipBTS/
+ls
+bash-class/
+ls
+cd bash-class/
+ls
+cd C
+ls
+cd Users/
+ls
+cd user/
+cd music/
+ls
+cd folder/
+ls
+
+ls
+vim "acdc-this house is on fire".txt
+ls
+vim "alden richards-one way".txt
+ls
+cat alden\ richards-one\ way.txt 
+ls
+vim "andy brown-tequila".txt
+ls
+vim "ash-jack names the plantes".txt
+vim "maroon 5-this love".txt
+vim "melisa o'neil-let it go".txt
+ls
+vim "michael jackson-love is here and now you're gone".txt
+vim "john prine-far from me".txt
+ls
+vim "juan miguel severo-you don't love me, it's okay".txt
+ls
+cd ..
+ls
+cp -r folder folder1
+ls
+cd folder1
+ls
+cd ..
+cp -r folder folder2
+cp -r folder folder3
+cp -r folder folder4
+cp -r folder folder5
+cp -r folder folder6
+cp -r folder folder7
+cp -r folder folder8
+cp -r folder folder9
+ls
+cd ..
+ls
+cd ..
+ls
+cp -r user user1
+ls
+cp -r user user2
+cp -r user user3
+cp -r user user4
+ls
+cd user2
+ls
+cd music/
+ls
+cd folder9/
+ls
+cd..
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+history -w tareaHistorial.txt
