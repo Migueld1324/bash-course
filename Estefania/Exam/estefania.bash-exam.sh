f7014866d26b16ae8530a77e5b9236865d6aaf64
Miguel Delgado <migueld@bluetrailsoft.com>
2019-08-04 20:41:08 -0500

    Added Estefania's exam

--- Estefania/Exam/estefania.bash-exam.sh
+++ Estefania/Exam/estefania.bash-exam.sh
@@ -3,18 +3,14 @@
 mkdir "Bash"
 cd Bash/
 
-   mkdir Ls
-    cd Ls
-    echo "list directory contents" >> Ls/ls.txt
-    echo "muestra los archivos y carpetas">> Ls/description.txt
-    echo "-a -l -la">> Ls/options.txt
-    echo "ls -a, ls -l, ls -la" >> Ls/examples.txt
-    cd ../
+mkdir Ls
+echo "list directory contents" >> Ls/ls.txt
+echo "muestra los archivos y carpetas">> Ls/description.txt
+echo "-a -l -la">> Ls/options.txt
+echo "ls -a, ls -l, ls -la" >> Ls/examples.txt
 
-   mkdir cd
-    cd Cd
-    echo "" >> Cd/cd.txt
-    echo "">> Cd/description.txt
-    echo "-a -l -la">> Cd/options.txt
-    echo "ls -a, ls -l, ls -la" >> Cd/examples.txt
-    cd ../
+mkdir cd
+echo "" >> Cd/cd.txt
+echo "">> Cd/description.txt
+echo "-a -l -la">> Cd/options.txt
+echo "ls -a, ls -l, ls -la" >> Cd/examples.txt
